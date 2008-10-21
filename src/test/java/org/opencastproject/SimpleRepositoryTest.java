27c1e1b1d27f63a60464c95cb55bb2bffc24e507
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-10-21 21:37:44 +0000

    Adding some JCR tests to get a feel for how the API works.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@2936 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/test/java/org/opencastproject/SimpleRepositoryTest.java
@@ -0,0 +1,88 @@
+package org.opencastproject;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SimpleRepositoryTest {
+	private static final Logger logger = LoggerFactory.getLogger(SimpleRepositoryTest.class);
+	
+	// TODO Configure a cluster-able JCR Repo (via spring ?)
+	// TODO Configure login to JCR.  Not sure how this is going to be done
+	
+	Repository repo;
+	ClassPathXmlApplicationContext appContext;
+	
+	@Before
+	public void setUp() throws Exception {
+		appContext = new ClassPathXmlApplicationContext("classpath:/jcr-test.xml");
+		repo = (Repository) appContext.getBean("jcrRepository");
+	}
+
+	public void tearDown() throws Exception {
+		appContext.destroy();
+	}
+
+	@Test
+	public void testTransientRepositoryExists() throws Exception {
+		assertNotNull(repo);
+	}
+	
+	@Test
+	public void testLogin() throws Exception {
+		Session session = repo.login();
+        try {
+            String user = session.getUserID();
+            String name = repo.getDescriptor(Repository.REP_NAME_DESC);
+            logger.info("Logged in as " + user + " to a " + name + " repository.");
+        } finally {
+            session.logout();
+        }
+	}
+
+	@Test
+	public void testStoreAndRetrieveContent() throws Exception {
+		Session session1 = repo.login(new SimpleCredentials("username", "password".toCharArray()));
+		Session session2 = repo.login(new SimpleCredentials("username", "password".toCharArray()));
+
+		try {
+            // Store content under session 1
+            Node root1 = session1.getRootNode();
+            Node hello1 = root1.addNode("hello");
+            Node world1 = hello1.addNode("world");
+            world1.setProperty("message", "Hello, World!");
+            root1.save();
+            session1.save();
+
+            // Retrieve content under session2
+            Node root2 = session2.getRootNode();
+            Node hello2 = root2.getNode("hello");
+            Node world2 = hello2.getNode("world");
+            logger.info(world2.getPath());
+            logger.info(world2.getProperty("message").getString());
+
+            // Remove content
+            hello2.remove();
+            root2.save();
+            session2.save();
+            assertFalse(root2.hasNode("hello"));
+            assertFalse(root2.hasNode("hello/world"));
+        } finally {
+            session1.logout();
+            session2.logout();
+        }
+	}
+	
+}
