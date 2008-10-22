8ecd8c3af85deb0f2256a1d0ead3534393ed7c49
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-10-22 00:49:23 +0000

    updated sample client code
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@2951 e6edf6fb-f266-4316-afb4-e53d95876a76

--- jcr/jcr-client/src/main/java/org/opencastproject/SampleJcrWriter.java
+++ jcr/jcr-client/src/main/java/org/opencastproject/SampleJcrWriter.java
@@ -5,8 +5,16 @@ import javax.jcr.Repository;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
 
-public class SampleJcrWriter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class SampleJcrWriter {
+	private Logger logger = LoggerFactory.getLogger(SampleJcrWriter.class);
+	public static void main(String... args) {
+		SampleJcrWriter writer = new SampleJcrWriter();
+		writer.writeNodes();
+	}
+	
 	public void writeNodes() {
 		OpencastJcrServer server = new OpencastJcrServer();
 		Repository repo = server.getRepository();
@@ -15,8 +23,9 @@ public class SampleJcrWriter {
 			Node currentParent = s.getRootNode();
 			int i=0;
 			while(i++ < 5) {
-				Node currentNode = addChild(currentParent);
-				currentNode.setProperty("count", new Integer(i).toString());
+				String suffix = new Integer(i).toString();
+				Node currentNode = addChild(currentParent, suffix);
+				logger.info("added node " + currentNode.getName() + " to parent " + currentParent.getName());
 				currentParent.save();
 				s.save();
 				currentParent = currentNode;
@@ -29,7 +38,7 @@ public class SampleJcrWriter {
 		}
 	}
 	
-	protected Node addChild(Node node) throws Exception {
-		return node.addNode("child");
+	protected Node addChild(Node node, String suffix) throws Exception {
+		return node.addNode("child_" + suffix);
 	}
 }
