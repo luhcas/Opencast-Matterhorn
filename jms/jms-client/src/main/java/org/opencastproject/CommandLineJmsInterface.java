964b9635c16a4496e2ced840e9e84ab8ee1c3957
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-10-28 20:17:24 +0000

    added jms-client
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@2964 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ jms/jms-client/src/main/java/org/opencastproject/CommandLineJmsInterface.java
@@ -0,0 +1,95 @@
+package org.opencastproject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.jms.Connection;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+public class CommandLineJmsInterface {
+	public static final String TOPIC_ID = "the_test_topic";
+	public static final String SUBSCRIBER_ID = "test_subscriber";
+
+	public static void main(String... args) {
+		try {
+			System.out.print("Enter a unique name for this JMS client: ");
+			String nodeNumberInput;
+			try {
+				nodeNumberInput = new BufferedReader(new InputStreamReader(System.in)).readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+			OpencastJmsServer server = new OpencastJmsServer();
+			Connection conn = server.getConnection("CommandLineJmsInterface_"+ nodeNumberInput);
+			Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Topic topic = session.createTopic(TOPIC_ID);
+			conn.start();
+
+			MessageConsumer subscriber = null;
+
+			try {
+				System.out.println("Type 'send <message>' to send a message to a pre-defined topic.");
+				System.out.println("Type 'subscribe' to display messages sent to the topic.");
+				System.out.println("Type 'unsubscribe' to stop displaying messages sent to the topic.");
+				System.out.println("Type 'quit' to exit.");
+				while(true) {
+					System.out.print("> ");
+					BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+					String input;
+					try {
+						input = br.readLine();
+					} catch (IOException e) {
+						e.printStackTrace();
+						break;
+					}
+					if(input == null) continue;
+
+					if("quit".equals(input)) {
+						break;
+					} else if(input.startsWith("send")) {
+						String messageText = input.substring(input.indexOf(" ") + 1);
+						try {
+							MessageProducer producer = session.createProducer(topic);
+							TextMessage message = session.createTextMessage(messageText);
+							message.setJMSDestination(topic);
+							producer.send(message);
+						} catch (Exception e) {
+							System.out.println(e.getMessage());
+						}
+					} else if(input.startsWith("subscribe")) {
+						if(subscriber == null) {
+							subscriber = session.createDurableSubscriber(topic, SUBSCRIBER_ID);
+							subscriber.setMessageListener(new MessageListener() {
+								public void onMessage(Message inboundMessage) {
+									System.out.println(inboundMessage);
+								}
+							});
+						}
+					} else if(input.startsWith("subscribe")) {
+						if(subscriber != null) {
+							session.unsubscribe(SUBSCRIBER_ID);
+							subscriber = null;
+						}
+					} else {
+						System.out.println("Unknown command: " + input);
+					}
+				}
+			} finally {
+				session.close();
+				conn.close();
+				server.closeServer();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
