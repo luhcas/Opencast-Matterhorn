e98d4fd6c67d9019ae4e771396b948f925067c3f
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-10-28 19:11:39 +0000

    removed core-api-draft
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@2962 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core-api-draft/core-api/src/main/java/org/opencastproject/core/api/model/CoreCapability.java
+++
@@ -1,7 +0,0 @@
-package org.opencastproject.core.api.model;
-
-/**
- * A marker interface identifying some capability provided by the core.  Once
- * these are better defined, this may stop being just a marker interface.
- */
-public interface CoreCapability {}
