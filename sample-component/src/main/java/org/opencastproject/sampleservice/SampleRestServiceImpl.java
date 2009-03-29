2930286c205cfbb7d5d7e884b34520f94fc32a01
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-03-29 22:45:13 +0000

    renamed the sample service bundles for clarity
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@2979 e6edf6fb-f266-4316-afb4-e53d95876a76

--- sample-component/src/main/java/org/opencastproject/sampleservice/SampleRestServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package org.opencastproject.sampleservice;
-
-import javax.ws.rs.QueryParam;
-
-public class SampleRestServiceImpl implements SampleRestService {
-	public String getSomething(@QueryParam("path") String path) {
-		return "sample";
-	}
-}
--- sample-service-impl/src/main/java/org/opencastproject/sampleservice/SampleRestServiceImpl.java
+++ sample-service-impl/src/main/java/org/opencastproject/sampleservice/SampleRestServiceImpl.java
@@ -6,9 +6,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.opencastproject.sampleservice.api.SampleRestService;
+
 @Path("/samplerest")
-public interface SampleRestService {
+public class SampleRestServiceImpl implements SampleRestService {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String getSomething(@QueryParam("path") String path);
+	public String getSomething(@QueryParam("path") String path) {
+		return "sample";
+	}
 }
