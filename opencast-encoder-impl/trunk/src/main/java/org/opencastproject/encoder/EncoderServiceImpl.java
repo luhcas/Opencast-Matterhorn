bfe724f67c0397f1b28891359b7b6bf26291a939
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-04-01 09:49:22 +0000

    added the skeletal osgi stuff for the encoder bundles
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3005 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ opencast-encoder-impl/trunk/src/main/java/org/opencastproject/encoder/EncoderServiceImpl.java
@@ -0,0 +1,12 @@
+package org.opencastproject.encoder;
+
+import org.opencastproject.encoder.api.EncoderService;
+
+public class EncoderServiceImpl implements EncoderService {
+
+  public String encode(String pathIn, String pathOut) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ opencast-encoder-impl/trunk/src/main/java/org/opencastproject/encoder/OsgiActivator.java
@@ -0,0 +1,9 @@
+package org.opencastproject.encoder;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class OsgiActivator implements BundleActivator {
+  public void start(BundleContext context) throws Exception {}
+  public void stop(BundleContext context) throws Exception {}
+}
