858fdd55b52879bdad17c4f88c2afe4bf2604b1b
mketterl <mketterl@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-08-11 17:52:22 +0000

    MH-448 FirstCodeFromOsnabrueck
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3271 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-delivery-service-impl/trunk/src/main/java/org/opencastproject/delivery/impl/DeliveryServiceImpl.java
@@ -0,0 +1,72 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.delivery.impl;
+
+import org.apache.commons.io.IOUtils;
+import org.opencastproject.delivery.api.DeliveryService;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Dictionary;
+
+/**
+ * FIXME -- Add javadocs
+ */
+public class DeliveryServiceImpl implements DeliveryService, ManagedService {
+  private static final Logger logger = LoggerFactory.getLogger(DeliveryServiceImpl.class);
+
+  private String rootDirectory = "/tmp";
+
+  
+  @SuppressWarnings("unchecked")
+  public void updated(Dictionary props) throws ConfigurationException {
+    // Update any configuration properties here
+  }
+
+  public InputStream download(String path) {
+    File f = new File(rootDirectory + File.separator + path);
+    logger.info("Attempting to read file " + f.getAbsolutePath());
+    try {
+      return new FileInputStream(f);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public void upload(InputStream stream, String path)
+  {
+    File f = new File (rootDirectory + File.separator + path);
+    
+    try {
+      FileOutputStream fos = new FileOutputStream(f);
+      IOUtils.copy(stream, fos);
+      IOUtils.closeQuietly(stream);
+      IOUtils.closeQuietly(fos);
+      
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
