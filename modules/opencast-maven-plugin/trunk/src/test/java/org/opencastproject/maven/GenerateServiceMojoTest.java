89b23221cdebe437aa6ee48d4863e68d210157c4
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-08-14 08:10:19 +0000

    MH-556 -- fixed invalid zip, which contained build artifacts
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3294 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-maven-plugin/trunk/src/test/java/org/opencastproject/maven/GenerateServiceMojoTest.java
+++ modules/opencast-maven-plugin/trunk/src/test/java/org/opencastproject/maven/GenerateServiceMojoTest.java
@@ -31,4 +31,23 @@ public class GenerateServiceMojoTest {
     Assert.assertTrue(new File("opencast-my-service-api/src/main/java/org/opencastproject/my/api/MyService.java").exists());
     Assert.assertTrue(new File("opencast-my-service-impl/src/main/java/org/opencastproject/my/impl/MyServiceImpl.java").exists());
   }
+
+  @Test
+  public void testForInvalidApiArtifacts() throws Exception {
+    checkForInvalidArtifacts(new File("opencast-my-service-api"));
+    checkForInvalidArtifacts(new File("opencast-my-service-impl"));
+  }
+
+  public void checkForInvalidArtifacts(File directory) throws Exception {
+    File dotClasspath = new File(directory, ".classpath");
+    File dotProject = new File(directory, ".project");
+    File dotSettings = new File(directory, ".settings");
+    File target = new File(directory, "target");
+    
+    Assert.assertFalse(dotClasspath.exists());
+    Assert.assertFalse(dotProject.exists());
+    Assert.assertFalse(dotSettings.exists());
+    Assert.assertFalse(target.exists());
+  }
+
 }
