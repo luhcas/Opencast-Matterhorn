4e2a372fc4f9bea018f42b9cc5abc97b767feba3
jamiehodge <jamiehodge@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-08-18 07:24:02 +0000

    MH-584 -- import a module for the 'example' service impl
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3309 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ contrib/opencast-example-service-impl/trunk/src/main/java/org/opencastproject/example/impl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Example service implementation classes
+ */
+@javax.xml.bind.annotation.XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED)
+package org.opencastproject.example.impl;
+
+import javax.xml.bind.annotation.XmlNsForm;
