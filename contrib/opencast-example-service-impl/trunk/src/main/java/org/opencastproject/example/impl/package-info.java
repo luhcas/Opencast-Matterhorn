0d63d7b3a46e1f29912450c28254babf4d058de3
jamiehodge <jamiehodge@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-01 12:38:06 +0000

    MH-584 Remove Example Service Modules
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3359 e6edf6fb-f266-4316-afb4-e53d95876a76

--- contrib/opencast-example-service-impl/trunk/src/main/java/org/opencastproject/example/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Example service implementation classes
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED)
-package org.opencastproject.example.impl;
-
-import javax.xml.bind.annotation.XmlNsForm;
