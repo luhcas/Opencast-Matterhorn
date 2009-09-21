1e32941bdbab45e4d22c3b805361b402865e50ff
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-21 10:17:07 +0000

    MH-637 - updating the start script to work around the xml parsing problem on Java 1.6
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3460 e6edf6fb-f266-4316-afb4-e53d95876a76

--- products/matterhorn/trunk/docs/start_matterhorn.sh
+++ products/matterhorn/trunk/docs/start_matterhorn.sh
@@ -9,5 +9,5 @@ M2_REPO='/Users/markusketterl/.m2/repository'
 ##
 rm -rf $FELIX/felix-cache/*
 cd $FELIX
-java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=9191,server=y,suspend=n -DM2_REPO=$M2_REPO -Dfelix.fileinstall.dir=$FELIX/load -jar $FELIX/bin/felix.jar $FELIX/felix-cache
+java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=9191,server=y,suspend=n -Djavax.xml.stream.XMLInputFactory=com.ctc.wstx.stax.WstxInputFactory -Djavax.xml.stream.XMLOutputFactory=com.ctc.wstx.stax.WstxOutputFactory -Djavax.xml.stream.XMLEventFactory=com.ctc.wstx.stax.WstxEventFactory -DM2_REPO=$M2_REPO -Dfelix.fileinstall.dir=$FELIX/load -jar $FELIX/bin/felix.jar $FELIX/felix-cache
 
