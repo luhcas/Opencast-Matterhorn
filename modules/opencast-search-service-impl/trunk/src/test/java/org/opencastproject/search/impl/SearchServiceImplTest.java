56fa13a6309e52fe19b52ec5f568556dc1c9142c
twunden <twunden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-22 11:09:09 +0000

    Added an initial test suite.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3473 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-search-service-impl/trunk/src/test/java/org/opencastproject/search/impl/SearchServiceImplTest.java
+++ modules/opencast-search-service-impl/trunk/src/test/java/org/opencastproject/search/impl/SearchServiceImplTest.java
@@ -13,30 +13,89 @@
  *  permissions and limitations under the License.
  *
  */
+
 package org.opencastproject.search.impl;
 
-import org.opencastproject.search.api.SearchService;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
 
-import junit.framework.Assert;
+import org.opencastproject.media.mediapackage.DefaultMediaPackageSerializerImpl;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageBuilder;
+import org.opencastproject.media.mediapackage.MediaPackageBuilderFactory;
+import org.opencastproject.media.mediapackage.MediaPackageException;
+import org.opencastproject.search.api.SearchResult;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Tests the functionality  of the search service.
+ */
 public class SearchServiceImplTest {
-  private SearchService service = null;
+
+  /** The search service */
+  private SearchServiceImpl service = null;
+
+  /** The solr root directory */
+  private String solrRoot = System.getProperty("java.io.tmpdir") + File.separator + "opencast" + File.separator + "searchindex";
+    
   @Before
   public void setup() {
-    service = new SearchServiceImpl();
+    service = new SearchServiceImpl(solrRoot);
+    service.activate(null);
   }
 
   @After
   public void teardown() {
-    service = null;
+    try {
+      FileUtils.deleteDirectory(new File(solrRoot));
+    } catch (IOException e) {
+      fail("Error on cleanup");
+    }
   }
   
+  /**
+   * Test wether an empty search index will work.
+   */
   @Test
-  public void testGetEntity() {
-    Assert.assertNotNull(service);
+  public void testEmptySearchIndex() {
+    SearchResult result = service.getEpisodeAndSeriesById("foo");
+    assertEquals(0, result.size());
   }
+  
+  /**
+   * Ads a simple media package that has a dublin core for the episode only.
+   */
+  @Test
+  public void testAddSimpleMediaPackage() {
+    MediaPackageBuilderFactory builderFactory = MediaPackageBuilderFactory.newInstance();
+    MediaPackageBuilder mediaPackageBuilder = builderFactory.newMediaPackageBuilder();
+    URL rootUrl = SearchServiceImplTest.class.getResource("/");
+    mediaPackageBuilder.setSerializer(new DefaultMediaPackageSerializerImpl(rootUrl));
+    
+    // Load the simple media package
+    MediaPackage mediaPackage = null;
+    try {
+      InputStream is = SearchServiceImplTest.class.getResourceAsStream("/manifest-simple.xml");
+      mediaPackage = mediaPackageBuilder.loadFromManifest(is);
+    } catch (MediaPackageException e) {
+      fail("Error loading simple media package");
+    }
+
+    // Add the media package to the search index
+    service.add(mediaPackage);
+    
+    // Make sure it's properly indexed and returned
+    assertEquals(1, service.getEpisodeById("00000/1").size());
+    assertEquals(1, service.getEpisodesByDate(0, Integer.MAX_VALUE).size());
+  }
+  
 }
