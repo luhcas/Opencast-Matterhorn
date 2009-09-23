bbcc4eb52c9885d0adf80f057d332a42897810da
twunden <twunden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-23 04:05:39 +0000

    Tried to fix the windows start script as well. Good luck...
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3483 e6edf6fb-f266-4316-afb4-e53d95876a76

--- products/matterhorn/trunk/docs/felix/bin/start_matterhorn.bat
+++ products/matterhorn/trunk/docs/felix/bin/start_matterhorn.bat
@@ -4,12 +4,29 @@ REM ##
 
 SET FELIX=E:/Libraries/felix-1.8.0
 SET M2_REPO=C:/Users/cab938/.m2/repository
+SET DEBUG_PORT=8000
+SET DEBUG_SUSPEND=n
 
 REM ##
-REM # Only change the line below if you want to customize the server
+REM # Only change the lines below if you know what you are doing
 REM ##
 
+SET MAVEN_OPTS=-DM2_REPO=%M2_REPO%
+SET FELIX_FILEINSTALL_OPTS=-Dfelix.fileinstall.dir=%FELIX%/load
+SET PAX_CONFMAN_OPTS=-Dbundles.configuration.location=%FELIX%/conf
+SET CXF_OPTS=-Djava.util.logging.config.file=%FELIX%/conf/cxf.properties
+
+REM # Clear the felix cache directory
+SET FELIX_CACHE=%FELIX%/felix-cache
+rm -rf %FELIX_CACHE%
+
+REM # Create the debug config
+SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PORT%,server=y,suspend=%DEBUG_SUSPEND%
+
+REM # For Java 6, you need some minor xml facility configuration
+SET XML_OPTS=-Djavax.xml.stream.XMLInputFactory=com.ctc.wstx.stax.WstxInputFactory -Djavax.xml.stream.XMLOutputFactory=com.ctc.wstx.stax.WstxOutputFactory -Djavax.xml.stream.XMLEventFactory=com.ctc.wstx.stax.WstxEventFactory
+
+REM # Finally start felix
 rm -rf %FELIX%/felix-cache/*
 cd %FELIX%
-java -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=9191,server=y,suspend=n -DM2_REPO=%M2_REPO% -Dfelix.fileinstall.dir=%FELIX%/load -jar %FELIX%/bin/felix.jar %FELIX%/felix-cache
-
+java %DEBUG_OPTS% %XML_OPTS% %MAVEN_OPTS% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %CXF_OPTS% -jar %FELIX%/bin/felix.jar %FELIX_CACHE%
