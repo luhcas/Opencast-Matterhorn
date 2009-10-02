661b1651b5e97e6f0f487fe82180ea1df03e0749
cab938 <cab938@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-02 22:20:56 +0000

    Clarified to work with start_matterhorn.sh
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3518 e6edf6fb-f266-4316-afb4-e53d95876a76

--- products/matterhorn/trunk/docs/felix/bin/start_matterhorn.bat
+++ products/matterhorn/trunk/docs/felix/bin/start_matterhorn.bat
@@ -1,9 +1,12 @@
 REM ##
 REM # Configure these variables to match your environment
+REM # If you have system-wide variables for FELIX_HOME and M2_REPO then you
+REM # should not have to make any changes to this file.
 REM ##
 
-SET FELIX=E:/Libraries/felix-1.8.0
-SET M2_REPO=C:/Users/cab938/.m2/repository
+REM # SET FELIX_HOME=E:\Libraries\felix-framework-2.0.0
+REM # SET M2_REPO=C:/Users/cab938/.m2/repository
+
 SET DEBUG_PORT=8000
 SET DEBUG_SUSPEND=n
 
@@ -11,23 +14,17 @@ REM ##
 REM # Only change the lines below if you know what you are doing
 REM ##
 
-SET MAVEN_OPTS=-DM2_REPO=%M2_REPO%
-SET FELIX_FILEINSTALL_OPTS=-Dfelix.fileinstall.dir=%FELIX%/load
-SET PAX_CONFMAN_OPTS=-Dbundles.configuration.location=%FELIX%/conf
-SET CXF_OPTS=-Djava.util.logging.config.file=%FELIX%/conf/cxf.properties
+SET FELIX_FILEINSTALL_OPTS=-Dfelix.fileinstall.dir=%FELIX_HOME%/load
+SET PAX_CONFMAN_OPTS=-Dbundles.configuration.location=%FELIX_HOME%/conf
+SET CXF_OPTS=-Djava.util.logging.config.file=%FELIX_HOME%/conf/cxf.properties
+SET M2_REPO=C:\Users\cab938\.m2\repository
+SET FELIX_CACHE=%FELIX_HOME%/felix-cache
 
-REM # Clear the felix cache directory
-SET FELIX_CACHE=%FELIX%/felix-cache
-rm -rf %FELIX_CACHE%
+REM # Clear felix cache dir
+del /F %FELIX_CACHE%
 
 REM # Create the debug config
 SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PORT%,server=y,suspend=%DEBUG_SUSPEND%
 
-REM # For Java 6, you need some minor xml facility configuration
-REM No longer needed for CXF post 01 Oct
-REM SET XML_OPTS=-Djavax.xml.stream.XMLInputFactory=com.ctc.wstx.stax.WstxInputFactory -Djavax.xml.stream.XMLOutputFactory=com.ctc.wstx.stax.WstxOutputFactory -Djavax.xml.stream.XMLEventFactory=com.ctc.wstx.stax.WstxEventFactory
-
 REM # Finally start felix
-rm -rf %FELIX%/felix-cache/*
-cd %FELIX%
-java %DEBUG_OPTS% %MAVEN_OPTS% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %CXF_OPTS% -jar %FELIX%/bin/felix.jar %FELIX_CACHE%
+java %DEBUG_OPTS% -DM2_REPO=%M2_REPO% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %CXF_OPTS% -jar %FELIX_HOME%/bin/felix.jar %FELIX_CACHE%  
