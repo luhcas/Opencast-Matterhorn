87bd33770c72b683b87f4c42aa363009098ae262
cab938 <cab938@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-07 16:57:00 +0000

    Delete of felix cache is now quiet
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3538 e6edf6fb-f266-4316-afb4-e53d95876a76

--- products/matterhorn/trunk/docs/felix/bin/start_matterhorn.bat
+++ products/matterhorn/trunk/docs/felix/bin/start_matterhorn.bat
@@ -1,12 +1,13 @@
+SETLOCAL
 REM ##
 REM # Configure these variables to match your environment
 REM # If you have system-wide variables for FELIX_HOME and M2_REPO then you
 REM # should not have to make any changes to this file.
 REM ##
 
-# Make sure the following two path entries do *not* contain spaces
-SET FELIX_HOME=C:\Libraries\felix-framework-2.0.0
-SET M2_REPO=C:\Users\johndoe\.m2\repository
+REM # Make sure the following two path entries do *not* contain spaces
+REM # SET FELIX_HOME=C:\Libraries\felix-framework-2.0.0
+REM # SET M2_REPO=C:\Users\johndoe\.m2\repository
 
 SET DEBUG_PORT=8000
 SET DEBUG_SUSPEND=n
@@ -21,10 +22,11 @@ SET CXF_OPTS=-Djava.util.logging.config.file=%FELIX_HOME%\conf\cxf.properties
 SET FELIX_CACHE=%FELIX_HOME%\felix-cache
 
 REM # Clear felix cache dir
-del \F %FELIX_CACHE%
+del /FQ %FELIX_CACHE%
 
 REM # Create the debug config
 SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PORT%,server=y,suspend=%DEBUG_SUSPEND%
 
 REM # Finally start felix
 java %DEBUG_OPTS% -DM2_REPO=%M2_REPO% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %CXF_OPTS% -jar %FELIX_HOME%\bin\felix.jar %FELIX_CACHE%  
+ENDLOCAL
