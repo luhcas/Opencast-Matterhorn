d96c8c462d451ca99cdaf86d0a9c540d00a9e94f
twunden <twunden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-15 10:04:39 +0000

    MH-1173, MH-1189 - Added convenience methods to access/modify properties. Added means of inserting and appending additional workflow operations.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3585 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-workflow-service-api/trunk/src/main/java/org/opencastproject/workflow/api/WorkflowInstance.java
+++ modules/opencast-workflow-service-api/trunk/src/main/java/org/opencastproject/workflow/api/WorkflowInstance.java
@@ -17,19 +17,22 @@ package org.opencastproject.workflow.api;
 
 import org.opencastproject.media.mediapackage.MediaPackage;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * An single instance of a running, paused, or stopped workflow.
  */
 public interface WorkflowInstance {
-  public enum State {RUNNING, STOPPED, PAUSED}
-  
+  public enum State {
+    RUNNING, STOPPED, PAUSED
+  }
+
   /**
    * The unique ID of this {@link WorkflowInstance}
    */
   String getId();
-  
+
   /**
    * The short title of this {@link WorkflowInstance}
    */
@@ -47,19 +50,96 @@ public interface WorkflowInstance {
   public WorkflowDefinition getWorkflowDefinition();
 
   /**
+   * Returns the {@link WorkflowOperation}s that make up this workflow.
+   * 
+   * @return the workflow operations
+   */
+  public WorkflowOperation[] getWorkflowOperations();
+
+  /**
+   * Returns the {@link WorkflowOperation} that is currently being executed.
+   * 
+   * @return the current operation
+   */
+  public WorkflowOperation getCurrentOperation();
+
+  /**
+   * Appends a {@link WorkflowOperation} to the end of the list of operations.
+   * 
+   * @param operation
+   *          the operation to append
+   */
+  public void addWorkflowOperation(WorkflowOperation operation);
+
+  /**
+   * Inserts a {@link WorkflowOperation}s at index <code>location</code> into the list of workflow operations.
+   * 
+   * @param location
+   *          index into the list of operations
+   * @param operation
+   *          the operation to append
+   */
+  public void addWorkflowOperation(int location, WorkflowOperation operation);
+
+  /**
+   * Appends a list of {@link WorkflowOperation} to the end of the list of operations.
+   * 
+   * @param operations
+   *          the operations to append
+   */
+  public void addWorkflowOperations(List<WorkflowOperation> operations);
+
+  /**
+   * Inserts a list of {@link WorkflowOperation}s at index <code>location</code> into the list of workflow operations.
+   * 
+   * @param location
+   *          index into the list of operations
+   * @param operations
+   *          the operations to append
+   */
+  public void addWorkflowOperations(int location, List<WorkflowOperation> operations);
+
+  /**
    * The current {@link State} of this {@link WorkflowInstance}
    */
   public State getState();
-  
+
   /**
    * The {@link MediaPackage} associated with this {@link WorkflowInstance}
    */
   public MediaPackage getMediaPackage();
-  
+
   /**
-   * The properties associated with this workflow instance.  Properties can be used to affect how
+   * The properties associated with this workflow instance. Properties can be used to affect how
    * {@link WorkflowDefinition#getOperations()} are eventually run.
    */
   public Map<String, String> getProperties();
-}
 
+  /**
+   * Returns the value of property <code>name</code> or <code>null</code> if no such property has been set.
+   * 
+   * @param name
+   *          the property name
+   * @return the property value
+   */
+  public String getProperty(String name);
+
+  /**
+   * Sets the property with name <code>name</code> to value <code>value</code>.
+   * 
+   * @param name
+   *          the property name
+   * @param value
+   *          the property value
+   */
+  public void setProperty(String name, String value);
+
+  /**
+   * Removes the property with name <code>name</code> from the list of defined properties.
+   * 
+   * @param name
+   *          the property name
+   */
+  public void removeProperty(String name);
+
+}
