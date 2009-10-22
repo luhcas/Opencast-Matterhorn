f098ff6a22f4219fe8b9142e999f19ce88343083
twunden <twunden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-22 17:08:55 +0000

    MH-1221 - opencast-conductor patch from Nejc.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3623 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-conductor/trunk/src/main/java/org/opencastproject/conductor/impl/ConductorServiceImpl.java
+++ modules/opencast-conductor/trunk/src/main/java/org/opencastproject/conductor/impl/ConductorServiceImpl.java
@@ -16,124 +16,166 @@
 package org.opencastproject.conductor.impl;
 
 import org.opencastproject.conductor.api.ConductorService;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageBuilderFactory;
 import org.opencastproject.workflow.api.WorkflowBuilder;
 import org.opencastproject.workflow.api.WorkflowService;
 
+import org.apache.commons.io.IOUtils;
 import org.osgi.service.component.ComponentContext;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
+import java.util.HashMap;
 
 /**
  * This is the default implementation of the conductor service.
  */
-public class ConductorServiceImpl implements ConductorService {
+public class ConductorServiceImpl implements ConductorService, EventHandler {
   private static final Logger logger = LoggerFactory.getLogger(ConductorServiceImpl.class);
 
   WorkflowService workflowService = null;
+
   public void setWorkflowService(WorkflowService workflowService) {
     this.workflowService = workflowService;
   }
-  
+
   public void activate(ComponentContext componentContext) {
     logger.info("init() loading default workflow definitions");
     try {
-      InputStream distOnly = ConductorServiceImpl.class.getClassLoader().getResourceAsStream("/workflows/distribute-only.xml");
+      InputStream review = ConductorServiceImpl.class.getClassLoader().getResourceAsStream("/workflows/review.xml");
+      workflowService.registerWorkflowDefinition(WorkflowBuilder.getInstance().parseWorkflowDefinition(review));
+
+      InputStream distOnly = ConductorServiceImpl.class.getClassLoader().getResourceAsStream(
+              "/workflows/distribute-only.xml");
       workflowService.registerWorkflowDefinition(WorkflowBuilder.getInstance().parseWorkflowDefinition(distOnly));
 
-      InputStream composeAndDist = ConductorServiceImpl.class.getClassLoader().getResourceAsStream("/workflows/compose-and-distribute.xml");
+      InputStream composeAndDist = ConductorServiceImpl.class.getClassLoader().getResourceAsStream(
+              "/workflows/compose-and-distribute.xml");
       workflowService.registerWorkflowDefinition(WorkflowBuilder.getInstance().parseWorkflowDefinition(composeAndDist));
 
-      InputStream errorHandler = ConductorServiceImpl.class.getClassLoader().getResourceAsStream("/workflows/default-error-handler.xml");
+      InputStream errorHandler = ConductorServiceImpl.class.getClassLoader().getResourceAsStream(
+              "/workflows/default-error-handler.xml");
       workflowService.registerWorkflowDefinition(WorkflowBuilder.getInstance().parseWorkflowDefinition(errorHandler));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  
-//  /**
-//   * {@inheritDoc}
-//   * 
-//   * @see org.opencastproject.conductor.api.ConductorService#getWorkflowDefinitions()
-//   */
-//  public WorkflowDefinitionList getWorkflowDefinitions() {
-//    WorkflowDefinitionListImpl list = new WorkflowDefinitionListImpl();
-//    for(WorkflowDefinition def : defs.values()) {
-//      if(allOperationsAvailable(def)) {
-//        list.getWorkflowDefinition().add(def);
-//      } else {
-//        logger.warn("Workflow definition " + def.getTitle() + " is unavailable due to missing operations");
-//      }
-//    }
-//    return list;
-//  }
-//
-//  protected boolean allOperationsAvailable(WorkflowDefinition def) {
-//    List<String> availableOperationNames = getOperationNames();
-//    for(WorkflowOperationDefinition op : def.getOperations().getOperation()) {
-//      if( ! availableOperationNames.contains(op.getName())) {
-//        return false;
-//      }
-//    }
-//    return true;
-//  }
-//  
-//  protected List<String> getOperationNames() {
-//    List<String> list = new ArrayList<String>();
-//    try {
-//      for(ServiceReference ref : componentContext.getBundleContext().getServiceReferences(WorkflowOperationHandler.class.getName(), null)) {
-//        WorkflowOperationHandler handler = (WorkflowOperationHandler) componentContext.getBundleContext().getService(ref);
-//        list.addAll(Arrays.asList(handler.getOperationsToHandle()));
-//      }
-//    } catch (InvalidSyntaxException e) {
-//      logger.warn(e.getMessage());
-//    }
-//    return list;
-//  }
-//  
-//  /**
-//   * {@inheritDoc}
-//   * 
-//   * @see org.opencastproject.conductor.api.ConductorService#getWorkflowDefinitionByTitle(java.lang.String)
-//   */
-//  public WorkflowDefinition getWorkflowDefinitionByTitle(String name) {
-//    WorkflowDefinition def = defs.get(name);
-//    if(def == null) {
-//      logger.warn("Workflow definition " + name + " is not installed");
-//      return null;
-//    }
-//    if(allOperationsAvailable(def)) {
-//      return def;
-//    } else {
-//      logger.warn("Workflow definition " + name + " is unavailable due to missing operations");
-//      return null;
-//    }
-//  }
-//
-//  /**
-//   * {@inheritDoc}
-//   * @see org.opencastproject.conductor.api.ConductorService#addWorkflowDefinition(org.opencastproject.workflow.api.WorkflowDefinition)
-//   */
-//  public void addWorkflowDefinition(WorkflowDefinition def) {
-//    defs.put(def.getTitle(), def);
-//  }
-//
-//  /**
-//   * {@inheritDoc}
-//   * @see org.opencastproject.conductor.api.ConductorService#removeWorkflowDefinition(java.lang.String)
-//   */
-//  public WorkflowDefinition removeWorkflowDefinition(String title) {
-//    for(Iterator<Entry<String, WorkflowDefinition>> iter = defs.entrySet().iterator(); iter.hasNext();) {
-//      Entry<String, WorkflowDefinition> entry = iter.next();
-//      WorkflowDefinition def = entry.getValue();
-//      if(title.equals(def.getTitle())) {
-//        iter.remove();
-//        return def;
-//      }
-//    }
-//    return null;
-//  }
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.osgi.service.event.EventHandler#handleEvent(org.osgi.service.event.Event)
+   */
+  public void handleEvent(Event event) {
+    logger.info("Received event");
+
+    Object property = event.getProperty("mediaPackage");
+    if (property == null || !(property instanceof String)) {
+      // received event, but without or invalid media package
+      logger.error("Property 'mediaPackage' not present or is invalid");
+    } else {
+      try {
+        MediaPackage mp = MediaPackageBuilderFactory.newInstance().newMediaPackageBuilder().loadFromManifest(
+                IOUtils.toInputStream((String) property));
+        logger.info("Received media package " + mp.getIdentifier().getLocalName());
+        // execute 'review' workflow
+        workflowService.start(workflowService.getWorkflowDefinitionByName("Review"), mp, new HashMap<String, String>());
+      } catch (Exception e) {
+        // invalid media package manifest or problem with workflow service
+        throw new RuntimeException(e);
+      }
+
+    }
+
+  }
+
+  // /**
+  // * {@inheritDoc}
+  // *
+  // * @see org.opencastproject.conductor.api.ConductorService#getWorkflowDefinitions()
+  // */
+  // public WorkflowDefinitionList getWorkflowDefinitions() {
+  // WorkflowDefinitionListImpl list = new WorkflowDefinitionListImpl();
+  // for(WorkflowDefinition def : defs.values()) {
+  // if(allOperationsAvailable(def)) {
+  // list.getWorkflowDefinition().add(def);
+  // } else {
+  // logger.warn("Workflow definition " + def.getTitle() + " is unavailable due to missing operations");
+  // }
+  // }
+  // return list;
+  // }
+  //
+  // protected boolean allOperationsAvailable(WorkflowDefinition def) {
+  // List<String> availableOperationNames = getOperationNames();
+  // for(WorkflowOperationDefinition op : def.getOperations().getOperation()) {
+  // if( ! availableOperationNames.contains(op.getName())) {
+  // return false;
+  // }
+  // }
+  // return true;
+  // }
+  //  
+  // protected List<String> getOperationNames() {
+  // List<String> list = new ArrayList<String>();
+  // try {
+  // for(ServiceReference ref :
+  // componentContext.getBundleContext().getServiceReferences(WorkflowOperationHandler.class.getName(), null)) {
+  // WorkflowOperationHandler handler = (WorkflowOperationHandler) componentContext.getBundleContext().getService(ref);
+  // list.addAll(Arrays.asList(handler.getOperationsToHandle()));
+  // }
+  // } catch (InvalidSyntaxException e) {
+  // logger.warn(e.getMessage());
+  // }
+  // return list;
+  // }
+  //  
+  // /**
+  // * {@inheritDoc}
+  // *
+  // * @see org.opencastproject.conductor.api.ConductorService#getWorkflowDefinitionByTitle(java.lang.String)
+  // */
+  // public WorkflowDefinition getWorkflowDefinitionByTitle(String name) {
+  // WorkflowDefinition def = defs.get(name);
+  // if(def == null) {
+  // logger.warn("Workflow definition " + name + " is not installed");
+  // return null;
+  // }
+  // if(allOperationsAvailable(def)) {
+  // return def;
+  // } else {
+  // logger.warn("Workflow definition " + name + " is unavailable due to missing operations");
+  // return null;
+  // }
+  // }
+  //
+  // /**
+  // * {@inheritDoc}
+  // * @see
+  // org.opencastproject.conductor.api.ConductorService#addWorkflowDefinition(org.opencastproject.workflow.api.WorkflowDefinition)
+  // */
+  // public void addWorkflowDefinition(WorkflowDefinition def) {
+  // defs.put(def.getTitle(), def);
+  // }
+  //
+  // /**
+  // * {@inheritDoc}
+  // * @see org.opencastproject.conductor.api.ConductorService#removeWorkflowDefinition(java.lang.String)
+  // */
+  // public WorkflowDefinition removeWorkflowDefinition(String title) {
+  // for(Iterator<Entry<String, WorkflowDefinition>> iter = defs.entrySet().iterator(); iter.hasNext();) {
+  // Entry<String, WorkflowDefinition> entry = iter.next();
+  // WorkflowDefinition def = entry.getValue();
+  // if(title.equals(def.getTitle())) {
+  // iter.remove();
+  // return def;
+  // }
+  // }
+  // return null;
+  // }
 
 }
