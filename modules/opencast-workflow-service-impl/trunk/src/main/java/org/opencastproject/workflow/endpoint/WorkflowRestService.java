37f62c2bf8d71faa65549903e9c9914e0c55524b
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-06 23:21:51 +0000

    MH-1454 -- switched from stringbuilder-based json generation to json-simple.  This has the benefit of being both cleaner to code escaping illegal characters in the json output
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3760 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-workflow-service-impl/trunk/src/main/java/org/opencastproject/workflow/endpoint/WorkflowRestService.java
+++ modules/opencast-workflow-service-impl/trunk/src/main/java/org/opencastproject/workflow/endpoint/WorkflowRestService.java
@@ -31,7 +31,6 @@ import org.opencastproject.workflow.api.WorkflowInstance;
 import org.opencastproject.workflow.api.WorkflowInstanceImpl;
 import org.opencastproject.workflow.api.WorkflowInstanceListImpl;
 import org.opencastproject.workflow.api.WorkflowOperationDefinition;
-import org.opencastproject.workflow.api.WorkflowOperationDefinitionList;
 import org.opencastproject.workflow.api.WorkflowOperationInstance;
 import org.opencastproject.workflow.api.WorkflowOperationInstanceList;
 import org.opencastproject.workflow.api.WorkflowService;
@@ -39,13 +38,14 @@ import org.opencastproject.workflow.api.WorkflowSet;
 import org.opencastproject.workflow.api.WorkflowInstance.State;
 
 import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -78,20 +78,19 @@ public class WorkflowRestService {
     this.service = null;
   }
 
+  @SuppressWarnings("unchecked")
   @GET
   @Path("definitions.{output:.*}")
   public Response getWorkflowDefinitions(@PathParam("output") String output) throws Exception {
     WorkflowDefinitionList list = service.listAvailableWorkflowDefinitions();
     if("json".equals(output)) {
-      StringBuilder sb = new StringBuilder("{\"workflow_definitions\" : [\n");
-      for(int i=0; i < list.size(); i++) {
-        WorkflowDefinition definition = list.get(i);
-        appendWorkflowDefinition(sb, definition);
-        if(i < list.size() - 1) sb.append(",");
-        sb.append("\n");
+      List<JSONObject> jsonDefs = new ArrayList<JSONObject>();
+      for(WorkflowDefinition definition : list) {
+        jsonDefs.add(getWorkflowDefinitionAsJson(definition));
       }
-      sb.append("]}");
-      return Response.ok(sb.toString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
+      JSONObject json = new JSONObject();
+      json.put("workflow_definitions", jsonDefs);
+      return Response.ok(json.toJSONString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
     } else {
       return Response.ok(WorkflowBuilder.getInstance().toXml(list)).header("Content-Type", MediaType.TEXT_XML).build();
     }
@@ -101,45 +100,26 @@ public class WorkflowRestService {
    * @param sb
    * @param definition
    */
-  protected void appendWorkflowDefinition(StringBuilder sb, WorkflowDefinition definition) {
-    sb.append(" { \"title\" : \"");
-    sb.append(definition.getTitle());
-    sb.append("\",\n");
-
-    sb.append("   \"description\" : \"");
-    sb.append(definition.getDescription());
-    sb.append("\",\n");
-    
-    sb.append("   \"operations\" : [\n");
-
-    WorkflowOperationDefinitionList operations = definition.getOperations();
-    for(int i=0; i < operations.size(); i++) {
-      WorkflowOperationDefinition op = operations.get(i);
-      sb.append("    {\"name\" : \"");
-      sb.append(op.getName());
-      sb.append("\", \"description\" : \"");
-      sb.append(op.getDescription());
-      sb.append("\", \"exception_handler_workflow\" : ");
-      String exceptionWorkflow = op.getExceptionHandlingWorkflow();
-      if(exceptionWorkflow == null) {
-        sb.append("null");
-      } else {
-        sb.append("\"");
-        sb.append(exceptionWorkflow);
-        sb.append("\"");
-      }
-      sb.append(", \"fail_on_error\" : ");
-      sb.append(op.isFailWorkflowOnException());
-      sb.append("}");
-      if(i < operations.size() - 1) sb.append(",");
-      sb.append("\n");
+  @SuppressWarnings("unchecked")
+  protected JSONObject getWorkflowDefinitionAsJson(WorkflowDefinition definition) {
+    JSONObject json = new JSONObject();
+    json.put("title", definition.getTitle());
+    json.put("description", definition.getDescription());
+    List<JSONObject> opList = new ArrayList<JSONObject>();
+    for(WorkflowOperationDefinition operationDefinition : definition.getOperations()) {
+      JSONObject op = new JSONObject();
+      op.put("name",operationDefinition.getName());
+      op.put("description",operationDefinition.getDescription());
+      op.put("exception_handler_workflow",operationDefinition.getExceptionHandlingWorkflow());
+      op.put("fail_on_error",operationDefinition.isFailWorkflowOnException());
+      opList.add(op);
     }
-    
-    sb.append("   ]\n");
-    sb.append(" }");
+    json.put("operations", opList);
+    return json;
   }
 
   
+  @SuppressWarnings("unchecked")
   @GET
   @Path("instances.{output:.*}")
   public Response getWorkflows(
@@ -162,15 +142,11 @@ public class WorkflowRestService {
     }
     
     if("json".equals(output)) {
-      StringBuilder sb = new StringBuilder("{\"workflows\" : [\n");
-      for(int i=0; i < set.getItems().length; i++) {
-        WorkflowInstance workflow = set.getItems()[i];
-        appendWorkflowInstance(sb, workflow, false);
-        if(i < set.getItems().length - 1) sb.append(",");
-        sb.append("\n");
+      JSONArray json = new JSONArray();
+      for(WorkflowInstance workflow : set.getItems()) {
+        json.add(getWorkflowInstanceAsJson(workflow, false));
       }
-      sb.append("]}");
-      return Response.ok(sb.toString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
+      return Response.ok(json.toJSONString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
     } else {
       WorkflowInstanceListImpl list = new WorkflowInstanceListImpl();
       for(WorkflowInstance instance : set.getItems()) {
@@ -188,9 +164,7 @@ public class WorkflowRestService {
           @PathParam("output") String output) throws Exception {
     WorkflowInstance instance = service.getWorkflowById(id);
     if("json".equals(output)) {
-      StringBuilder sb = new StringBuilder();
-      appendWorkflowInstance(sb, instance, true);
-      return Response.ok(sb.toString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
+      return Response.ok(getWorkflowInstanceAsJson(instance, true).toString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
     } else {
       return Response.ok(WorkflowBuilder.getInstance().toXml(instance))
         .header("Content-Type", MediaType.TEXT_XML).build();
@@ -245,84 +219,57 @@ public class WorkflowRestService {
     return Response.ok("resumed " + workflowInstanceId).build();
   }
 
-  protected void appendWorkflowInstance(StringBuilder sb, WorkflowInstance workflow, boolean includeDublinCoreFields) {
+  @SuppressWarnings("unchecked")
+  protected JSONObject getWorkflowInstanceAsJson(WorkflowInstance workflow, boolean includeDublinCoreFields) {
     String mediaPackageTitle = getDublinCoreProperty(getDublinCore(workflow.getSourceMediaPackage()),
             DublinCoreCatalog.PROPERTY_TITLE);
 
-    sb.append(" { \"workflow_id\" : \"");
-    sb.append(workflow.getId());
-    sb.append("\",\n");
-
-    sb.append("   \"workflow_title\" : \"");
-    sb.append(workflow.getTitle());
-    sb.append("\",\n");
-    
-    sb.append("   \"workflow_current_operation\" : \"");
-    sb.append(workflow.getCurrentOperation().getName());
-    sb.append("\",\n");
-
-    sb.append("   \"workflow_state\" : \"");
-    sb.append(workflow.getState().name().toLowerCase());
-    sb.append("\",\n");
-
+    JSONObject jsInstance = new JSONObject();
+    jsInstance.put("workflow_id", workflow.getId());
+    jsInstance.put("workflow_title", workflow.getTitle());
+    jsInstance.put("workflow_current_operation", workflow.getCurrentOperation().getName());
+    jsInstance.put("workflow_state", workflow.getState().name().toLowerCase());
     Set<WorkflowConfiguration> configs = workflow.getConfigurations();
-    appendConfigs(sb, configs, true);
-    sb.append("   \"workflow_operations\" : [\n");
+    jsInstance.put("configuration", getConfigsAsJson(configs));
     WorkflowOperationInstanceList operations = workflow.getWorkflowOperationInstanceList();
-    for(int i=0; i < operations.size(); i++) {
-      WorkflowOperationInstance op = operations.get(i);
-      sb.append("     {\"name\" : \"");
-      sb.append(op.getName());
-      sb.append("\", \"description\" : \"");
-      sb.append(op.getDescription());
-      sb.append("\", \"state\" : \"");
-      sb.append(op.getState().name().toLowerCase());
-      sb.append("\"}");
-      if(i < operations.size() - 1) sb.append(",");
-      sb.append("\n");
-    }
-    sb.append("]\n");
-
+    jsInstance.put("operations", getOperationsAsJson(operations));
     if(includeDublinCoreFields) {
-      DublinCoreCatalog dc = getDublinCore(workflow.getSourceMediaPackage());
+      DublinCoreCatalog dc = getDublinCore(workflow.getCurrentMediaPackage());
       List<EName> props = new ArrayList<EName>(dc.getProperties());
       for(int i=0; i<props.size(); i++) {
-        sb.append(",\n");
-        sb.append("   \"mediapackage_" + props.get(i).getLocalName().toLowerCase() + "\" : ");
-        String value = dc.getFirst(props.get(i));
-        if(value == null) {
-          sb.append("null");
-        } else {
-          sb.append("\"");
-          sb.append(value);
-          sb.append("\"");
-        }
+        jsInstance.put("mediapackage_" + props.get(i).getLocalName().toLowerCase(), dc.getFirst(props.get(i)));
       }
     } else {
-      sb.append(",\n   \"mediapackage_title\" : \"");
-      sb.append(mediaPackageTitle);
-      sb.append("\"");
+      jsInstance.put("mediapackage_title", mediaPackageTitle);
     }
-
-    sb.append("\n }");
+    return jsInstance;
   }
 
-  protected void appendConfigs(StringBuilder sb, Set<WorkflowConfiguration> configs, boolean moreElements) {
-    if(configs != null) {
-      sb.append("   \"configuration\" : [\n");
-      for(Iterator<WorkflowConfiguration> iter = configs.iterator(); iter.hasNext();) {
-        WorkflowConfiguration config = iter.next();
-        sb.append("     {\"");
-        sb.append(config.getKey());
-        sb.append("\" : \"");
-        sb.append(config.getValue());
-        sb.append("\"}");
-        if(iter.hasNext()) sb.append(",");
-        sb.append("\n");
-      }
-      sb.append("   ]");
+  @SuppressWarnings("unchecked")
+  protected JSONArray getOperationsAsJson(WorkflowOperationInstanceList operations) {
+    JSONArray jsonArray = new JSONArray();
+    for(WorkflowOperationInstance op : operations) {
+      JSONObject jsOp = new JSONObject();
+      jsOp.put("name", op.getName());
+      jsOp.put("description", op.getDescription());
+      jsOp.put("state", op.getState().name().toLowerCase());
+      jsOp.put("configurations", getConfigsAsJson(op.getConfigurations()));
+      jsonArray.add(jsOp);
     }
-    if(moreElements) sb.append(",\n");
+    return jsonArray;
+  }
+
+  @SuppressWarnings("unchecked")
+  protected JSONArray getConfigsAsJson(Set<WorkflowConfiguration> configs) {
+    JSONArray json = new JSONArray();
+    if(configs != null) {
+      for(WorkflowConfiguration config : configs) {
+        JSONObject jsConfig = new JSONObject();
+        jsConfig.put(config.getKey(), config.getValue());
+        json.add(jsConfig);
+      }
+    }
+    return json;
   }
 
   protected DublinCoreCatalog getDublinCore(MediaPackage mediaPackage) {
