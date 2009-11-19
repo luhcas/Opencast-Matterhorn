7594e9a231e32744d95206dd8ef48f9c20cfd546
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-19 22:02:09 +0000

    Creating a branch for 0.4 Release Candidate
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3903 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-engage-service-impl/branches/0.4-RC/src/main/java/org/opencastproject/engage/impl/EngageServiceRestImpl.java
@@ -0,0 +1,124 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.engage.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.io.IOUtils;
+import org.opencastproject.engage.api.EngageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The REST endpoint 
+ */
+@Path("/")
+public class EngageServiceRestImpl implements EngageService {
+  
+  private static final Logger logger = LoggerFactory.getLogger(EngageServiceImpl.class);
+  
+  private EngageService service;
+  
+  public void setService(EngageService service) {
+    this.service = service;
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("docs")
+  public String getDocumentation() {
+    return docs;
+  }
+
+  protected final String docs;
+  
+  /**
+   * Creates an engage service 
+   */
+  public EngageServiceRestImpl() {
+    String docsFromClassloader = null;
+    InputStream in = null;
+    try {
+      in = getClass().getResourceAsStream("/html/index.html");
+      docsFromClassloader = IOUtils.toString(in);
+    } catch (IOException e) {
+      logger.error("failed to read documentation", e);
+      docsFromClassloader = "unable to load documentation for " + EngageServiceRestImpl.class.getName();
+    } finally {
+      IOUtils.closeQuietly(in);
+    }
+    docs = docsFromClassloader;
+  }
+
+  public String deliverPlayer(String mediaPackageId) {
+    return service.deliverPlayer(mediaPackageId);
+  }
+  
+  public String deliverBrowsePage() {
+    return service.deliverBrowsePage();
+  }
+  
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("search")
+  public String search(@Context HttpServletRequest request) {
+    return "Search";
+  }
+  
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("/")
+  public String browse(@Context HttpServletRequest request) {
+    return deliverBrowsePage();
+  }
+  
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("search/episodes")
+  public String episodesByDate(
+          @QueryParam("start") int start, 
+          @Context HttpServletRequest request) 
+  {
+    return service.getEpisodesByDate(start, 10); 
+  }
+  
+  public String getEpisodesByDate(int limit, int offset)
+  {
+    return null;
+  }
+  
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("watch/{name}/{localName}")
+  public String watch(
+          @PathParam("name") String name, 
+          @PathParam("localName") String localName, 
+          @Context HttpServletRequest request) {
+    
+    return deliverPlayer(name+"/"+localName);
+  }
+  
+}
---
+++ modules/opencast-engage-service-impl/branches/0.4-RC/src/main/resources/player/player-hybrid-tovideodisplay.js
@@ -0,0 +1,179 @@
+/*global $, Videodisplay, Opencast*/
+/*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, onevar: false */
+
+
+/* ------------------------------------------------------------------------
+ * the global opencast namespace ToVideodisplay
+ * ------------------------------------------------------------------------ */
+
+Opencast.ToVideodisplay = (function () {
+
+    var pressKey = false,
+        playing = "playing",
+        pausing = "pausing",
+        currentPlayPauseState = pausing;
+
+    function doSeek(time) {
+        $('#slider').slider('value', time);
+        Videodisplay.seek(time);
+    }
+
+    function doSkipBackward() {
+        Videodisplay.skipBackward();
+    }
+
+    function doRewind() {
+        Videodisplay.rewind();
+    }
+
+    function doPlay() {
+        Videodisplay.play();
+    }
+
+    function doPause() {
+        Videodisplay.pause();
+    }
+
+    function doStop() {
+        Videodisplay.stop();
+    }
+
+    function doSetCurrentPlayPauseState(state)
+    {
+        currentPlayPauseState = state;
+    }
+    
+    function getCurrentPlayPauseState()
+    {
+        return currentPlayPauseState;
+    }
+    
+    function getPressKey()
+    {
+        return pressKey;
+    }
+
+    function doTogglePlayPause() {
+    
+        // Checking if btn_play_pause is "play"
+        if (currentPlayPauseState === pausing) {
+            // Changing the volume to 1.0 and the value of the button of btn_volume to "unmute"
+            Opencast.FromVideodisplay.setPlayPauseState(playing);
+            doPlay();
+        } else {
+            // Changing the volume to 0.0 and the value of the button of btn_volume to "mute"
+            Opencast.FromVideodisplay.setPlayPauseState(pausing);
+            doPause();
+        }
+    }
+
+    function doFastForward() {
+        Videodisplay.fastForward();
+    }
+
+    function doSkipForward() {
+        Videodisplay.skipForward();
+    }
+
+    function doSetVolume(value) {
+        Videodisplay.setVolume(value);
+    }
+
+    function doToggleVolume() {
+        var mute = "Mute";
+        // Checking if btn_volume is "mute"
+        if (document.getElementById("btn_volume").value === mute) {
+            //Changing the volume to 1.0 and the value of the button of btn_volume to "unmute"
+            document.getElementById("btn_volume").value = "Unmute";
+            document.getElementById("btn_volume").alt = "Unmute";
+            document.getElementById("btn_volume").title = "Unmute";
+            document.getElementById("btn_volume").src = "./icons/volume---mute.png";
+            Opencast.volume = $('#volume_slider').slider('option', 'value') / 100;
+            doSetVolume(0);
+        } else {
+            // Changing the volume to 0.0 and the value of the button of btn_volume to "mute"
+            document.getElementById("btn_volume").value = "Mute";
+            document.getElementById("btn_volume").alt = "Mute";
+            document.getElementById("btn_volume").title = "Mute";
+            document.getElementById("btn_volume").src = "./icons/volume---high.png";
+            doSetVolume(Opencast.volume);
+        }
+    }
+    
+    function doClosedCaptions(cc) {
+        Videodisplay.closedCaptions(cc);
+    }
+
+	function doToogleClosedCaptions() {
+        var on = "Closed Caption on";
+        // Checking if btn_cc is "CC off"
+        if (document.getElementById("btn_cc").value === on) {
+            document.getElementById("btn_cc").value = "Closed Caption off";
+            document.getElementById("btn_cc").alt = "Closed Caption off";
+            document.getElementById("btn_cc").title = "Closed Caption off";
+            document.getElementById("btn_cc").src = "./icons/cc_on.png";
+            doClosedCaptions(true);
+            return;
+        } else {
+            document.getElementById("btn_cc").value = "Closed Caption on";
+            document.getElementById("btn_cc").alt = "Closed Caption on";
+            document.getElementById("btn_cc").title = "Closed Caption on";
+            document.getElementById("btn_cc").src = "./icons/cc_off.png";
+            doClosedCaptions(false);
+            return;
+        }
+    }
+
+    var isCtrl = false;
+    var isAlt = false;
+    
+    $(document).keyup(function (e) { 
+        if (e.which === 17) 
+        {
+            isCtrl = false; 
+        }
+        if (e.which === 18) 
+        {
+            isAlt = false; 
+        }
+        pressKey = false;
+    }).keydown(function (e) { 
+        if (e.which === 17)
+        {
+            isCtrl = true;
+        }
+        if (e.which === 18)
+        {
+            isAlt = true;
+        }
+        if (isCtrl === true && isAlt === true) {
+          
+            pressKey = true;
+            if (e.which === 80 || e.which === 112 || e.which === 83 || e.which === 115 || e.which === 77 || e.which === 109 || e.which === 85 || e.which === 117  || e.which === 68 || e.which === 100 || e.which === 49 || e.which === 50 || e.which === 51 || e.which === 52 || e.which === 53 || e.which === 67 || e.which === 99 || e.which === 82 || e.which === 114 || e.which === 70 || e.which === 102)
+            {
+                Videodisplay.passCharCode(e.which);
+                
+            }
+            return false;
+        }
+    }); 
+
+    return {
+        doSeek: doSeek,
+        doSkipBackward : doSkipBackward,
+        doRewind : doRewind,
+        getCurrentPlayPauseState : getCurrentPlayPauseState,
+        getPressKey : getPressKey,
+        doPlay: doPlay,
+        doPause: doPause,
+        doStop: doStop,
+        doSetCurrentPlayPauseState : doSetCurrentPlayPauseState,
+        doTogglePlayPause : doTogglePlayPause,
+        doFastForward: doFastForward,
+        doSkipForward : doSkipForward,
+        doToggleVolume: doToggleVolume,
+        doSetVolume: doSetVolume,
+        doClosedCaptions: doClosedCaptions,
+        doToogleClosedCaptions : doToogleClosedCaptions
+    };
+}());
