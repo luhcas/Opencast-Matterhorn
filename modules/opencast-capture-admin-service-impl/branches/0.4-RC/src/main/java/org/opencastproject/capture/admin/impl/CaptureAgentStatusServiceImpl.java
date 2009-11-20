9ee1dfcb8391a17175367a56be0b10eedf41b9e9
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-20 18:33:13 +0000

    MH-1432 Creating a branch for 0.4 Release Candidate
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3943 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-capture-admin-service-impl/branches/0.4-RC/src/main/java/org/opencastproject/capture/admin/impl/CaptureAgentStatusServiceImpl.java
@@ -0,0 +1,142 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.capture.admin.impl;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.opencastproject.capture.admin.api.Agent;
+import org.opencastproject.capture.admin.api.CaptureAgentStatusService;
+import org.opencastproject.capture.admin.api.Recording;
+import org.opencastproject.capture.api.AgentState;
+import org.opencastproject.capture.api.RecordingState;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * FIXME -- Add javadocs
+ */
+public class CaptureAgentStatusServiceImpl implements CaptureAgentStatusService, ManagedService {
+  private static final Logger logger = LoggerFactory.getLogger(CaptureAgentStatusServiceImpl.class);
+
+  private HashMap<String, Agent> agents;
+  private HashMap<String, Recording> recordings;
+
+  public CaptureAgentStatusServiceImpl() {
+    agents = new HashMap<String, Agent>();
+    recordings = new HashMap<String, Recording>();
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#getAgentState(java.lang.String)
+   */
+  public Agent getAgentState(String agentName) {
+    Agent req = agents.get(agentName);
+    //If that agent doesn't exist, return an unknown agent, else return the known agent
+    if (req == null) {
+      Agent a = new Agent(agentName, AgentState.UNKNOWN);
+      return a;
+    } else {
+      return req;
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#setAgentState(java.lang.String, java.lang.String)
+   */
+  public void setAgentState(String agentName, String state) {
+    Agent req = agents.get(agentName);
+    //if the agent is known set the state
+    if (req != null) {
+      req.setState(state);
+    } else {
+      Agent a = new Agent(agentName, state);
+      agents.put(agentName, a);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#removeAgent(java.lang.String)
+   */
+  public void removeAgent(String agentName) {
+    agents.remove(agentName);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#getKnownAgents()
+   */
+  public Map<String, Agent> getKnownAgents() {
+    return agents;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#getRecordingState(java.lang.String)
+   */
+  public Recording getRecordingState(String id) {
+    Recording req = recordings.get(id);
+    //If that agent doesn't exist, return an unknown agent, else return the known agent
+    if (req == null) {
+      Recording r = new Recording(id, RecordingState.UNKNOWN);
+      return r;
+    } else {
+      return req;
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#setRecordingState(java.lang.String, java.lang.String)
+   */
+  public void setRecordingState(String id, String state) {
+    Recording req = recordings.get(id);
+    if (req != null) {
+      req.setState(state);
+    } else {
+      Recording r = new Recording(id, state);
+      recordings.put(id, r);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#removeRecording(java.lang.String)
+   */
+  public void removeRecording(String id) {
+    recordings.remove(id);
+  }
+  
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.capture.admin.api.CaptureAgentStatusService#getKnownRecordings()
+   */
+  public Map<String,Recording> getKnownRecordings() {
+    return recordings;
+  }
+
+
+  @SuppressWarnings("unchecked")
+  public void updated(Dictionary props) throws ConfigurationException {
+    // Update any configuration properties here
+  }
+}
---
+++ modules/opencast-capture-admin-service-impl/branches/0.4-RC/src/test/java/org/opencastproject/capture/admin/impl/CaptureAgentStatusServiceImplTest.java
@@ -0,0 +1,129 @@
+package org.opencastproject.capture.admin.impl;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.opencastproject.capture.admin.api.Agent;
+import org.opencastproject.capture.admin.api.CaptureAgentStatusService;
+import org.opencastproject.capture.admin.api.Recording;
+import org.opencastproject.capture.api.AgentState;
+import org.opencastproject.capture.api.RecordingState;
+
+public class CaptureAgentStatusServiceImplTest {
+  private CaptureAgentStatusService service = null;
+
+  @Before
+  public void setup() {
+    service = new CaptureAgentStatusServiceImpl();
+    Assert.assertNotNull(service);
+  }
+
+  @After
+  public void teardown() {
+    service = null;
+  }
+
+  @Test
+  public void nonExistantAgent() {
+    Agent agent = service.getAgentState("doesNotExist");
+    Assert.assertEquals("doesNotExist", agent.getName());
+    Assert.assertEquals(AgentState.UNKNOWN, agent.getState());
+  }
+
+  @Test
+  public void noAgents() {
+    Assert.assertEquals(0, service.getKnownAgents().size());
+  }
+
+  private void verifyAgent(String name, String state) {
+    Agent agent = service.getAgentState(name);
+    Assert.assertEquals(name, agent.getName());
+    Assert.assertEquals(state, agent.getState());
+  }
+
+  @Test
+  public void oneAgent() {
+    service.setAgentState("agent1", AgentState.IDLE);
+    Assert.assertEquals(1, service.getKnownAgents().size());
+
+    verifyAgent("notAgent1", AgentState.UNKNOWN);
+    verifyAgent("agent1", AgentState.IDLE);
+
+    service.setAgentState("agent1", AgentState.CAPTURING);
+    Assert.assertEquals(1, service.getKnownAgents().size());
+
+    verifyAgent("notAgent1", AgentState.UNKNOWN);
+    verifyAgent("agent1", AgentState.CAPTURING);
+  }
+
+  @Test
+  public void removeAgent() {
+    service.setAgentState("agent1", AgentState.IDLE);
+    Assert.assertEquals(1, service.getKnownAgents().size());
+    service.setAgentState("agent2", AgentState.UPLOADING);
+    Assert.assertEquals(2, service.getKnownAgents().size());
+
+    verifyAgent("notAnAgent", AgentState.UNKNOWN);
+    verifyAgent("agent1", AgentState.IDLE);
+    verifyAgent("agent2", AgentState.UPLOADING);
+
+    service.removeAgent("agent1");
+    Assert.assertEquals(1, service.getKnownAgents().size());
+    verifyAgent("notAnAgent", AgentState.UNKNOWN);
+    verifyAgent("agent1", AgentState.UNKNOWN);
+    verifyAgent("agent2", AgentState.UPLOADING);
+  }
+
+  @Test
+  public void nonExistantRecording() {
+    Recording Recording = service.getRecordingState("doesNotExist");
+    Assert.assertEquals("doesNotExist", Recording.getID());
+    Assert.assertEquals(RecordingState.UNKNOWN, Recording.getState());
+  }
+
+  @Test
+  public void noRecordings() {
+    Assert.assertEquals(0, service.getKnownRecordings().size());
+  }
+
+  private void verifyRecording(String id, String state) {
+    Recording Recording = service.getRecordingState(id);
+    Assert.assertEquals(id, Recording.getID());
+    Assert.assertEquals(state, Recording.getState());
+  }
+
+  @Test
+  public void oneRecording() {
+    service.setRecordingState("Recording1", RecordingState.UPLOAD_FINISHED);
+    Assert.assertEquals(1, service.getKnownRecordings().size());
+
+    verifyRecording("notRecording1", RecordingState.UNKNOWN);
+    verifyRecording("Recording1", RecordingState.UPLOAD_FINISHED);
+
+    service.setRecordingState("Recording1", RecordingState.CAPTURING);
+    Assert.assertEquals(1, service.getKnownRecordings().size());
+
+    verifyRecording("notRecording1", RecordingState.UNKNOWN);
+    verifyRecording("Recording1", RecordingState.CAPTURING);
+  }
+
+  @Test
+  public void removeRecording() {
+    service.setRecordingState("Recording1", RecordingState.CAPTURING);
+    Assert.assertEquals(1, service.getKnownRecordings().size());
+    service.setRecordingState("Recording2", RecordingState.UPLOADING);
+    Assert.assertEquals(2, service.getKnownRecordings().size());
+
+    verifyRecording("notAnRecording", RecordingState.UNKNOWN);
+    verifyRecording("Recording1", RecordingState.CAPTURING);
+    verifyRecording("Recording2", RecordingState.UPLOADING);
+
+    service.removeRecording("Recording1");
+    Assert.assertEquals(1, service.getKnownRecordings().size());
+    verifyRecording("notAnRecording", RecordingState.UNKNOWN);
+    verifyRecording("Recording1", RecordingState.UNKNOWN);
+    verifyRecording("Recording2", RecordingState.UPLOADING);
+  }
+}
