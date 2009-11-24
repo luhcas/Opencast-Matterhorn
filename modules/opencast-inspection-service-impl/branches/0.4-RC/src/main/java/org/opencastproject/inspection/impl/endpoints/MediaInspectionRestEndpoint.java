7b58ae6fd32c13be02774db8289cb52ced7fb877
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-24 15:19:29 +0000

    MH-1432 Recreating 0.4 Release Candidate
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4055 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-inspection-service-impl/branches/0.4-RC/src/main/java/org/opencastproject/inspection/impl/endpoints/MediaInspectionRestEndpoint.java
@@ -0,0 +1,92 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.inspection.impl.endpoints;
+
+import org.opencastproject.inspection.api.MediaInspectionService;
+import org.opencastproject.media.mediapackage.Stream;
+import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.media.mediapackage.jaxb.AudioType;
+import org.opencastproject.media.mediapackage.jaxb.ChecksumType;
+import org.opencastproject.media.mediapackage.jaxb.ObjectFactory;
+import org.opencastproject.media.mediapackage.jaxb.ScanTypeType;
+import org.opencastproject.media.mediapackage.jaxb.TrackType;
+import org.opencastproject.media.mediapackage.jaxb.VideoType;
+import org.opencastproject.media.mediapackage.track.AudioStreamImpl;
+import org.opencastproject.media.mediapackage.track.VideoStreamImpl;
+
+import java.net.URL;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * A service endpoint to expose the {@link MediaInspectionService} via REST.
+ */
+@Path("/")
+public class MediaInspectionRestEndpoint {
+  protected MediaInspectionService service;
+  public void setService(MediaInspectionService service) {
+    this.service = service;
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  public TrackType getTrack(@QueryParam("url") URL url) {
+    Track t = service.inspect(url);
+    ObjectFactory of = new ObjectFactory();
+    TrackType track = of.createTrackType();
+    ChecksumType checksum = of.createChecksumType();
+    checksum.setType(t.getChecksum().getType().getName());
+    checksum.setValue(t.getChecksum().getValue());
+    track.setDuration(t.getDuration());
+    track.setChecksum(checksum);
+    
+    for(Stream stream : t.getStreams()) {
+      if(stream instanceof AudioStreamImpl) {
+        AudioStreamImpl a = (AudioStreamImpl) stream;
+        AudioType audio = of.createAudioType();
+        audio.setBitrate(a.getBitRate());
+        audio.setChannels(a.getChannels());
+        audio.setSamplingrate(a.getSamplingRate());
+        track.setAudio(audio);
+      } else if(stream instanceof VideoStreamImpl) {
+        VideoStreamImpl v = (VideoStreamImpl) stream;
+        VideoType video = of.createVideoType();
+        video.setBitrate(v.getBitRate());
+        video.setFrameRate(v.getFrameRate());
+        ScanTypeType scanType = of.createScanTypeType();
+        scanType.setType(v.getScanType().name());
+        video.setScanType(scanType);
+        track.setVideo(video);
+      } else {
+        throw new IllegalStateException("stream is of an unknown type: " + stream);
+      }
+    }
+    return track;
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("docs")
+  public String getDocumentation() {
+    return "Analyze media files by specifying a URL.  For example:" + 
+      "<a href=\"./?url=http://source.opencastproject.org/svn/modules/opencast-media/trunk/src/test/resources/aonly.mov\">" +
+      "analyze a sample movie file</a> at http://source.opencastproject.org/svn/modules/opencast-media/trunk/src/test/resources/aonly.mov";
+  }
+}
