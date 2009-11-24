508244c936686cb9563c6808beaf501d74450073
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-24 14:17:48 +0000

    MH-1432 Need to recreate Release Candidate
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@3980 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-service-impl/branches/0.4-RC/src/main/java/org/opencastproject/capture/impl/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Status service implementation classes
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED)
-package org.opencastproject.capture.impl;
-
-import javax.xml.bind.annotation.XmlNsForm;
-
