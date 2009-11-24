b3d8b0b368ae33171ccb4c121bdbc05ab78ee747
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-24 15:07:54 +0000

    MH-1432 Recreating 0.4 Release Candidate
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4030 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-capture-admin-service-api/branches/0.4-RC/src/main/java/org/opencastproject/capture/admin/api/AgentStateUpdate.java
@@ -0,0 +1,63 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.capture.admin.api;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * A representation of an agent which stores its name, state and time-since-last-update value
+ */
+@XmlType(name="agent-state-update", namespace="http://capture.admin.opencastproject.org")
+@XmlRootElement(name="agent-state-update", namespace="http://capture.admin.opencastproject.org")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AgentStateUpdate {
+
+  /**
+   * The agent's name
+   */
+  @XmlElement(name="name")
+  public String name;
+
+  /**
+   * The state of the agent.  This should be defined from the constants in AgentState
+   * @see org.opencastproject.capture.api.AgentState
+   */
+  @XmlElement(name="state")
+  public String state;
+
+  /**
+   * The number of milliseconds since the last time the agent checked in.  Note that this is relative (ie, it's been 3000 ms) rather than absolute (milliseconds since 1970)
+   */
+  @XmlElement(name="time-since-last-update")
+  public Long time_delta;
+
+  /** Required 0-arg constructor.  Does nothing, do not use */
+  public AgentStateUpdate() {}
+
+  /**
+   * Builds an AgentStateUpdate object about the Agent a.  This calculates the time delta for you.
+   * @param a The agent you wish to know more information about
+   */
+  public AgentStateUpdate(Agent a) {
+    name = a.getName();
+    state = a.getState();
+    time_delta = System.currentTimeMillis() - a.getLastCheckinTime();
+  }
+}
