7c8db1f00d364e90cf9df46cf96393dd8c328f21
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-24 15:14:13 +0000

    MH-1432 Recreating 0.4 Release Candidate
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4045 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-engage-player/branches/0.4-RC/src/main/html/html-debug/player-hybrid-download.js
@@ -0,0 +1,100 @@
+/*global $, Videodisplay*/
+/*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, onevar: false */
+
+
+/**
+    @namespace the global Opencast namespace
+*/
+var Opencast = Opencast || {};
+
+Opencast.volume = 1.0;
+Opencast.mouseOverBool = false;
+
+$(document).ready(function () {
+    $("#slider").slider();
+    $('#slider').slider('option', 'animate', false);
+    $('#slider').slider('option', 'min', 0);
+    $('#slider').bind('slide', function (event, ui) {
+        Videodisplay.seek(ui.value);
+    });
+    
+    $('#volume_slider').slider();
+    $('#volume_slider').slider('option', 'min', 0);
+    $('#volume_slider').slider('option', 'max', 100);
+    $('#volume_slider').slider({
+        steps: 10
+    });
+    $('#volume_slider').slider('value', 100);
+    $('#volume_slider').bind('slide', function (event, ui) {
+        Opencast.ToVideodisplay.doSetVolume(ui.value / 100);
+        Opencast.volume = ui.value / 100;
+    });
+    $('#volume_slider').bind('slidechange', function (event, ui) {
+        if (ui.value !== 0) 
+        {
+            Opencast.global.doUnmute();
+        }
+    });  
+});
+
+Opencast.global = (function () {
+
+    var playing = "playing",
+    pausing     = "pausing",
+    unmute      = "Unmute",
+    mute        = "mute";
+
+    /**
+        @memberOf Opencast.global
+        @description Do unmute the volume of the video.
+    */
+    function doUnmute() {
+        if ($("#btn_volume").attr("value") === unmute) {  
+            $("#btn_volume").attr({ 
+                value: mute,
+                alt: mute,
+                title: mute,
+                src: "./icons/volume---high.png"
+            });
+        } 
+    }
+    
+    /**
+        @memberOf Opencast.global
+        @description Mouse over effect.
+    */
+    function mouseOver() {
+        if (Opencast.ToVideodisplay.getCurrentPlayPauseState() === playing) {
+            $("#btn_play_pause").attr("className", "btn_pause_over");
+            Opencast.mouseOverBool = true;
+        }
+         else if (Opencast.ToVideodisplay.getCurrentPlayPauseState() === pausing) {
+            $("#btn_play_pause").attr("className", "btn_play_over");
+            Opencast.mouseOverBool = true;
+        }
+    }
+    /**
+        @memberOf Opencast.global
+        @description Mouse out effect.
+    */
+    function mouseOut() {
+        if (Opencast.ToVideodisplay.getCurrentPlayPauseState() === playing) {
+            $("#btn_play_pause").attr("className", "btn_pause_out");
+            Opencast.mouseOverBool = false;
+        }
+        else if (Opencast.ToVideodisplay.getCurrentPlayPauseState() === pausing) {
+            $("#btn_play_pause").attr("className", "btn_play_out");
+            Opencast.mouseOverBool = false;
+        }
+    }
+    
+    
+
+    return {
+        doUnmute : doUnmute,
+        mouseOver : mouseOver,
+        mouseOut : mouseOut
+
+    };
+}());
+
---
+++ modules/opencast-engage-player/branches/0.4-RC/src/main/html/html-debug/player-hybrid-fromvideodisplay.js
@@ -0,0 +1,177 @@
+/*global $, Videodisplay, Opencast*/
+/*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, onevar: false */
+
+
+/**
+    @namespace the global Opencast namespace FromVideodisplay
+*/
+Opencast.FromVideodisplay = (function () {
+
+    var playing = "playing",
+        pausing = "pausing",
+        play    = "Play",
+        pause   = "Pause",
+        unmute  = "Unmute",
+        ccon    = "Closed Caption On",
+        ccoff   = "Closed Caption Off";
+    
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the new position of the seek slider.
+        @param number newPosition 
+    */
+    function setPlayhead(newPosition) {
+        $('#slider').slider('value', newPosition);
+    }
+
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the new position of the volume slider.
+        @param number newVolume 
+    */
+    function setVolume(newVolume) {
+        $('#volume_slider').slider('value', newVolume);
+    }
+
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the current time of the video.
+        @param string text 
+    */
+    function setCurrentTime(text) {
+        $("#time-current").text(text);
+    }
+
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the total time of the video.
+        @param string text 
+    */
+    function setTotalTime(text) {
+        $("#time-total").text(text);
+    }
+
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the slider max time.
+        @param number time 
+    */
+    function setDuration(time) {
+        $('#slider').slider('option', 'max', time);
+    }
+    
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the with of the progress bar.
+        @param number value 
+    */
+    function setProgress(value) {
+        $('.matterhorn-progress-bar').css("width", (value + "%"));
+    }
+
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Set the html captions.
+        @param html text 
+    */
+    function setCaptions(text) {
+        $("#captions").html(text);
+    }
+    
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Toggle the cc button between on or off.
+        @param boolean bool 
+    */
+    function setCaptionsButton(bool) {
+       
+        if (bool === true)
+        {
+            $("#btn_cc").attr({ 
+                value: ccoff,
+                alt: ccoff,
+                title: ccoff,
+                src: "./icons/cc_on.png"
+            });
+        }
+        else
+        {
+            $("#btn_cc").attr({ 
+                value: ccon,
+                alt: ccon,
+                title: ccon,
+                src: "./icons/cc_off.png"
+            });
+        }
+    }
+    
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Toogle between play and pause.
+        @param sting state 
+    */
+    function setPlayPauseState(state) {
+        if (state === playing) {
+            $("#btn_play_pause").attr({ 
+                value: play,
+                alt: play,
+                title: play
+            });
+            
+            if (Opencast.ToVideodisplay.getPressKey())
+            {
+                $("#btn_play_pause").attr("className", "btn_play_out");
+            }
+            else if (Opencast.mouseOverBool === true)
+            {
+                $("#btn_play_pause").attr("className", "btn_play_over");
+            }
+            Opencast.ToVideodisplay.doSetCurrentPlayPauseState(pausing);
+        } 
+        else {
+            $("#btn_play_pause").attr({ 
+                value: pause,
+                alt: pause,
+                title: pause
+            });
+           
+            if (Opencast.ToVideodisplay.getPressKey())
+            {
+                $("#btn_play_pause").attr("className", "btn_pause_out");
+             
+            }
+            else if (Opencast.mouseOverBool === true)
+            {
+                $("#btn_play_pause").attr("className", "btn_pause_over");
+            }
+            
+            Opencast.ToVideodisplay.doSetCurrentPlayPauseState(playing);
+        }
+    }
+    
+    /**
+        @memberOf Opencast.FromVideodisplay
+        @description Mute the player.
+    */
+    function setDoToggleVolume() {
+        $("#btn_volume").attr({ 
+            value: unmute,
+            alt: unmute,
+            title: unmute,
+            src: "./icons/volume---mute.png"
+        });
+    }
+    
+    return {
+        setPlayhead : setPlayhead,
+        setVolume : setVolume,
+        setCurrentTime : setCurrentTime,
+        setTotalTime: setTotalTime,
+        setDuration: setDuration,
+        setProgress : setProgress,
+        setCaptions : setCaptions,
+        setPlayPauseState : setPlayPauseState,
+        setCaptionsButton : setCaptionsButton,
+        setDoToggleVolume : setDoToggleVolume
+    };
+}());
