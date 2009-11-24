33b8cd3221fd093ecd866bb6a4241a1441304a26
azeckoski <azeckoski@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-11-24 15:01:12 +0000

    Creating tag for captions service before moving to trunk
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4020 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ contrib/captions/tags/20091124/opencast-captions-service-impl/src/main/java/org/opencastproject/captions/impl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Captions service implementation classes
+ */
+@javax.xml.bind.annotation.XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED)
+package org.opencastproject.captions.impl;
+
+import javax.xml.bind.annotation.XmlNsForm;
