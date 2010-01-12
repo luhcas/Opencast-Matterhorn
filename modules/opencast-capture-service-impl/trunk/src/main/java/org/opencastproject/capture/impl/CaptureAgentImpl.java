77a2016c15975b0c7288855b2fb84cfbb32121c9
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-12 23:04:56 +0000

    Continuing work on ingest as part of MH-1888
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4472 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-service-impl/trunk/src/main/java/org/opencastproject/capture/impl/CaptureAgentImpl.java
+++ modules/opencast-capture-service-impl/trunk/src/main/java/org/opencastproject/capture/impl/CaptureAgentImpl.java
@@ -15,6 +15,34 @@
  */
 package org.opencastproject.capture.impl;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Vector;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.gstreamer.Bus;
+import org.gstreamer.GstObject;
+import org.gstreamer.Pipeline;
+import org.gstreamer.State;
 import org.opencastproject.capture.admin.api.AgentState;
 import org.opencastproject.capture.admin.api.RecordingState;
 import org.opencastproject.capture.api.CaptureAgent;
@@ -30,40 +58,11 @@ import org.opencastproject.media.mediapackage.MediaPackageElements;
 import org.opencastproject.media.mediapackage.MediaPackageException;
 import org.opencastproject.media.mediapackage.UnsupportedElementException;
 import org.opencastproject.util.ZipUtil;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.gstreamer.Bus;
-import org.gstreamer.GstObject;
-import org.gstreamer.Pipeline;
-import org.gstreamer.State;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLConnection;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Properties;
-import java.util.Vector;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 
 /**
@@ -211,6 +210,8 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     if (newRec.getRecordingID() == null) {
       logger.error("Couldn't create a valid recording ID");
       setAgentState(AgentState.IDLE);
+      //TODO:  Heh, now what?  We can't set it if the id doesn't exist...
+      //setRecordingState(recordingID, RecordingState.CAPTURE_ERROR);
       return null;
     }
 
@@ -236,6 +237,7 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     if (pipe == null) {
       logger.error("Capture {} could not start, pipeline was null!", recordingID);
       setAgentState(AgentState.IDLE);
+      setRecordingState(recordingID, RecordingState.CAPTURE_ERROR);
       return null;
     }
 
@@ -299,10 +301,8 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     currentRecID = null;
 
     //Update the states of everything.
-    //String recordingID = cur.getProperty(CaptureParameters.RECORDING_ID);
     setRecordingState(theRec.getRecordingID(), RecordingState.CAPTURE_FINISHED);
     setAgentState(AgentState.IDLE);
-    //currentCaptureDir = null;
 
     try {
       // Sending End Of Stream event to the Pipeline so its components stop appropriately
@@ -315,7 +315,7 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
 
       stopFlag.createNewFile();
     } catch (IOException e) {
-      setRecordingState(theRec.getRecordingID(), RecordingState.UPLOAD_ERROR);
+      setRecordingState(theRec.getRecordingID(), RecordingState.CAPTURE_ERROR);
       logger.error("IOException: Could not create \"capture.stopped\" file: {}.", e.getMessage());
       return false; 
     }
@@ -350,72 +350,6 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     return false;
   }
 
-  // TODO: This should go in a separate method. Was in createManifest but makes it too long.
-  /*// Create a metadata catalog with some info
-  // TODO: Should this be moved out to its own method?
-  try {
-    // Create the document to hold the metadata
-    metaFile = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-    
-    // Keep the root node of the document
-    Element root = metaFile.createElement("agent-metadata");
-    
-    // Inserts the agent name node
-    Element agentName = metaFile.createElement("agent-name");
-    // FIXME: Where do we get the Capture Agent name?
-    agentName.setTextContent("Change Me");
-    root.appendChild(agentName);
-    
-    // Inserts the "mappings" node
-    Node mappings = metaFile.createElement("mappings");
-    root.appendChild(mappings);
-    
-    // Inserts the friendly names and the mappings
-    for (String fName : friendlyNames) {
-      //// Friendly name
-      Element friendlyName = metaFile.createElement("friendly-name");
-      friendlyName.setTextContent(fName);
-      mappings.appendChild(friendlyName);
-      //// Mapping
-      Element mapping = metaFile.createElement("mapping");
-      mapping.setAttribute("friendly-name", fName);
-      Element input = metaFile.createElement("input");
-      String inputDevice = recording.getProperty(
-              CaptureParameters.CAPTURE_DEVICE_PREFIX +
-              fName +
-              CaptureParameters.CAPTURE_DEVICE_SOURCE
-              );
-      input.setTextContent(inputDevice);
-      Element flavor = metaFile.createElement("flavor");
-      // TODO: This should be changed according with the solution adopted for the "TODO" in line 449
-      if (fName.equals("PRESENTER") || fName.equals("AUDIO"))
-        flavor.setTextContent(MediaPackageElements.PRESENTER_TRACK.toString());
-      else if (fName.equals("SCREEN"))
-        flavor.setTextContent(MediaPackageElements.PRESENTATION_TRACK.toString());
-      
-      mapping.appendChild(input);
-      mapping.appendChild(flavor);
-      mappings.appendChild(mapping);
-    }
-    
-    // Inserts the zipName
-    Element zNameElement = metaFile.createElement("zip-name");
-    zNameElement.setTextContent(recording.getZipName());
-    root.appendChild(zNameElement);
-    
-    // Adds this document to the MediaPackage
-    recording.getMediaPackage().add(new URI(recording.getAgentCatalogName()));
-    
-  } catch (ParserConfigurationException e) {
-    logger.error("Parser Exception when creating an XML Document: {}", e.getMessage());
-    return false;
-  } catch (UnsupportedElementException e) {
-    logger.error("The agent metadata file seems not to be supported by the MediaPackage implementation: {}", e.getMessage());
-    return false;
-  } catch (URISyntaxException e) {
-    logger.error("Incorrect URI for the agent metadata file: {}. Cause: {}", recording.getAgentCatalogName(), e.getMessage());
-    return false;
-  }*/
 
   /**
    * Generates the manifest.xml file from the files specified in the properties
@@ -431,7 +365,6 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
       logger.debug("Generating manifest for recording {}", recID);
 
     String[] friendlyNames = recording.getProperty(CaptureParameters.CAPTURE_DEVICE_NAMES).split(",");
-    Document metaFile = null;
     
     // Includes the tracks in the MediaPackage
     try {
@@ -457,7 +390,7 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
 
         // Adds the file to the MediaPackage
         if (outputFile.exists())
-          recording.getMediaPackage().add(elemBuilder.elementFromURI(new URI(outputFile.getName()),
+          recording.getMediaPackage().add(elemBuilder.elementFromURI(outputFile.toURI(),
                   MediaPackageElement.Type.Track,
                   flavor));
         else 
@@ -474,9 +407,6 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
           pkg.add(new URI(item.getName()));*/
     } catch (UnsupportedElementException e) {
       logger.error("Unsupported Element Exception: {}.", e.getMessage());
-      return false;
-    } catch (URISyntaxException e) {
-      logger.error("URI Exception: {}.", e.getMessage());
       return false;
     }
         
@@ -545,7 +475,8 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     filesToZip.add(recording.getManifest());
     
     for (MediaPackageElement item : mpElements) {
-      File tmpFile = new File(recording.getDir(), item.getURI().toString());
+      logger.info("GDL: \"" + item.getURI().toString() + "\" " + item.getURI().isAbsolute());
+      File tmpFile = new File(item.getURI().normalize());
       // TODO: Is this really a warning or should we fail completely and return an error?
       if (!tmpFile.exists())
         logger.warn("Required file {} doesn't exist!", tmpFile.getName());
@@ -574,11 +505,24 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
       return -1;
     }
 
-    String url = recording.getProperty(CaptureParameters.INGEST_ENDPOINT_URL);
-    //File baseDir = recording.getDir();
+    URL url = null;
+    try {
+      url = new URL(recording.getProperty(CaptureParameters.INGEST_ENDPOINT_URL));
+    } catch (NullPointerException e) {
+      logger.warn("Nullpointer while parsing ingest target URL.");
+      return -2;
+    } catch (MalformedURLException e) {
+      logger.warn("Malformed URL for ingest target.");
+      return -3;
+    }
 
+    if (url == null) {
+      logger.warn("Unable to ingest media because the ingest target URL is null.");
+      return -1;
+    }
+    
     HttpClient client = new DefaultHttpClient();
-    HttpPost postMethod = new HttpPost(url);
+    HttpPost postMethod = new HttpPost(url.toString());
     int retValue = -1;
 
     File fileDesc = new File(recording.getDir(), "media.zip");
@@ -589,6 +533,8 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
 
       logger.warn("Sending the file " + fileDesc.getAbsolutePath() + " with a size of "+ fileDesc.length());
 
+      setRecordingState(recID, RecordingState.UPLOADING);
+
       postMethod.setEntity(myFileEntity);
 
       // Send the file
@@ -596,13 +542,13 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
 
       retValue = response.getStatusLine().getStatusCode();
 
-      //singleton.setRecordingState(recordingID, RecordingState.UPLOAD_FINISHED);
+      setRecordingState(recID, RecordingState.UPLOAD_FINISHED);
     } catch (ClientProtocolException e) {
       logger.error("Failed to submit the data: {}.", e.getMessage());
-      //singleton.setRecordingState(recordingID, RecordingState.UPLOAD_ERROR);
+      setRecordingState(recID, RecordingState.UPLOAD_ERROR);
     } catch (IOException e) {
       logger.error("I/O Exception: {}.", e.getMessage());
-      //singleton.setRecordingState(recordingID, RecordingState.UPLOAD_ERROR);
+      setRecordingState(recID, RecordingState.UPLOAD_ERROR);
     } finally {
       client.getConnectionManager().shutdown();
       //setAgentState(AgentState.IDLE);
@@ -622,7 +568,7 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     if (stateService != null) {
       stateService.setAgentState(agentState);
     } else {
-      logger.warn("State service for capture agent is null, unable to push updates to remote server!");
+      logger.warn("State service for capture agent is null, unable to push updates to remote server!  This is only a problem if you see this message repeating.");
     }
   }
 
@@ -644,7 +590,7 @@ public class CaptureAgentImpl implements CaptureAgent, ManagedService {
     if (stateService != null) {
       stateService.setRecordingState(recordingID, state);
     } else {
-      logger.warn("State service for capture agent is null, unable to push updates to remote server!");
+      logger.warn("State service for capture agent is null, unable to push updates to remote server!  This is only a problem if you see this message repeating.");
     }
   }
 
