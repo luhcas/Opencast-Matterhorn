8759d4be99fed7a713caec61e1ce47bcbaf1318a
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-18 14:22:59 +0000

    MH-2116 Creating 0.5-SNAPSHOT Branch
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4597 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-conductor/branches/0.5-SNAPSHOT/src/main/java/org/opencastproject/conductor/impl/InspectWorkflowOperationHandler.java
@@ -0,0 +1,78 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.conductor.impl;
+
+import org.opencastproject.inspection.api.MediaInspectionService;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageException;
+import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.media.mediapackage.UnsupportedElementException;
+import org.opencastproject.workflow.api.WorkflowBuilder;
+import org.opencastproject.workflow.api.WorkflowInstance;
+import org.opencastproject.workflow.api.WorkflowOperationException;
+import org.opencastproject.workflow.api.WorkflowOperationHandler;
+import org.opencastproject.workflow.api.WorkflowOperationResult;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Workflow operation used to inspect all tracks of a media package.
+ */
+public class InspectWorkflowOperationHandler implements
+        WorkflowOperationHandler {
+
+  /** The logging facility */
+  private static final Logger logger = LoggerFactory.getLogger(ComposeWorkflowOperationHandler.class);
+
+  /** The inspection service */
+  private MediaInspectionService inspectionService = null;
+
+  /**
+   * Callback for the OSGi declarative services configuration.
+   * 
+   * @param inspectionService
+   *          the inspection service
+   */
+  protected void setInspectionService(MediaInspectionService inspectionService) {
+    this.inspectionService = inspectionService;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.opencastproject.workflow.api.WorkflowOperationHandler#run(org.opencastproject.workflow.api.WorkflowInstance)
+   */
+  @Override
+  public WorkflowOperationResult run(WorkflowInstance workflowInstance)
+          throws WorkflowOperationException {
+    MediaPackage mediaPackage = workflowInstance.getCurrentMediaPackage();
+    for (Track track : mediaPackage.getTracks()) {
+      logger.info("Inspecting track '{}' of {}", track.getIdentifier(), mediaPackage);
+      Track inspectedTrack = inspectionService.enrich(track, false);
+      try {
+        mediaPackage.remove(track);
+        mediaPackage.add(inspectedTrack);
+      } catch (MediaPackageException e) {
+        logger.error("Error modifying media package {}", mediaPackage, e);
+      } catch (UnsupportedElementException e) {
+        logger.error("Error adding {} to media package", inspectedTrack, e);
+      }
+    }
+    return WorkflowBuilder.getInstance().buildWorkflowOperationResult(mediaPackage, null, false);
+
+  }
+
+}
