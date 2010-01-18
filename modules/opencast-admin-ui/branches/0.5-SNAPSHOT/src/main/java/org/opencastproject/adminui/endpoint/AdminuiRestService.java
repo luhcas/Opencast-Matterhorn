6746067ce8e3d998ab7b3b01bc7b115849c2d697
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-18 14:18:43 +0000

    MH-2116 Creating 0.5-SNAPSHOT Branch
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4585 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-admin-ui/branches/0.5-SNAPSHOT/src/main/java/org/opencastproject/adminui/endpoint/AdminuiRestService.java
@@ -0,0 +1,317 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.adminui.endpoint;
+
+import org.opencastproject.adminui.api.RecordingDataView;
+import org.opencastproject.adminui.api.RecordingDataViewImpl;
+import org.opencastproject.adminui.api.RecordingDataViewList;
+import org.opencastproject.adminui.api.RecordingDataViewListImpl;
+import org.opencastproject.capture.admin.api.AgentState;
+import org.opencastproject.capture.admin.api.CaptureAgentStateService;
+import org.opencastproject.capture.admin.api.Recording;
+import org.opencastproject.media.mediapackage.Catalog;
+import org.opencastproject.media.mediapackage.DublinCoreCatalog;
+import org.opencastproject.media.mediapackage.EName;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageReferenceImpl;
+import org.opencastproject.scheduler.api.SchedulerEvent;
+import org.opencastproject.scheduler.api.SchedulerService;
+import org.opencastproject.workflow.api.WorkflowInstance;
+import org.opencastproject.workflow.api.WorkflowOperationInstance;
+import org.opencastproject.workflow.api.WorkflowService;
+import org.opencastproject.workflow.api.WorkflowInstance.State;
+
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST endpoint for the Admin UI proxy service
+ */
+@Path("/")
+public class AdminuiRestService {
+  private static final Logger logger = LoggerFactory.getLogger(AdminuiRestService.class);
+
+  private SchedulerService schedulerService;
+  private WorkflowService workflowService;
+  private CaptureAgentStateService captureAdminService;
+
+  public void setSchedulerService(SchedulerService service) {
+    logger.info("binding SchedulerService");
+    schedulerService = service;
+  }
+
+  public void unsetSchedulerService(SchedulerService service) {
+    logger.info("unbinding SchedulerService");
+    schedulerService = null;
+  }
+
+  public void setWorkflowService(WorkflowService service) {
+    logger.info("binding WorkflowService");
+    workflowService = service;
+  }
+
+  public void unsetWorkflowService(WorkflowService service) {
+    logger.info("unbinding WorkflowService");
+    workflowService = null;
+  }
+
+  public void setCaptureAdminService(CaptureAgentStateService service) {
+    logger.info("binding CaptureAgentStatusService");
+    captureAdminService = service;
+  }
+
+  public void unsetCaptureAdminService(CaptureAgentStateService service) {
+    logger.info("unbinding CaptureAgentStatusService");
+    captureAdminService = null;
+  }
+
+
+  /**
+   * Returns a list of recordings in a certain state.
+   * @param state state according to which the recordings should filtered
+   * @return recordings list of recordings in specified state
+   */
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  @Path("recordings/{state}")
+  public RecordingDataViewListImpl getRecordings(@PathParam("state") String state) {
+    RecordingDataViewListImpl out = new RecordingDataViewListImpl();
+    if ( (state.toUpperCase().equals("UPCOMING")) || (state.toUpperCase().equals("ALL")) ) {
+      out.addAll(getUpcomingRecordings());
+    }
+    if ( (state.toUpperCase().equals("PROCESSING")) || (state.toUpperCase().equals("ALL")) ) {
+      out.addAll(getRecordingsFromWorkflowService(State.RUNNING));
+    }
+    if ( (state.toUpperCase().equals("FINISHED")) || (state.toUpperCase().equals("ALL")) ) {
+      out.addAll(getRecordingsFromWorkflowService(State.SUCCEEDED));
+    }
+    if ( (state.toUpperCase().equals("FAILED")) || (state.toUpperCase().equals("ALL")) ) {
+      out.addAll(getRecordingsFromWorkflowService(State.FAILED));
+      out.addAll(getRecordingsFromWorkflowService(State.FAILING));
+    }
+    return out;
+  }
+
+  /**
+   * returns a RecordingDataViewList of recordings that are currently begin processed. 
+   * If the WorkflowService is not present an empty list is returned.
+   * @return RecordingDataViewList list of upcoming recordings
+   */
+  private RecordingDataViewList getRecordingsFromWorkflowService(State state) {
+    RecordingDataViewList out = new RecordingDataViewListImpl();
+    if (workflowService != null) {
+      logger.info("getting currently processed/finished recordings from workflowService");
+      WorkflowInstance[] workflows = workflowService.getWorkflowInstances(workflowService.newWorkflowQuery().withState(state)).getItems();
+      // next line is for debuging: return all workflowInstaces
+      //WorkflowInstance[] workflows = workflowService.getWorkflowInstances(workflowService.newWorkflowQuery()).getItems();
+      for (int i = 0; i < workflows.length; i++) {
+        RecordingDataView item = new RecordingDataViewImpl();
+        item.setId(workflows[i].getId());
+        item.setId(workflows[i].getId());
+        DublinCoreCatalog dcCatalog = getDublinCore(workflows[i].getCurrentMediaPackage());
+        if (dcCatalog != null) {
+          item.setTitle(getDublinCoreProperty(dcCatalog, DublinCoreCatalog.PROPERTY_TITLE));
+          item.setPresenter(getDublinCoreProperty(dcCatalog, DublinCoreCatalog.PROPERTY_CREATOR));
+          item.setSeries(getDublinCoreProperty(dcCatalog, DublinCoreCatalog.PROPERTY_IS_PART_OF));
+          item.setStartTime(getDublinCoreProperty(dcCatalog, DublinCoreCatalog.PROPERTY_DATE));  // FIXME get timestamp
+          item.setCaptureAgent(getDublinCoreProperty(dcCatalog, DublinCoreCatalog.PROPERTY_SPATIAL)); //FIXME get capture agent from where...?
+          WorkflowOperationInstance operation = workflows[i].getCurrentOperation();
+          if (operation != null) {
+            item.setProcessingStatus(operation.getName());
+          }
+          // TODO get distribution status #openquestion is there a way to find out if a workflowOperation does distribution?
+          out.add(item);
+        } else {
+          logger.warn("MediaPackage has no Catalog");
+        }
+      }
+    } else {
+      logger.warn("WorkflowService not present, returning empty list");
+    }
+    return out;
+  }
+
+  /**
+   * copied from WorkflowRestService
+   * @param mediaPackage
+   * @return
+   */
+  protected DublinCoreCatalog getDublinCore(MediaPackage mediaPackage) {
+    Catalog[] dcCatalogs = mediaPackage.getCatalogs(DublinCoreCatalog.FLAVOR, MediaPackageReferenceImpl.ANY_MEDIAPACKAGE);
+    if(dcCatalogs.length == 0) return null;
+    return (DublinCoreCatalog)dcCatalogs[0];
+  }
+
+  /**
+   * copied from WorkflowRestService
+   * @param catalog
+   * @param property
+   * @return
+   */
+  protected String getDublinCoreProperty(DublinCoreCatalog catalog, EName property) {
+    if(catalog == null) return null;
+    return catalog.getFirst(property, DublinCoreCatalog.LANGUAGE_ANY);
+  }
+
+  /**
+   * Retruns simple statistics about "recordings" in the system
+   * @return simple statistics about "recordings" in the system
+   */
+  @SuppressWarnings("unchecked")
+  @GET
+  @Path("countRecordings")
+  public Response countRecordings() {
+    HashMap<String,Integer> stats = getRecordingsStatistic();
+    Iterator<Entry<String,Integer>> i = stats.entrySet().iterator();
+    JSONObject out = new JSONObject();
+    while (i.hasNext()) {
+      Entry<String,Integer> ent = i.next();
+      out.put(ent.getKey(), ent.getValue());
+    }
+    return Response.ok(out.toJSONString()).header("Content-Type", MediaType.APPLICATION_JSON).build();
+  }
+
+  /**
+   * returns a statistic about number and state of recordings in the system
+   * @return statistic about number and state of recordings in the system
+   */
+  public HashMap<String,Integer> getRecordingsStatistic() {
+    String logMessage = "got statistics from: ";
+    HashMap<String,Integer> out = new HashMap<String,Integer>();
+    Integer total = new Integer(0);
+
+    // get number of upcoming recordings if scheduler is present
+    if (schedulerService != null) {
+      SchedulerEvent[] events = schedulerService.getUpcomingEvents();
+      out.put("upcoming", new Integer(events.length));
+      logMessage += "scheduler-service";
+      total += events.length;
+    } else {
+      logger.warn("scheduler service not present, unable to retreive number of upcoming events");
+    }
+
+    // get statistics from capture admin if present
+    if (captureAdminService != null) {
+      Map<String,Recording> recordings = captureAdminService.getKnownRecordings();
+      Iterator<String> i = recordings.keySet().iterator();
+      int capturing = 0;
+      while (i.hasNext()) {
+        if (recordings.get(i.next()).getState().equals(AgentState.CAPTURING)) {
+          capturing++;
+        }
+      }
+      out.put("capturing", new Integer(capturing));
+      total += capturing;
+      logMessage += " capture-admin-service";
+    } else {
+      logger.warn("CaptureAdmin service not present, unable to retrieve capture statistics");
+    }
+
+    // get statistics from workflowService if present
+    if (workflowService != null) {
+      WorkflowInstance[] workflows = workflowService.getWorkflowInstances(workflowService.newWorkflowQuery()).getItems();
+      int i = 0, processing = 0, inactive = 0, finished = 0, errors = 0;
+      for (; i < workflows.length; i++) {
+        switch (workflows[i].getState()) {
+          case FAILED:
+          case FAILING:
+            errors++;
+            break;
+          case INSTANTIATED:
+          case RUNNING:
+            processing++;
+            break;
+          case PAUSED:
+          case STOPPED:
+            inactive++; 
+            break;
+          case SUCCEEDED:
+            finished++;
+        }
+      }
+      out.put("processing", Integer.valueOf(processing));
+      out.put("inactive", Integer.valueOf(inactive));
+      out.put("errors", Integer.valueOf(errors));
+      out.put("finished", Integer.valueOf(finished));
+      total += i;
+      logMessage += " workflow-service";
+    } else {
+      logger.warn("workflow service not present, unable to retrieve workflow statistics");
+    }
+    out.put("total", total);
+    logger.info(logMessage);
+    return out;
+  }
+
+  /**
+   * returns a RecordingDataViewList of upcoming events. If the schedulerService
+   * is not present an empty list is returned.
+   * @return RecordingDataViewList list of upcoming recordings
+   */
+  private RecordingDataViewList getUpcomingRecordings() {
+    RecordingDataViewList out = new RecordingDataViewListImpl();
+    if (schedulerService != null) {
+      logger.info("getting upcoming recordings from scheudler");
+      SchedulerEvent[] events = schedulerService.getUpcomingEvents();
+      for (int i = 0; i < events.length; i++) {
+        RecordingDataView item = new RecordingDataViewImpl();
+        item.setId(events[i].getID());
+        item.setId(events[i].getID());
+        item.setTitle(events[i].getTitle());
+        item.setPresenter(events[i].getCreator());
+        item.setSeries(events[i].getSeriesID());    // FIXME get title for seriesID -- not used by now
+        item.setStartTime(Long.toString(events[i].getStartdate().getTime()));
+        item.setStartTime(Long.toString(events[i].getEnddate().getTime()));
+        item.setCaptureAgent(events[i].getDevice());
+        item.setProcessingStatus("scheduled");
+        item.setDistributionStatus("not distributed");
+        out.add(item);
+      }
+    } else {
+      logger.warn("scheduler not present, returning empty list");
+    }
+    return out;
+  }
+
+  /**
+   * @return documentation for this endpoint
+   */
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("docs")
+  public String getDocumentation() {
+    return docs;
+  }
+
+  protected final String docs;
+  
+  public AdminuiRestService() {
+    docs = "FIXME -- add documentation";
+  }
+}
---
+++ modules/opencast-admin-ui/branches/0.5-SNAPSHOT/src/main/resources/ui/js/scheduler.js
@@ -0,0 +1,450 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+// Configurable Page Variables
+var BASE_URL          = '';
+var SCHEDULER_URL     = BASE_URL + '/scheduler/rest';
+var WORKFLOW_URL      = BASE_URL + '/workflow/rest';
+var CAPTURE_ADMIN_URL = BASE_URL + '/capture-admin/rest';
+
+/**
+ *  EventManager Object
+ *  Handles form validation, serialization, and form value population
+ *  @class
+ */
+function EventManager(eventFields){
+  //Contains an array of eventFields
+  this.fields         = eventFields || new Array();
+  //Defines the root namespace for scheduler service
+  this.rootNS         = 'http://scheduler.opencastproject.org';
+  //Defines the root element of xml document used for scheudler service
+  this.rootEl         = 'scheduler-event';
+}
+
+/**
+ *  Create new rest document
+ *  @return {DOM Object} Empty DOM document
+ */
+function createDoc(){
+  var doc = null;
+  //Create a DOM Document, methods vary between browsers, e.g. IE and Firefox
+  if(document.implementation && document.implementation.createDocument){ //Firefox, Opera, Safari, Chrome, etc.
+    doc = document.implementation.createDocument(this.rootNS, this.rootEl, null);
+  }else{ // IE
+    doc = new ActiveXObject('MSXML2.DOMDocument');
+    doc.loadXML('<' + this.rootEl + ' xmlns="' + this.rootNS + '"></' + this.rootEl + '>');
+  }
+  return doc;
+}
+//Set EventManager createDoc function
+EventManager.prototype.createDoc = createDoc;
+
+/** 
+ *  Validate the event form and display the notifcations box
+ *  @returns {boolean} True if the form value's are valid, otherwise false;
+ */
+function checkForm() {
+  //Todo: create some functions in schedulerUI to manipulate the notification box.
+  var missingFields = new Array();
+  for(var i in this.fields){
+    //if the field is required, and does not contain valid data notify user
+    $('#label-' + this.fields[i].id).css('color', 'black');
+    if(this.fields[i].required && !this.fields[i].checkValue()){
+      missingFields.push(i);
+    }
+  }
+  if(missingFields.length > 0){
+    $('#missingFields-container li').hide();
+    
+    $('#missingFields-container').show('fast');
+    for(var f in missingFields){
+      $('#missing-' + missingFields[f]).show();
+      $('#label-' + missingFields[f]).css('color', 'red');
+    }
+    return false;
+  }else{
+    $('#missingFields-container').hide('fast');
+    return true;
+  }
+}
+//Set EventManager checkForm function
+EventManager.prototype.checkForm = checkForm;
+
+/** 
+ *  Serialize the form into an XML file for consumption by the
+ *  scheduler service: addEvent
+ */
+function serialize() {
+  //Todo: EventField/Group should know how to render themselves. this is bad, refactor
+  if(this.checkForm()){
+    var doc = this.createDoc();
+    var metadata = doc.createElement('metadata');
+    for(var i in this.fields){
+      if(this.fields[i].getValue()){
+        el = doc.createElement(i);
+        if(i == "startdate" || i == "enddate" || i == "duration"){
+          el.appendChild(doc.createTextNode(this.fields[i].getValue().getTime()));
+          doc.documentElement.appendChild(el);
+        }else if(i == "attendees"){
+          var attendee = doc.createElement("attendee");
+          attendee.appendChild(doc.createTextNode(this.fields[i].getValue()));
+          el.appendChild(attendee);
+          doc.documentElement.appendChild(el);
+        }else if(i == "id"){
+          el.appendChild(doc.createTextNode(this.fields[i].getValue()));
+          doc.documentElement.appendChild(el);
+        }else if(i == "resources"){
+          var resource = doc.createElement("resource");
+          resource.appendChild(doc.createTextNode(this.fields[i].getValue()));
+          el.appendChild(resource);
+          doc.documentElement.appendChild(el);
+        }else{
+          var item = doc.createElement('item');
+          var val = doc.createElement('value');
+          val.appendChild(doc.createTextNode(this.fields[i].getValue()));
+          item.setAttribute('key', i);
+          item.appendChild(val);
+          metadata.appendChild(item);
+        }
+      }
+    }
+    doc.documentElement.appendChild(metadata);
+    if(typeof XMLSerializer != 'undefined'){
+      return (new XMLSerializer()).serializeToString(doc);
+    }else if(doc.xml){ return doc.xml; }
+    else{ throw "Unable to serialize SchedulerEvent."; }
+  }else{
+    //error handle
+  }
+}
+//Set EventManager serialize function
+EventManager.prototype.serialize = serialize;
+
+/**
+ *  Populate form with values from an existing event.
+ *  @param {DOM Object}
+ *  
+ *  This approach needs more work. Probably need custom deserializing and
+ *  serilizing functions for each EventField/Group.
+ */
+function populateForm(document){
+  for(var e in this.fields){
+    //Todo: select the agent field when loading an event.
+    if(e != "resources" || e != "attendees" || e != "channel-id"){
+      switch(e){
+        case 'startdate':
+          this.fields[e].setValue(new Date(parseInt($("startdate", document).text())));
+          break;
+        case 'enddate': //we have to know the start date before we can use the enddate to sort out duration. This is dumb, talk to rudiger about switching to just use duration.
+          this.fields[e].setValue(parseInt($("duration", document).text()));
+          break;
+        case 'id':
+          this.fields[e].setValue($("id", document).text());
+          break;
+        default:
+          this.fields[e].setValue($("item[key='" + e + "'] > value", document).text());
+      }
+      /*
+      if(this.fields[e] instanceof EventField){
+        this.fields[e].setValue($("item[key='" + e + "'] > value", document).text());
+      }else{
+        //bad hack, refactor
+        
+        //Dealing with resources(inputs) aka capabilities is uncertain at this point.
+        //Deal with this for editing later.
+        /*
+        console.log(e);
+        var o = {};
+        o[e] = $("item[key='" + e + "'] > value", document).text();
+        this.fields[e].setValue(o);
+      } */
+    }
+  }
+}
+//Set EventManager populate form
+EventManager.prototype.populateForm = populateForm;
+
+/**
+ *  EventField Object
+ *  Encapsulates form fields, provides validation, get/set value,
+ *  and indicating if the field is required or optional.
+ *  @class
+ */
+function EventField(id, required, getValue, setValue, checkValue){
+  if(id != "" && $('#' + id)[0]){
+    this.id = id;
+    this.formElement = $('#' + id);
+  }else{
+    throw "Unable to find field " + id;
+  }
+  this.required   = required || false;
+  this.value      = null;
+
+  // If getValue is specified, override the default getValue.
+  if($.isFunction(getValue)){
+    this.getValue = getValue;
+  }
+  
+  // If setValue is specified, override the default setValue.
+  if($.isFunction(setValue)){
+    this.setValue = setValue;
+  }
+  
+  // If checkValue is specified, override the default checkValue.
+  if($.isFunction(checkValue)){
+    this.checkValue = checkValue;
+  }
+}
+
+/**
+ *  get the value of an event form field
+ *  @return {string} 
+ */
+function getEventFieldValue() {
+  if(this.formElement){
+    this.value = this.formElement.val()
+  }
+  return this.value;
+}
+//Set EventField getValue function
+EventField.prototype.getValue = getEventFieldValue;
+
+/**
+ *  set the value of an event form field
+ *  @param {string}
+ */
+function setEventFieldValue(val) {
+  this.value = val;
+  this.formElement.val(val);
+}
+//Set EventField setValue function
+EventField.prototype.setValue = setEventFieldValue;
+
+/**
+ *  validate the value of an event form field
+ *  @return {boolean} True if the field is valid, otherwise false.
+ */
+function checkEventFieldValue() {
+  if(this.getValue()){
+    return true;
+  }
+  return false;
+}
+//Set EventField checkValue function
+EventField.prototype.checkValue = checkEventFieldValue;
+
+
+/**
+ *  Overrides the default getValue function of EventField for checkboxes
+ *  @return {string} Value of a checkbox that is checked.
+ */
+function getCheckboxValue() {
+  if(this.formElement[0].checked){
+    return this.formElement.val();
+  }
+  return "";
+}
+
+/**
+ *  EventFieldGroup Object
+ *  Container for a group of EventFields that need to be evaluated
+ *  together.
+ *  @class
+ */
+function EventFieldGroup(idArray, required, getValue, setValue, checkValue) {
+  this.groupElements = new Array();
+  if($.isArray(idArray)){
+    for(var i in idArray){
+      if(!$('#' + idArray)[0]){
+        throw "Unable to find field " + idArray[i];
+      }
+      this.groupElements[idArray[i]] = $('#' + idArray[i]);
+    }
+  }else{
+    throw "EventFieldGroup idArray must not be empty.";
+  }
+  
+  // Is this field required for form validation?
+  this.required = required || false;
+  
+  // If getValue is specified, override the default getValue.
+  if($.isFunction(getValue)){
+    this.getValue = getValue;
+  }
+  
+  // If setValue is specified, override the default setValue.
+  if($.isFunction(setValue)){
+    this.setValue = setValue;
+  }
+  
+  // If checkValue is specified, override the default checkValue.
+  if($.isFunction(checkValue)){
+    this.checkValue = checkValue;
+  }
+}
+
+/**
+ *  get an string of each field's value.
+ *  @return {string} a string of values, seperated by ','
+ */
+function getEventFieldGroupValue() {
+  
+  values = new Array();
+  for(var el in this.groupElements){
+    if(this.groupElements[el][0].checked){
+      values.push(this.groupElements[el].val());
+    }
+  }
+  if(values.length > 0){
+    this.value = values.toString();
+  }
+  return this.value;
+}
+//Set EventFieldGroup getValue function
+EventFieldGroup.prototype.getValue = getEventFieldGroupValue;
+
+/**
+ *  set the values of each field in the group
+ *  @param {Object} Object of key/value pairs, where the key is the id of an EventFieldGroup groupElement
+ */
+function setEventFieldGroupValue(values) {
+  for(var el in values){
+    if(this.groupElements[el]){
+      this.groupElements[el][0].checked = values[el];
+    }
+  }
+  
+}//Set EventFieldGroup setValue function
+EventFieldGroup.prototype.setValue = setEventFieldGroupValue;
+
+/**
+ *  validate an EventFieldGroup
+ *  @return {boolean} True if EventFieldGroup valid, otherwise false
+ */
+function checkEventFieldGroupValue() {
+  oneIsChecked = false;
+  for(var el in this.groupElements){
+    oneIsChecked = this.groupElements[el][0].checked || oneIsChecked;
+  }
+  if(this.required && oneIsChecked){
+    return true;
+  }
+  return false;
+}
+//Set EventFieldGroup checkValue function
+EventFieldGroup.prototype.checkValue = checkEventFieldGroupValue;
+
+// ===== Custom get/set/check functions for EventFieldGroup =====
+
+/**
+ *  Overrides getValue of EventFieldGroup for duration fields
+ *  @return {Date Object} Date object, start date, incremented by duration.
+ */
+function getDuration() {
+  //to do: add duration to starttime, and return the end time.
+  if(this.checkValue()){
+    duration = this.groupElements['durationHour'].val() * 3600; // seconds per hour
+    duration += this.groupElements['durationMin'].val() * 60; // seconds per min
+    //get the start date, increment it by the duration(in seconds), convert to milliseconds. bad coupling...refactor
+    this.value = new Date( eventsManager.fields['startdate'].getValue().getTime() + (duration * 1000) );
+  }
+  return this.value;
+}
+
+/**
+ *  Overrides setValue for EventFieldGroup for duration fields
+ *  @param {integer} Duration in milliseconds
+ */
+function setDuration(val) {
+  if(this.groupElements['durationHour'] && this.groupElements['durationMin']){
+    val = val/1000; //milliseconds -> seconds
+    var hour  = Math.floor(val/3600);
+    var min   = Math.floor((val/60) % 60);
+    this.groupElements['durationHour'].val(hour);
+    this.groupElements['durationMin'].val(min);
+  }
+}
+
+/**
+ *  Overrides checkValue for EventFieldGroup for duration fields
+ *  @return {boolean} True if the duration is valid, otherwise false.
+ */
+function checkDuration(){
+  if(this.groupElements['durationHour'] && this.groupElements['durationMin'] &&
+     (this.groupElements['durationHour'].val() !== "0" || this.groupElements['durationMin'].val() !== "0")){
+    return true;
+  }
+  return false;
+}
+
+/**
+ *  Overrides getValue for EventFieldGroup for startDate fields
+ *  @return {Date Object}
+ */
+function getStartDate(){
+  var date = 0;
+  if(this.checkValue()){
+    date = this.groupElements['startDate'].datepicker('getDate').getTime() / 1000; // Get date in milliseconds, convert to seconds.
+    date += this.groupElements['startTimeHour'].val() * 3600; // convert hour to seconds, add to date.
+    date += this.groupElements['startTimeMin'].val() * 60; //convert minutes to seconds, add to date.
+    date = date * 1000; //back to milliseconds
+    this.value = new Date(date);
+  }
+  
+  return this.value; //TODO: set seconds/milliseconds to 0.
+}
+
+/**
+ *  Overrides setValue for EventFieldGroup for startDate fields
+ *  @param {Date Object}
+ */
+function setStartDate(date){
+  if(this.groupElements['startDate'] && this.groupElements['startTimeHour'] && this.groupElements['startTimeMin']){
+    var hour = date.getHours();
+    //console.log('Hours: ' + hour);
+    
+    //console.log("Minutes: " + date.getMinutes());
+    this.groupElements['startTimeHour'].val(hour);
+    this.groupElements['startTimeMin'].val(date.getMinutes());
+    
+    //datepicker modifies the date object removing the time.
+    this.groupElements['startDate'].datepicker('setDate', date);
+  }
+}
+
+/**
+ *  Overrides checkValue for EventFieldGroup for startDate fields
+ *  @return {boolean} True if the startDate is valid, otherwise false.
+ */
+function checkStartDate(){
+  var date = this.groupElements['startDate'].datepicker('getDate');
+  var now = new Date();
+  if( this.groupElements['startDate'] &&
+      date &&
+      this.groupElements['startTimeHour'] &&
+      this.groupElements['startTimeMin']){
+    //console.log(date.getDate() + ":" + now.getDate() + "," + date.getMonth() + ":" + now.getMonth() + "," + date.getYear() + ":" + now.getYear() + "," + this.groupElements['startTimeHour'].val() + ":" + now.getHours());
+    if(date.getDate() >= now.getDate() && 
+       date.getMonth() >= now.getMonth() &&
+       date.getYear() >= now.getYear() &&
+       this.groupElements['startTimeHour'].val() > now.getHours()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  return false;
+}
