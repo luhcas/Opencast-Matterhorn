3a86d964e304fda53cab85d7e4c1f2c0c185fbeb
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-18 14:29:31 +0000

    MH-2116 Creating 0.5-SNAPSHOT Branch
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4615 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-inspection-service-impl/branches/0.5-SNAPSHOT/src/main/java/org/opencastproject/inspection/impl/MediaInspectionServiceImpl.java
@@ -0,0 +1,259 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.inspection.impl;
+
+import org.opencastproject.inspection.api.MediaInspectionService;
+import org.opencastproject.media.mediapackage.AbstractMediaPackageElement;
+import org.opencastproject.media.mediapackage.MediaPackageElement;
+import org.opencastproject.media.mediapackage.MediaPackageElementBuilder;
+import org.opencastproject.media.mediapackage.MediaPackageElementBuilderFactory;
+import org.opencastproject.media.mediapackage.MediaPackageElementFlavor;
+import org.opencastproject.media.mediapackage.MediaPackageElements;
+import org.opencastproject.media.mediapackage.Stream;
+import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.media.mediapackage.UnsupportedElementException;
+import org.opencastproject.media.mediapackage.MediaPackageElement.Type;
+import org.opencastproject.media.mediapackage.track.AudioStreamImpl;
+import org.opencastproject.media.mediapackage.track.TrackImpl;
+import org.opencastproject.media.mediapackage.track.VideoStreamImpl;
+import org.opencastproject.util.Checksum;
+import org.opencastproject.util.ChecksumType;
+import org.opencastproject.workspace.api.Workspace;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.security.NoSuchAlgorithmException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * Inspects media via the 3rd party MediaInfo tool by default, and can be configured to use other media analyzers.
+ */
+public class MediaInspectionServiceImpl implements MediaInspectionService, ManagedService {
+  private static final Logger logger = LoggerFactory.getLogger(MediaInspectionServiceImpl.class);
+
+  Workspace workspace;
+
+  public void setWorkspace(Workspace workspace) {
+    logger.debug("setting " + workspace);
+    this.workspace = workspace;
+  }
+
+  public void unsetWorkspace(Workspace workspace) {
+    logger.debug("unsetting " + workspace);
+  }
+
+  public Track inspect(URI uri) {
+    logger.debug("inspect(" + uri + ") called, using workspace " + workspace);
+
+    // Get the file from the URL
+    File file = workspace.get(uri);
+
+    MediaContainerMetadata metadata = null;
+    try {
+      MediaAnalyzerFactory analyzerFactory = MediaAnalyzerFactory.newInstance();
+      MediaAnalyzer mediaAnalyzer = analyzerFactory.newMediaAnalyzer();
+      metadata = mediaAnalyzer.analyze(file);
+    } catch (Throwable t) {
+      throw new IllegalStateException("Unable to create media analyzer", t);
+    }
+
+    if (metadata == null) {
+      logger.warn("Unable to acquire media metadata for " + uri);
+      return null;
+    } else {
+      MediaPackageElementBuilder elementBuilder = MediaPackageElementBuilderFactory.newInstance().newElementBuilder();
+      TrackImpl track;
+        MediaPackageElement element;
+        try {
+          element = elementBuilder.elementFromURI(uri, Type.Track,
+                  MediaPackageElements.INDEFINITE_TRACK);
+        } catch (UnsupportedElementException e) {
+          throw new RuntimeException(e);
+        }
+        track = (TrackImpl) element;
+        if (metadata.getDuration() != null)
+          track.setDuration(metadata.getDuration());
+        try {
+          track.setChecksum(Checksum.create(ChecksumType.DEFAULT_TYPE, file));
+        } catch (NoSuchAlgorithmException e) {
+          throw new RuntimeException(e);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+        List<AudioStreamMetadata> audioList = metadata.getAudioStreamMetadata();
+        if (audioList != null && !audioList.isEmpty()) {
+          for (int i = 0; i < audioList.size(); i++) {
+            AudioStreamImpl audio = new AudioStreamImpl("audio-" + (i + 1));
+            AudioStreamMetadata a = audioList.get(i);
+            audio.setBitRate(a.getBitRate());
+            audio.setChannels(a.getChannels());
+            audio.setFormat(a.getFormat());
+            audio.setFormatVersion(a.getFormatVersion());
+            audio.setResolution(a.getResolution());
+            audio.setSamplingRate(a.getSamplingRate());
+            track.addStream(audio);
+          }
+        }
+        List<VideoStreamMetadata> videoList = metadata.getVideoStreamMetadata();
+        if (videoList != null && !videoList.isEmpty()) {
+          for (int i = 0; i < audioList.size(); i++) {
+            VideoStreamImpl video = new VideoStreamImpl("video-" + (i + 1));
+            VideoStreamMetadata v = videoList.get(i);
+            video.setBitRate(v.getBitRate());
+            video.setFormat(v.getFormat());
+            video.setFormatVersion(v.getFormatVersion());
+            video.setFrameHeight(v.getFrameHeight());
+            video.setFrameRate(v.getFrameRate());
+            video.setFrameWidth(v.getFrameWidth());
+            video.setScanOrder(v.getScanOrder());
+            video.setScanType(v.getScanType());
+            track.addStream(video);
+          }
+        }
+        // FIXME: how should we determine flavor?
+      return track;
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.opencastproject.inspection.api.MediaInspectionService#enrich(Track, Boolean)
+   */
+  public Track enrich(Track originalTrack, Boolean override) {
+    URI originalTrackUrl = originalTrack.getURI();
+    MediaPackageElementFlavor flavor = originalTrack.getFlavor() == null ? MediaPackageElements.INDEFINITE_TRACK : originalTrack.getFlavor();
+    logger.debug("enrych(" + originalTrackUrl + ") called");
+
+    // Get the file from the URL
+    File file = workspace.get(originalTrackUrl);
+
+    MediaContainerMetadata metadata = null;
+    try {
+      MediaAnalyzerFactory analyzerFactory = MediaAnalyzerFactory.newInstance();
+      MediaAnalyzer mediaAnalyzer = analyzerFactory.newMediaAnalyzer();
+      metadata = mediaAnalyzer.analyze(file);
+    } catch (Throwable t) {
+      throw new IllegalStateException("Unable to create media analyzer", t);
+    }
+
+    if (metadata == null) {
+      logger.warn("Unable to acquire media metadata for " + originalTrackUrl);
+      return null;
+    } else {
+      TrackImpl track = null;
+      try {
+        track = (TrackImpl) MediaPackageElementBuilderFactory.newInstance().newElementBuilder()
+          .elementFromURI(originalTrackUrl, Type.Track, flavor);
+      } catch (UnsupportedElementException e) {
+        throw new RuntimeException(e);
+      }
+      // init the new track with old
+      track.setChecksum(originalTrack.getChecksum());
+      track.setDuration(originalTrack.getDuration());
+      track.setElementDescription(originalTrack.getElementDescription());
+      track.setFlavor(flavor);
+      track.setIdentifier(originalTrack.getIdentifier());
+      track.setMimeType(originalTrack.getMimeType());
+      track.setReference(originalTrack.getReference());
+      track.setSize(originalTrack.getSize());
+      track.setURI(originalTrackUrl);
+      // enrich the new track with basic info
+      if (track.getDuration() == -1L || override)
+        track.setDuration(metadata.getDuration());
+      if (track.getChecksum() == null || override)
+        try {
+          track.setChecksum(Checksum.create(ChecksumType.DEFAULT_TYPE, file));
+        } catch (NoSuchAlgorithmException e) {
+          throw new RuntimeException(e);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      // find all streams
+      Dictionary<String, Stream> streamsId2Stream = new Hashtable<String, Stream>();
+      for (Stream stream : originalTrack.getStreams()) {
+        streamsId2Stream.put(stream.getIdentifier(), stream);
+      }
+
+      // audio list
+      List<AudioStreamMetadata> audioList = metadata.getAudioStreamMetadata();
+      if (audioList != null && !audioList.isEmpty()) {
+        for (int i = 0; i < audioList.size(); i++) {
+          AudioStreamImpl audio = new AudioStreamImpl("audio-" + (i + 1));
+          AudioStreamMetadata a = audioList.get(i);
+          audio.setBitRate(a.getBitRate());
+          audio.setChannels(a.getChannels());
+          audio.setFormat(a.getFormat());
+          audio.setFormatVersion(a.getFormatVersion());
+          audio.setResolution(a.getResolution());
+          audio.setSamplingRate(a.getSamplingRate());
+          // TODO: retain the original audio metadata
+          track.addStream(audio);
+        }
+      }
+      // video list
+      List<VideoStreamMetadata> videoList = metadata.getVideoStreamMetadata();
+      if (videoList != null && !videoList.isEmpty()) {
+        for (int i = 0; i < videoList.size(); i++) {
+          VideoStreamImpl video = new VideoStreamImpl("video-" + (i + 1));
+          VideoStreamMetadata v = videoList.get(i);
+          video.setBitRate(v.getBitRate());
+          video.setFormat(v.getFormat());
+          video.setFormatVersion(v.getFormatVersion());
+          video.setFrameHeight(v.getFrameHeight());
+          video.setFrameRate(v.getFrameRate());
+          video.setFrameWidth(v.getFrameWidth());
+          video.setScanOrder(v.getScanOrder());
+          video.setScanType(v.getScanType());
+          // TODO: retain the original video metadata
+          track.addStream(video);
+        }
+      }
+      return track;
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.opencastproject.inspection.api.MediaInspectionService#enrich(AbstractMediaPackageElement, Boolean)
+   */
+  public AbstractMediaPackageElement enrich(AbstractMediaPackageElement element, Boolean override) {
+    File file = workspace.get(element.getURI());
+    if (element.getChecksum() == null || override)
+      try {
+        element.setChecksum(Checksum.create(ChecksumType.DEFAULT_TYPE, file));
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    return element;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void updated(Dictionary properties) throws ConfigurationException {
+    logger.info("Updating configuration on {}", this.getClass().getName());
+    // TODO Update the local path to the mediainfo binary
+  }
+
+}
