bea596894b203c26f605438f7abf468ef2f35547
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-18 14:30:13 +0000

    MH-2116 Creating 0.5-SNAPSHOT Branch
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4617 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-media/branches/0.5-SNAPSHOT/src/main/java/org/opencastproject/media/mediapackage/attachment/CoverImpl.java
@@ -0,0 +1,123 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+package org.opencastproject.media.mediapackage.attachment;
+
+import org.opencastproject.media.mediapackage.Attachment;
+import org.opencastproject.media.mediapackage.Cover;
+import org.opencastproject.util.Checksum;
+import org.opencastproject.util.MimeType;
+
+import java.net.URI;
+
+/**
+ * This is a specialized implementation for a media package cover.
+ * 
+ * @author Tobias Wunden
+ * @version $Id
+ */
+
+public class CoverImpl extends AttachmentImpl implements Cover {
+
+  /** Serial Version UID */
+  private static final long serialVersionUID = -7420968157749682999L;
+
+  /**
+   * Creates a cover attachment.
+   * 
+   * @parm identifier the cover identifier
+   * @param uri
+   *          the cover location
+   * @param size
+   *          the cover size in bytes
+   * @param checksum
+   *          the cover checksum
+   * @param mimeType
+   *          the cover mime type
+   */
+  protected CoverImpl(String identifier, URI uri, long size, Checksum checksum, MimeType mimeType) {
+    super(identifier, Cover.FLAVOR, uri, size, checksum, mimeType);
+  }
+
+  /**
+   * Creates a cover attachment.
+   * 
+   * @param uri
+   *          the covers location
+   * @param size
+   *          the covers size in bytes
+   * @param checksum
+   *          the covers checksum
+   * @param mimeType
+   *          the covers mime type
+   */
+  protected CoverImpl(URI uri, long size, Checksum checksum, MimeType mimeType) {
+    super(Cover.FLAVOR, uri, size, checksum, mimeType);
+  }
+
+  /**
+   * Creates a cover attachment.
+   * 
+   * @param uri
+   *          the covers location
+   */
+  protected CoverImpl(URI uri) {
+    this(null, uri, -1, null, null);
+  }
+
+  /**
+   * Dresses the attachment as a {@link Cover}.
+   * 
+   * @param attachment
+   *          the general attachment representation
+   */
+  public static CoverImpl fromAttachment(Attachment attachment) {
+    return new CoverImpl(attachment.getIdentifier(), attachment.getURI(), attachment.getSize(), attachment
+            .getChecksum(), attachment.getMimeType());
+  }
+
+  /**
+   * Creates a new {@link Cover} object.
+   * 
+   * @param uri
+   *          the cover location
+   * @param size
+   *          the covers size in bytes
+   * @param checksum
+   *          the file checksum
+   * @param mimeType
+   *          the mime type
+   * @return the cover object
+   */
+  public static CoverImpl fromURI(URI uri) {
+    return new CoverImpl(uri);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.attachment.AttachmentImpl#toString()
+   */
+  @Override
+  public String toString() {
+    StringBuffer buf = new StringBuffer("cover");
+    if (getMimeType() != null) {
+      buf.append(" (");
+      buf.append(getMimeType());
+      buf.append(")");
+    }
+    return buf.toString();
+  }
+
+}
---
+++ modules/opencast-media/branches/0.5-SNAPSHOT/src/main/java/org/opencastproject/media/mediapackage/elementbuilder/PresenterTrackBuilderPlugin.java
@@ -0,0 +1,104 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+package org.opencastproject.media.mediapackage.elementbuilder;
+
+import org.opencastproject.media.mediapackage.MediaPackageElement;
+import org.opencastproject.media.mediapackage.MediaPackageElementFlavor;
+import org.opencastproject.media.mediapackage.MediaPackageElements;
+import org.opencastproject.media.mediapackage.UnsupportedElementException;
+import org.opencastproject.media.mediapackage.track.TrackImpl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Node;
+
+import java.net.URI;
+
+import javax.xml.xpath.XPathExpressionException;
+
+/**
+ * This implementation of the {@link MediaPackageElementBuilderPlugin} recognizes presenter tracks and provides the
+ * functionality of reading it on behalf of the media package.
+ */
+public class PresenterTrackBuilderPlugin extends AbstractTrackBuilderPlugin {
+
+  /** the logging facility provided by log4j */
+  private final static Logger log_ = LoggerFactory.getLogger(PresenterTrackBuilderPlugin.class.getName());
+
+  public PresenterTrackBuilderPlugin() throws IllegalStateException {
+    setPriority(1);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.MediaPackageElementBuilderPlugin#accept(org.opencastproject.media.mediapackage.MediaPackageElement.Type
+   *      , org.opencastproject.media.mediapackage.MediaPackageElementFlavor)
+   */
+  public boolean accept(MediaPackageElement.Type type, MediaPackageElementFlavor flavor) {
+    return type.equals(MediaPackageElement.Type.Track) && flavor.equals(MediaPackageElements.PRESENTER_TRACK);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.MediaPackageElementBuilderPlugin#accept(org.w3c.dom.Node)
+   */
+  public boolean accept(Node elementNode) {
+    try {
+      String name = elementNode.getNodeName();
+      String flavor = xpath.evaluate("@type", elementNode);
+      return name.equalsIgnoreCase(MediaPackageElement.Type.Track.toString())
+              && MediaPackageElements.PRESENTER_TRACK.eq(flavor);
+    } catch (XPathExpressionException e) {
+      return false;
+    }
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.elementbuilder.MediaPackageElementBuilderPlugin#accept(URI,
+   *      org.opencastproject.media.mediapackage.MediaPackageElement.Type,
+   *      org.opencastproject.media.mediapackage.MediaPackageElementFlavor)
+   */
+  public boolean accept(URI uri, MediaPackageElement.Type type, MediaPackageElementFlavor flavor) {
+    return type != null && flavor != null && MediaPackageElement.Type.Track.equals(type)
+            && MediaPackageElements.PRESENTER_TRACK.equals(flavor);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.elementbuilder.MediaPackageElementBuilderPlugin#elementFromURI(URI)
+   */
+  public MediaPackageElement elementFromURI(URI uri) throws UnsupportedElementException {
+    log_.trace("Creating presenter track from " + uri);
+    TrackImpl track = TrackImpl.fromURI(uri);
+    track.setFlavor(MediaPackageElements.PRESENTER_TRACK);
+    return track;
+  }
+
+  /**
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "Presenter Track Builder Plugin";
+  }
+
+  @Override
+  protected TrackImpl trackFromManifest(String id, URI uri) {
+    TrackImpl track = TrackImpl.fromURI(uri);
+    track.setIdentifier(id);
+    track.setFlavor(MediaPackageElements.PRESENTER_TRACK);
+    return track;
+  }
+
+}
