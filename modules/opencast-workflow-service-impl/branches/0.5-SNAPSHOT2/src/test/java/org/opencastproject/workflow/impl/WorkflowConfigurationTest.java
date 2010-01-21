8165a85bee1428ba0368b85c05accb633ef702fc
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-21 03:39:10 +0000

    MH-2193 Creating 0.5-SNAPSHOT2 Branch
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@4820 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-workflow-service-impl/branches/0.5-SNAPSHOT2/src/test/java/org/opencastproject/workflow/impl/WorkflowConfigurationTest.java
@@ -0,0 +1,46 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.workflow.impl;
+
+import org.opencastproject.workflow.api.WorkflowBuilder;
+import org.opencastproject.workflow.api.WorkflowInstanceImpl;
+import org.opencastproject.workflow.api.WorkflowConfiguration;
+import org.opencastproject.workflow.api.WorkflowConfigurationImpl;
+import org.opencastproject.workflow.api.WorkflowOperationInstanceImpl;
+import org.opencastproject.workflow.api.WorkflowOperationInstanceListImpl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WorkflowConfigurationTest {
+  @Test
+  public void testConfigurationSerialization() throws Exception {
+    WorkflowOperationInstanceImpl op = new WorkflowOperationInstanceImpl();
+    Set<WorkflowConfiguration> config = new HashSet<WorkflowConfiguration>();
+    config.add(new WorkflowConfigurationImpl("this", "that"));
+    op.setConfiguration(config);
+    WorkflowInstanceImpl instance = new WorkflowInstanceImpl();
+    WorkflowOperationInstanceListImpl ops = new WorkflowOperationInstanceListImpl();
+    ops.add(op);
+    instance.setWorkflowOperationInstanceList(ops);
+    String xml = WorkflowBuilder.getInstance().toXml(instance);
+    System.out.println(xml);
+    Assert.assertTrue(xml.contains("<configurations><configuration key=\"this\">that</configuration></configurations>"));
+  }
+}
