77dc4cfd7937c69dd12b89d1b5969a518da83bfe
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-22 22:40:55 +0000

    MH-2193 Creating 0.5-RC1 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5054 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-capture-service-api/tags/0.5-RC1/src/main/java/org/opencastproject/capture/api/StateService.java
@@ -0,0 +1,75 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.capture.api;
+
+import java.util.Map;
+
+import org.opencastproject.capture.admin.api.Agent;
+import org.opencastproject.capture.admin.api.Recording;
+
+/**
+ * Service for querying the capture agent's current state (MH-58)
+ */
+public interface StateService {
+
+  /**
+   * Gets the internal Agent used to store this agent's state
+   * @return The Agent which represents this capture agent
+   * @see org.opencastproject.capture.admin.api.Agent
+   */
+  public Agent getAgent();
+
+  //TODO: explain why we are passing strings instead of something more type safe
+  /**
+   * Sets the state of the agent.  Note that this should not change the *actual* state of the agent, only update the StateService's record of its state.
+   * @param state The state of the agent.  Should be defined in AgentState.
+   * @see org.opencastproject.capture.api.AgentState
+   */
+  public void setAgentState(String state);
+
+  /**
+   * Gets the state of the agent
+   * @return The state of the agent (should be defined in AgentState)
+   * @see org.opencastproject.capture.api.AgentState
+   */
+  public String getAgentState();
+
+  //TODO: indicate the format of the string (is it an item defined in AgentState?  Maybe this whole interface needs an introduction as to why strings are being passed?)
+  /**
+   * Gets the state of all recordings in the system
+   * @return A map of recording-state pairs
+   */
+  public Map<String,Recording> getKnownRecordings();
+
+  /**
+   * Sets the recording's current state
+   * 
+   * @param recordingID The ID of the recording.
+   * @param state The state for the recording.  Defined in RecordingState.
+   * @see org.opencastproject.capture.api.RecordingState
+   */
+  public void setRecordingState(String recordingID, String state);
+  
+  /**
+   * Gets the state of a recording
+   * 
+   * @param recordingID The ID of the recording in question
+   * @return A state (should be defined in RecordingState)
+   * @see org.opencastproject.capture.api.RecordingState
+   */
+  public Recording getRecordingState(String recordingID);
+}
+
