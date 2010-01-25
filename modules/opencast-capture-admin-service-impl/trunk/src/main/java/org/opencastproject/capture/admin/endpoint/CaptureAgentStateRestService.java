770ee49e810db84475c885123cf118099194fc42
msutton <msutton@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-25 20:13:04 +0000

    MH-2279, changed capture-admin rest endpoints verbs form PUT to POST
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5170 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-admin-service-impl/trunk/src/main/java/org/opencastproject/capture/admin/endpoint/CaptureAgentStateRestService.java
+++ modules/opencast-capture-admin-service-impl/trunk/src/main/java/org/opencastproject/capture/admin/endpoint/CaptureAgentStateRestService.java
@@ -25,7 +25,7 @@ import java.util.Map.Entry;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -63,7 +63,7 @@ public class CaptureAgentStateRestService {
     return new AgentStateUpdate(service.getAgentState(agentName));
   }
 
-  @PUT
+  @POST
   @Produces(MediaType.TEXT_XML)
   @Path("agents/{name}")
   public AgentStateUpdate setAgentState(@PathParam("name") String agentName, @FormParam("state") String state) {
@@ -101,7 +101,7 @@ public class CaptureAgentStateRestService {
     return new RecordingStateUpdate(service.getRecordingState(id));
   }
 
-  @PUT
+  @POST
   @Path("recordings/{id}")
   // TODO: setRecordingState should return a boolean indicating if this recording existed or not
   // This way we could return a 404 if the recording doesn't exist, or OK otherwise
