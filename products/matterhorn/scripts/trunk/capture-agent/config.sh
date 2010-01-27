163f18eef85c15bffdc766b0ae15c73e1b64d63e
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-27 22:49:50 +0000

    More work on MH-1199
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5242 e6edf6fb-f266-4316-afb4-e53d95876a76

--- products/matterhorn/scripts/trunk/capture-agent/config.sh
+++ products/matterhorn/scripts/trunk/capture-agent/config.sh
@@ -4,9 +4,12 @@ supportedDevices[0]="Hauppauge WinTV PVR-350"
 supportedDevices[1]="BT878 video (ProVideo PV143)"
 supportedDevices[2]="Epiphan VGA2USB"
 
-ls /dev/video* | while read line
+#ls the dev directory, then grep for video devices with *only* one number and dump the result to a file
+ls /dev/video* | grep '/dev/video[0-9]$' > /tmp/devlist.txt
+
+#Read each line in the file.  Note that if we move the call from above to this line the devices array gets all kinds of scoping problems.
+while read line
  do
-	supported=0
 	let aryLen=${#supportedDevices[@]}-1
 	for item in $(seq 0 1 $aryLen)
 	 do
@@ -15,7 +18,30 @@ ls /dev/video* | while read line
 	
 		if [ "$test" == "$device" ];
 		 then
-			echo $device $line
+			devices[$item]="$device|$line"
 		fi
-	 done
-done
+	done
+done < /tmp/devlist.txt
+
+
+let devAryLen=${#devices[@]}-1
+for dev in $(seq 0 1 $devAryLen)
+ do
+	name="$(echo ${devices[$dev]} | grep -o '.*|' | sed s/\|//g)"
+	cleanName="$(echo $name | sed s/\ //g)"
+	device="$(echo ${devices[$dev]} | grep -o '|.*' | sed s/\|//g)"
+	echo -n "Device $name has been found, should it be called $cleanName? (Y/n) "
+	read response
+	if [ "$response" == "n" ];
+	 then
+		echo -n "Please enter the new name: "
+		read cleanName
+	fi
+	echo "capture.device.$cleanName.src=$device" >> capture.properties
+	echo "capture.device.$cleanName.outputfile=$cleanName" >> capture.properties
+ done
+
+audioDevice=hw:$(sudo arecord -l | grep Analog | cut -c 6)
+cleanAudioDevice=`echo $audioDevice | sed s/\://g`
+echo "capture.device.$cleanAudioDevice.src=$audioDevice" >> capture.properties
+echo "capture.device.$cleanAudioDevice.outputfile=$cleanAudioDevice" >> capture.properties
