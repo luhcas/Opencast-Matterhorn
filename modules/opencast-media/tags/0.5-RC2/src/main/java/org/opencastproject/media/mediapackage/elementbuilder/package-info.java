bcc1559a3e1ce3f6c8b2d7679a0ae612db3a46b0
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 01:51:37 +0000

    MH-2359 Deleted 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5315 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-media/tags/0.5-RC2/src/main/java/org/opencastproject/media/mediapackage/elementbuilder/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlSchema(elementFormDefault = XmlNsForm.UNQUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED)
-package org.opencastproject.media.mediapackage.elementbuilder;
-
-import javax.xml.bind.annotation.XmlSchema;
-import javax.xml.bind.annotation.XmlNsForm;
