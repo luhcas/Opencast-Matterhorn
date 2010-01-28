bebb559a894aaacc3f78542b30bb82c7e2be641a
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 13:11:44 +0000

    MH-2359 Creating 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5365 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-conductor/tags/0.5-RC2/src/test/java/org/opencastproject/conductor/impl/PublishWorkflowOperationHandlerTest.java
@@ -0,0 +1,71 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.conductor.impl;
+
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageBuilderFactory;
+import org.opencastproject.media.mediapackage.MediaPackageElementBuilder;
+import org.opencastproject.media.mediapackage.MediaPackageElementBuilderFactory;
+import org.opencastproject.media.mediapackage.MediaPackageElements;
+import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.search.api.SearchService;
+import org.opencastproject.workflow.api.WorkflowInstanceImpl;
+import org.opencastproject.workflow.api.WorkflowOperationInstanceImpl;
+import org.opencastproject.workflow.api.WorkflowOperationInstanceListImpl;
+import org.opencastproject.workflow.api.WorkflowOperationResult;
+import org.opencastproject.workflow.api.WorkflowInstance.State;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URI;
+
+public class PublishWorkflowOperationHandlerTest {
+  @Test
+  public void testPublishOperation() throws Exception {
+    // Set up a mediapackage to publish
+    MediaPackage mp = MediaPackageBuilderFactory.newInstance().newMediaPackageBuilder().createNew();
+    MediaPackageElementBuilder elementBuilder = MediaPackageElementBuilderFactory.newInstance().newElementBuilder();
+    Track presentationTrack = (Track)elementBuilder.elementFromURI(new URI("http://foo.bar"), Track.TYPE, MediaPackageElements.PRESENTATION_TRACK);
+    Track presenterTrack = (Track)elementBuilder.elementFromURI(new URI("http://foo.bar"), Track.TYPE, MediaPackageElements.PRESENTER_TRACK);
+    mp.add(presentationTrack);
+    mp.add(presenterTrack);
+    
+    // Add the mediapackage to a workflow instance
+    WorkflowInstanceImpl workflowInstance = new WorkflowInstanceImpl();
+    workflowInstance.setId("workflow-1");
+    workflowInstance.setState(State.RUNNING.toString());
+    workflowInstance.setSourceMediaPackage(mp);
+    WorkflowOperationInstanceImpl operationInstance = new WorkflowOperationInstanceImpl();
+    operationInstance.setState(State.RUNNING.toString());
+    WorkflowOperationInstanceListImpl operationsList = new WorkflowOperationInstanceListImpl();
+    operationsList.add(operationInstance);
+    workflowInstance.setWorkflowOperationInstanceList(operationsList);
+    
+    // Set up the operation handler using mock collaborators
+    PublishWorkflowOperationHandler operationHandler = new PublishWorkflowOperationHandler();
+    SearchService searchService = EasyMock.createNiceMock(SearchService.class);
+    EasyMock.replay(searchService);
+    operationHandler.setSearchService(searchService);
+    
+    // Run the media package through the operation handler, ensuring that the flavors are retained
+    WorkflowOperationResult result = operationHandler.run(workflowInstance);
+    for(Track t : result.getResultingMediaPackage().getTracks()) {
+      Assert.assertNotNull(t.getFlavor());
+    }
+  }
+}
