0a68be080c52debb4d5e9fd2d653b452e8fd8d2b
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 18:19:28 +0000

    MH-2359 Deleted 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5406 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-service-impl/tags/0.5-RC2/src/main/java/org/opencastproject/capture/endpoint/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * State and capture agent REST and WSDL Service endpoints.
- */
-@XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.QUALIFIED)
-package org.opencastproject.capture.endpoint;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
-
