f6a70ebf5e9ebdeb03bbe7c44c9a62094456397d
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 01:18:29 +0000

    MH-2359 Creating 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5276 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-working-file-repository-service-impl/tags/0.5-RC2/src/test/java/org/opencastproject/workingfilerepository/impl/WorkingFileRepositoryRestEndpointTest.java
@@ -0,0 +1,15 @@
+package org.opencastproject.workingfilerepository.impl;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class WorkingFileRepositoryRestEndpointTest {
+
+  @Test
+  public void testDocumentation() {
+    WorkingFileRepositoryRestEndpoint endpoint = new WorkingFileRepositoryRestEndpoint();
+    String docs = endpoint.getDocumentation();
+    Assert.assertTrue(docs.indexOf("<html") > -1);
+  }
+}
