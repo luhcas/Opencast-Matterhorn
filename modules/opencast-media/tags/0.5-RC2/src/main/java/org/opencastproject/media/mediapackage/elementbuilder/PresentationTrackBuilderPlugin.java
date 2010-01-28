71dd26dcc5a7bc2202a07ec386000dbc777f6793
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 01:16:22 +0000

    MH-2359 Creating 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5272 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-media/tags/0.5-RC2/src/main/java/org/opencastproject/media/mediapackage/elementbuilder/PresentationTrackBuilderPlugin.java
@@ -0,0 +1,104 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+package org.opencastproject.media.mediapackage.elementbuilder;
+
+import org.opencastproject.media.mediapackage.MediaPackageElement;
+import org.opencastproject.media.mediapackage.MediaPackageElementFlavor;
+import org.opencastproject.media.mediapackage.MediaPackageElements;
+import org.opencastproject.media.mediapackage.UnsupportedElementException;
+import org.opencastproject.media.mediapackage.track.TrackImpl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Node;
+
+import java.net.URI;
+
+import javax.xml.xpath.XPathExpressionException;
+
+/**
+ * This implementation of the {@link MediaPackageElementBuilderPlugin} recognises presentation tracks and provides the
+ * functionality of reading it on behalf of the media package.
+ */
+public class PresentationTrackBuilderPlugin extends AbstractTrackBuilderPlugin {
+
+  /** the logging facility provided by log4j */
+  private final static Logger log_ = LoggerFactory.getLogger(PresentationTrackBuilderPlugin.class.getName());
+
+  public PresentationTrackBuilderPlugin() throws IllegalStateException {
+    setPriority(1);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.elementbuilder.MediaPackageElementBuilderPlugin#accept(org.opencastproject.media.mediapackage.MediaPackageElement.Type
+   *      , org.opencastproject.media.mediapackage.MediaPackageElementFlavor)
+   */
+  public boolean accept(MediaPackageElement.Type type, MediaPackageElementFlavor flavor) {
+    return type.equals(MediaPackageElement.Type.Track) && flavor.equals(MediaPackageElements.PRESENTATION_TRACK);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.elementbuilder.MediaPackageElementBuilderPlugin#accept(org.w3c.dom.Node)
+   */
+  public boolean accept(Node elementNode) {
+    try {
+      String name = elementNode.getNodeName();
+      String flavor = xpath.evaluate("@type", elementNode);
+      return name.equalsIgnoreCase(MediaPackageElement.Type.Track.toString())
+              && MediaPackageElements.PRESENTATION_TRACK.eq(flavor);
+    } catch (XPathExpressionException e) {
+      return false;
+    }
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.elementbuilder.MediaPackageElementBuilderPlugin#accept(URI,
+   *      org.opencastproject.media.mediapackage.MediaPackageElement.Type,
+   *      org.opencastproject.media.mediapackage.MediaPackageElementFlavor)
+   */
+  public boolean accept(URI uri, MediaPackageElement.Type type, MediaPackageElementFlavor flavor) {
+    return type != null && flavor != null && MediaPackageElement.Type.Track.equals(type)
+            && MediaPackageElements.PRESENTATION_TRACK.equals(flavor);
+  }
+
+  /**
+   * @see org.opencastproject.media.mediapackage.elementbuilder.MediaPackageElementBuilderPlugin#elementFromURI(URI)
+   */
+  public MediaPackageElement elementFromURI(URI uri) throws UnsupportedElementException {
+    log_.trace("Creating presentation track from " + uri);
+    TrackImpl track = TrackImpl.fromURI(uri);
+    track.setFlavor(MediaPackageElements.PRESENTATION_TRACK);
+    return track;
+  }
+
+  /**
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "Presentation Track Builder Plugin";
+  }
+
+  @Override
+  protected TrackImpl trackFromManifest(String id, URI uri) {
+    TrackImpl track = TrackImpl.fromURI(uri);
+    track.setIdentifier(id);
+    track.setFlavor(MediaPackageElements.PRESENTATION_TRACK);
+    return track;
+  }
+
+}
---
+++ modules/opencast-media/tags/0.5-RC2/src/main/java/org/opencastproject/media/mediapackage/selector/AudioVisualElementSelector.java
@@ -0,0 +1,216 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.media.mediapackage.selector;
+
+import org.opencastproject.media.mediapackage.AudioStream;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageElementFlavor;
+import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.media.mediapackage.TrackSupport;
+import org.opencastproject.media.mediapackage.VideoStream;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This <code>MediaPackageElementSelector</code> selects a combination of tracks
+ * from a <code>MediaPackage</code> that contain audio and video stream.
+ */
+public class AudioVisualElementSelector extends
+        SimpleMediaPackageElementSelector<Track> {
+
+  /** Explicit video flavor */
+  protected MediaPackageElementFlavor videoFlavor = null;
+
+  /** Explicit audio flavor */
+  protected MediaPackageElementFlavor audioFlavor = null;
+
+  /** Make this selector require an audio track */
+  protected boolean requireAudio = true;
+
+  /** Make this selector require a video track */
+  protected boolean requireVideo = true;
+
+  /**
+   * Creates a new selector.
+   */
+  public AudioVisualElementSelector() {
+  }
+
+  /**
+   * Creates a new selector that will restrict the result of
+   * <code>select()</code> to the given flavor.
+   * 
+   * @param flavor
+   *          the flavor
+   */
+  public AudioVisualElementSelector(String flavor) {
+    this(MediaPackageElementFlavor.parseFlavor(flavor));
+  }
+
+  /**
+   * Creates a new selector that will restrict the result of
+   * <code>select()</code> to the given flavor.
+   * 
+   * @param flavor
+   *          the flavor
+   */
+  public AudioVisualElementSelector(MediaPackageElementFlavor flavor) {
+    addFlavor(flavor);
+  }
+
+  /**
+   * Specifies an explicit audio flavor.
+   * 
+   * @param flavor
+   *          the flavor
+   */
+  public void setAudioFlavor(String flavor) {
+    if (flavor == null) {
+      audioFlavor = null;
+      return;
+    }
+    setAudioFlavor(MediaPackageElementFlavor.parseFlavor(flavor));
+  }
+
+  /**
+   * Specifies an explicit audio flavor.
+   * 
+   * @param flavor
+   *          the flavor
+   */
+  public void setAudioFlavor(MediaPackageElementFlavor flavor) {
+    if (flavor != null)
+      addFlavor(flavor);
+    audioFlavor = flavor;
+  }
+
+  /**
+   * If set to <code>true</code>, this selector requires an audio track to be
+   * part of the result set, or a video track containing at least one audio
+   * stream.
+   * 
+   * @param require
+   *          <code>true</code> to require an audio track
+   */
+  public void setRequireAudioTrack(boolean require) {
+    requireAudio = require;
+  }
+
+  /**
+   * If set to <code>true</code>, this selector requires a video track to be
+   * part of the result set.
+   * 
+   * @param require
+   *          <code>true</code> to require a video track
+   */
+  public void setRequireVideoTrack(boolean require) {
+    requireVideo = require;
+  }
+
+  /**
+   * Returns the explicit audio flavor or <code>null</code> if none was
+   * specified.
+   * 
+   * @return the audio flavor
+   */
+  public MediaPackageElementFlavor getAudioFlavor() {
+    return audioFlavor;
+  }
+
+  /**
+   * Specifies an explicit video flavor.
+   * 
+   * @param flavor
+   *          the flavor
+   */
+  public void setVideoFlavor(String flavor) {
+    if (flavor == null) {
+      videoFlavor = null;
+    } else {
+      setVideoFlavor(MediaPackageElementFlavor.parseFlavor(flavor));
+    }
+  }
+
+  /**
+   * Specifies an explicit video flavor.
+   * 
+   * @param flavor
+   *          the flavor
+   */
+  public void setVideoFlavor(MediaPackageElementFlavor flavor) {
+    if (flavor != null)
+      addFlavor(flavor);
+    videoFlavor = flavor;
+  }
+
+  /**
+   * Returns the explicit video flavor or <code>null</code> if none was
+   * specified.
+   * 
+   * @return the video flavor
+   */
+  public MediaPackageElementFlavor getVideoFlavor() {
+    return videoFlavor;
+  }
+
+  /**
+   * Returns a track or a number of tracks from the media package that together
+   * contain audio and video. If no such combination can be found, e. g. there
+   * is no audio or video at all, an empty array is returned.
+   * 
+   * @see org.opencastproject.media.mediapackage.selector.SimpleMediaPackageElementSelector#select(org.opencastproject.media.mediapackage.MediaPackage)
+   */
+  @Override
+  public Collection<Track> select(MediaPackage mediaPackage) {
+    // instead of relying on the broken superclass, we'll inspect every track
+    // Collection<Track> candidates = super.select(mediaPackage);
+    Collection<Track> candidates = Arrays.asList(mediaPackage.getTracks());
+    Set<Track> result = new HashSet<Track>();
+
+    boolean foundAudio = false;
+    boolean foundVideo = false;
+
+    // Try to look for the perfect match: a track containing audio and video
+    for (Track t : candidates) {
+      if (TrackSupport.byType(t.getStreams(), AudioStream.class).length > 0) {
+        if (!foundAudio
+                && (audioFlavor == null || audioFlavor.equals(t.getFlavor()))) {
+          result.add(t);
+          foundAudio = true;
+        }
+      }
+      // TODO: Fix! byType is not working, let's audio streams pass as video
+      // streams
+      if (TrackSupport.byType(t.getStreams(), VideoStream.class).length > 0) {
+        if (!foundVideo
+                && (videoFlavor == null || videoFlavor.equals(t.getFlavor()))) {
+          result.add(t);
+          foundVideo = true;
+        }
+      }
+    }
+
+    if ((!foundAudio && requireAudio) || (!foundVideo && requireVideo))
+      result.clear();
+
+    // We were lucky, a combination was found!
+    return result;
+  }
+
+}
