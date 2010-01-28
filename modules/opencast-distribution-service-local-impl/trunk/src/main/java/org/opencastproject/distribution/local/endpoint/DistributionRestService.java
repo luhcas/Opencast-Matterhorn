9854a31799e02042b7de6bf1295346337c3cdcc4
jamiehodge <jamiehodge@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 09:54:44 +0000

    MH-2188 Local Distribution Service REST Documentation
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5349 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-distribution-service-local-impl/trunk/src/main/java/org/opencastproject/distribution/local/endpoint/DistributionRestService.java
+++ modules/opencast-distribution-service-local-impl/trunk/src/main/java/org/opencastproject/distribution/local/endpoint/DistributionRestService.java
@@ -19,14 +19,17 @@ import org.opencastproject.distribution.api.DistributionService;
 import org.opencastproject.media.mediapackage.MediaPackage;
 import org.opencastproject.media.mediapackage.MediaPackageImpl;
 import org.opencastproject.util.UrlSupport;
+import org.opencastproject.util.DocUtil;
+import org.opencastproject.util.doc.DocRestData;
+import org.opencastproject.util.doc.Format;
+import org.opencastproject.util.doc.Param;
+import org.opencastproject.util.doc.RestEndpoint;
+import org.opencastproject.util.doc.RestTestForm;
 
-import org.apache.commons.io.IOUtils;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import javax.ws.rs.FormParam;
@@ -44,7 +47,6 @@ import javax.ws.rs.core.Response.Status;
 @Path("/")
 public class DistributionRestService {
   private static final Logger logger = LoggerFactory.getLogger(DistributionRestService.class);
-  protected String docs = null;
   protected String serverUrl = UrlSupport.DEFAULT_BASE_URL;
   private DistributionService service;
   public void setService(DistributionService service) {
@@ -74,7 +76,66 @@ public class DistributionRestService {
   @Produces(MediaType.TEXT_HTML)
   @Path("docs")
   public String getDocumentation() {
+    if (docs == null) { docs = generateDocs(); }
     return docs;
+  }
+
+  protected String docs;
+  private String[] notes = {
+    "All paths above are relative to the REST endpoint base (something like http://your.server/files)",
+    "If the service is down or not working it will return a status 503, this means the the underlying service is not working and is either restarting or has failed",
+    "A status code 500 means a general failure has occurred which is not recoverable and was not anticipated. In other words, there is a bug! You should file an error report with your server logs from the time when the error occurred: <a href=\"https://issues.opencastproject.org\">Opencast Issue Tracker</a>", };
+  
+  private String generateMediaPackage() {
+    return  "<ns2:mediapackage xmlns:ns2=\"http://mediapackage.opencastproject.org\" id=\"10.0000/1\" start=\"2007-12-05T13:40:00\" duration=\"1004400000\">\n"+
+            "  <media>\n"+
+            "    <track id=\"track-1\" type=\"presentation/source\">\n"+
+            "      <mimetype>video/quicktime</mimetype>\n"+
+            "      <url>http://localhost:8080/workflow/samples/camera.mpg</url>\n"+
+            "      <checksum type=\"md5\">43b7d843b02c4a429b2f547a4f230d31</checksum>\n"+
+            "      <duration>1004400000</duration>\n"+
+            "      <video>\n"+
+            "        <device type=\"UFG03\" version=\"30112007\" vendor=\"Unigraf\" />\n"+
+            "        <encoder type=\"H.264\" version=\"7.4\" vendor=\"Apple Inc\" />\n"+
+            "        <resolution>640x480</resolution>\n"+
+            "        <scanType type=\"progressive\" />\n"+
+            "        <bitrate>540520</bitrate>\n"+
+            "        <frameRate>2</frameRate>\n"+
+            "      </video>\n"+
+            "    </track>\n"+
+            "  </media>\n"+
+            "  <metadata>\n"+
+            "    <catalog id=\"catalog-1\" type=\"metadata/dublincore\">\n"+
+            "      <mimetype>text/xml</mimetype>\n"+
+            "      <url>http://localhost:8080/workflow/samples/dc-1.xml</url>\n"+
+            "      <checksum type=\"md5\">20e466615251074e127a1627fd0dae3e</checksum>\n"+
+            "    </catalog>\n"+
+            "  </metadata>\n"+
+            "</ns2:mediapackage>";
+  }
+
+  private String generateDocs() {
+    DocRestData data = new DocRestData("localdistributionservice", "Local Distribution Service", "/distribution/local", notes);
+
+    // abstract
+    data.setAbstract("This service distributes media packages to the Matterhorn feed and engage services.");
+    
+    // distribute
+    RestEndpoint endpoint = new RestEndpoint("distribute", RestEndpoint.Method.POST,
+        "/",
+        "Distribute a media package");
+    endpoint.addFormat(new Format("XML", null, null));
+    endpoint.addRequiredParam(new Param("mediaPackage", Param.Type.TEXT, generateMediaPackage(),
+        "The media package as XML"));
+    endpoint.addRequiredParam(new Param("elementId", Param.Type.STRING, "videoTrack1",
+        "A media package element ID"));
+    endpoint.addNote("Accepts additional elementId fields");
+    endpoint.addStatus(org.opencastproject.util.doc.Status.OK(null));
+    endpoint.addStatus(org.opencastproject.util.doc.Status.ERROR(null));
+    endpoint.setTestForm(RestTestForm.auto());
+    data.addEndpoint(RestEndpoint.Type.WRITE, endpoint);
+
+    return DocUtil.generate(data);
   }
 
   public void activate(ComponentContext cc) {
@@ -90,21 +151,5 @@ public class DistributionRestService {
         serverUrl = ccServerUrl;
       }
     }
-
-    // Pre-load the documentation
-    String docsFromClassloader = null;
-    InputStream in = null;
-    try {
-      in = getClass().getResourceAsStream("/html/index.html");
-      docsFromClassloader = IOUtils.toString(in);
-      docsFromClassloader = docsFromClassloader.replaceAll("@SERVER_URL@", serverUrl + "/workflow/rest");
-      docsFromClassloader = docsFromClassloader.replaceAll("@SAMPLES_URL@", serverUrl + "/workflow/samples");
-    } catch (IOException e) {
-      logger.error("failed to read documentation", e);
-      docsFromClassloader = "unable to load documentation for " + DistributionRestService.class.getName();
-    } finally {
-      IOUtils.closeQuietly(in);
-    }
-    docs = docsFromClassloader;
   }
 }
