8b56e1b4ddcc97e0485b1aaa48cfc9c908f9f674
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 18:20:55 +0000

    MH-2359 Deleted 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5411 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-media/tags/0.5-RC2/src/main/java/org/opencastproject/media/mediapackage/track/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlSchema(elementFormDefault = XmlNsForm.UNQUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED)
-package org.opencastproject.media.mediapackage.track;
-
-import javax.xml.bind.annotation.XmlSchema;
-import javax.xml.bind.annotation.XmlNsForm;
