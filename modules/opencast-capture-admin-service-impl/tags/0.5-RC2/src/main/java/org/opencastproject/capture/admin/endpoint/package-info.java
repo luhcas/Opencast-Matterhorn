de033201562715211812bd9c78b8bfc01cf7f592
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 18:32:56 +0000

    MH-2359 Creating 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5449 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-capture-admin-service-impl/tags/0.5-RC2/src/main/java/org/opencastproject/capture/admin/endpoint/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Capture-admin REST and WSDL Service endpoints
+ */
+@XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED, attributeFormDefault=XmlNsForm.QUALIFIED)
+package org.opencastproject.capture.admin.endpoint;
+
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.bind.annotation.XmlNsForm;
