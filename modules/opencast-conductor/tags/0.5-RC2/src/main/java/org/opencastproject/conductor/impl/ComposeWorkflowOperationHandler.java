efd77d1392437b7c8af2ab71bd2a74f0a920985b
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 01:15:51 +0000

    MH-2359 Creating 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5271 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-conductor/tags/0.5-RC2/src/main/java/org/opencastproject/conductor/impl/ComposeWorkflowOperationHandler.java
@@ -0,0 +1,196 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.conductor.impl;
+
+import org.opencastproject.composer.api.ComposerService;
+import org.opencastproject.composer.api.EncoderException;
+import org.opencastproject.composer.api.EncodingProfile;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageElementFlavor;
+import org.opencastproject.media.mediapackage.MediaPackageException;
+import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.media.mediapackage.UnsupportedElementException;
+import org.opencastproject.media.mediapackage.selector.AudioVisualElementSelector;
+import org.opencastproject.util.MimeTypes;
+import org.opencastproject.workflow.api.WorkflowBuilder;
+import org.opencastproject.workflow.api.WorkflowInstance;
+import org.opencastproject.workflow.api.WorkflowOperationException;
+import org.opencastproject.workflow.api.WorkflowOperationHandler;
+import org.opencastproject.workflow.api.WorkflowOperationInstance;
+import org.opencastproject.workflow.api.WorkflowOperationResult;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * The workflow definition for handling "compose" operations
+ */
+public class ComposeWorkflowOperationHandler implements WorkflowOperationHandler {
+  
+  /** The logging facility */
+  private static final Logger logger = LoggerFactory.getLogger(ComposeWorkflowOperationHandler.class);
+
+  /** The composer service */
+  private ComposerService composerService = null;
+
+  /**
+   * Callback for the OSGi declarative services configuration.
+   * 
+   * @param composerService
+   *          the composer service
+   */
+  protected void setComposerService(ComposerService composerService) {
+    this.composerService = composerService;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.opencastproject.workflow.api.WorkflowOperationHandler#run(org.opencastproject.workflow.api.WorkflowInstance)
+   */
+  public WorkflowOperationResult run(final WorkflowInstance workflowInstance) throws WorkflowOperationException {
+    logger.debug("Running compose workflow operation on workflow {}", workflowInstance.getId());
+
+    // Encode the media package
+    MediaPackage resultingMediaPackage = null;
+    try {
+      resultingMediaPackage = encode(workflowInstance.getCurrentMediaPackage(), workflowInstance.getCurrentOperation());
+    } catch (Exception e) {
+      throw new WorkflowOperationException(e);
+    }
+
+    logger.debug("Compose operation completed");
+
+    return WorkflowBuilder.getInstance().buildWorkflowOperationResult(resultingMediaPackage, null, false);
+  }
+
+  /**
+   * Encode tracks from MediaPackage using profiles stored in properties and updates current MediaPackage.
+   * 
+   * @param src The source media package
+   * @param properties
+   * @return
+   * @throws EncoderException
+   * @throws MediaPackageException
+   * @throws UnsupportedElementException
+   * @throws ExecutionException
+   * @throws InterruptedException
+   */
+  private MediaPackage encode(MediaPackage src, WorkflowOperationInstance operation) throws EncoderException,
+          MediaPackageException, UnsupportedElementException, InterruptedException, ExecutionException {
+    MediaPackage mediaPackage = MediaPackageUtil.clone(src);
+    // Read the configuration properties
+    String sourceVideoFlavor = StringUtils.trimToNull(operation.getConfiguration("source-video-flavor"));
+    String sourceAudioFlavor = StringUtils.trimToNull(operation.getConfiguration("source-audio-flavor"));
+    String targetTrackTags = StringUtils.trimToNull(operation.getConfiguration("target-tags"));
+    String targetTrackFlavor = StringUtils.trimToNull(operation.getConfiguration("target-flavor"));
+    String encodingProfileName = StringUtils.trimToNull(operation.getConfiguration("encoding-profile"));
+
+    if (sourceAudioFlavor == null && sourceVideoFlavor == null)
+      throw new IllegalStateException("either source audio flavor or source video flavor or both must be specified");
+
+    // Find the encoding profile
+    EncodingProfile profile = null;
+    for (EncodingProfile p : composerService.listProfiles()) {
+      if (p.getIdentifier().equals(encodingProfileName)) {
+        profile = p;
+        break;
+      }
+    }
+    if (profile == null) {
+      throw new IllegalStateException("Encoding profile '" + encodingProfileName + "' was not found");
+    }
+    
+    // Depending on the input type of the profile and the configured flavors and tags,
+    // make sure we have the required tracks:
+    AudioVisualElementSelector avSelector = new AudioVisualElementSelector();
+    avSelector.setVideoFlavor(sourceVideoFlavor);
+    avSelector.setAudioFlavor(sourceAudioFlavor);
+    switch (profile.getApplicableMediaType()) {
+      case AudioVisual:
+        avSelector.setRequireVideoTrack(true);
+        avSelector.setRequireAudioTrack(true);
+        break;
+      case Visual:
+        avSelector.setRequireVideoTrack(true);
+        avSelector.setRequireAudioTrack(false);
+        break;
+      case Audio:
+        avSelector.setRequireVideoTrack(false);
+        avSelector.setRequireAudioTrack(true);
+        break;
+      default:
+        logger.warn("Don't know if the current track is applicable to encoding profile '" + profile + "' based on type " + profile.getApplicableMediaType());
+        return mediaPackage;
+    }
+    Collection<Track> tracks = avSelector.select(mediaPackage);
+    
+    String videoSourceTrackId = null;
+    String audioSourceTrackId = null;
+
+    // Did we get the set of tracks that we need?
+    if (tracks.size() == 0) {
+      logger.debug("Skipping encoding of media package to '{}': no suitable input tracks found", profile);
+      return mediaPackage;
+    } else {
+      for (Track t : tracks) {
+        if (t.hasVideo()) {
+          videoSourceTrackId = t.getIdentifier();
+        } else if (t.hasAudio()) {
+          audioSourceTrackId = t.getIdentifier();
+        }
+      }
+    }
+
+    // Start encoding and wait for the result
+    Future<Track> futureTrack = composerService.encode(mediaPackage, videoSourceTrackId, audioSourceTrackId, profile.getIdentifier());
+    Track composedTrack = futureTrack.get();
+    if (composedTrack == null)
+      throw new RuntimeException("unable to retrieve composed track");
+
+    // Add the flavor, either from the operation configuration or from the composer
+    if (targetTrackFlavor != null)
+      composedTrack.setFlavor(MediaPackageElementFlavor.parseFlavor(targetTrackFlavor));
+    logger.debug("Composed track has flavor '{}'", composedTrack.getFlavor());
+
+    // Set the mimetype
+    if (profile.getMimeType() != null)
+      composedTrack.setMimeType(MimeTypes.parseMimeType(profile.getMimeType()));
+    
+    // Add tags
+    if (targetTrackTags != null) {
+      for (String tag : targetTrackTags.split("\\W")) {
+        if(StringUtils.trimToNull(tag) == null) continue;
+        logger.trace("Tagging composed track with '{}'", tag);
+        composedTrack.addTag(tag);
+      }
+    }
+
+    // store new tracks to mediaPackage
+    // FIXME derived media comes from multiple sources, so how do we choose which is the "parent" of the derived
+    // media?
+    String parentId = videoSourceTrackId == null ? audioSourceTrackId : videoSourceTrackId; 
+    mediaPackage.addDerived(composedTrack, mediaPackage.getElementById(parentId));
+
+    return mediaPackage;
+  }
+
+}
---
+++ modules/opencast-conductor/tags/0.5-RC2/src/main/java/org/opencastproject/conductor/impl/DistributeWorkflowOperationHandler.java
@@ -0,0 +1,114 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.conductor.impl;
+
+import org.opencastproject.distribution.api.DistributionService;
+import org.opencastproject.media.mediapackage.Catalog;
+import org.opencastproject.media.mediapackage.MediaPackage;
+import org.opencastproject.media.mediapackage.MediaPackageElement;
+import org.opencastproject.workflow.api.WorkflowBuilder;
+import org.opencastproject.workflow.api.WorkflowInstance;
+import org.opencastproject.workflow.api.WorkflowOperationException;
+import org.opencastproject.workflow.api.WorkflowOperationHandler;
+import org.opencastproject.workflow.api.WorkflowOperationResult;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * The workflow definition for handling "distribute" operations
+ */
+public class DistributeWorkflowOperationHandler implements WorkflowOperationHandler {
+
+  /** The logging facility */
+  private static final Logger logger = LoggerFactory.getLogger(DistributeWorkflowOperationHandler.class);
+
+  /** The distribution service */
+  private DistributionService distributionService = null;
+
+  /**
+   * Callback for the OSGi declarative services configuration.
+   * 
+   * @param distributionService
+   *          the distribution service
+   */
+  protected void setDistributionService(DistributionService distributionService) {
+    this.distributionService = distributionService;
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.opencastproject.workflow.api.WorkflowOperationHandler#run(org.opencastproject.workflow.api.WorkflowInstance)
+   */
+  public WorkflowOperationResult run(final WorkflowInstance workflowInstance) throws WorkflowOperationException {
+    logger.debug("Running distribution workflow operation");
+
+    MediaPackage resultingMediaPackage = null;
+    try {
+
+      // Check which tags have been configured
+      String sourceTags = workflowInstance.getCurrentOperation().getConfiguration("source-tags");
+      String targetTags = workflowInstance.getCurrentOperation().getConfiguration("target-tags");
+      MediaPackage currentMediaPackage = workflowInstance.getCurrentMediaPackage();
+      if (StringUtils.trimToNull(sourceTags) == null) {
+        logger.warn("No tags have been specified");
+        return WorkflowBuilder.getInstance().buildWorkflowOperationResult(currentMediaPackage, null, false);
+      }
+
+      // Send a mediapackage clone to the distribution service so we don't manipulate mediapackages associated with previous workflow operations
+      MediaPackage clone = MediaPackageUtil.clone(currentMediaPackage);
+      
+      // Look for elements matching any tag
+      Set<String> elementIds = new HashSet<String>();
+      for (String tag : sourceTags.split("\\W")) {
+        if(StringUtils.trimToNull(tag) == null) continue;
+        MediaPackageElement[] elts = currentMediaPackage.getElementsByTag(tag);
+        for (MediaPackageElement e : elts) {
+          elementIds.add(e.getIdentifier());
+          logger.info("Distributing '{}' of {} to the local repository", e.getIdentifier(), currentMediaPackage);
+        }
+      }
+
+      // Also distribute all of the metadata catalogs
+      for(Catalog c : clone.getCatalogs()) elementIds.add(c.getIdentifier());
+      
+      resultingMediaPackage = distributionService.distribute(clone, elementIds.toArray(new String[elementIds.size()]));
+      
+      // Tag the distributed elements
+      String[] tags = targetTags.split("\\W");
+      for (MediaPackageElement element : resultingMediaPackage.getElements()) {
+        if (currentMediaPackage.getElementById(element.getIdentifier()) == null) {
+          for (String tag : tags) {
+            if(StringUtils.trimToNull(tag) == null) continue;
+            element.addTag(tag);
+          }
+        }
+      }
+ 
+      logger.debug("Distribute operation completed");
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+      throw new WorkflowOperationException(e);
+    }
+    return WorkflowBuilder.getInstance().buildWorkflowOperationResult(resultingMediaPackage, null, false);
+  }
+
+}
