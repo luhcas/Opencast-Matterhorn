caa75149926132b04e7bb918097109aa7951ce88
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-01-28 01:50:05 +0000

    MH-2359 Deleted 0.5-RC2 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5310 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-service-impl/tags/0.5-RC2/src/main/java/org/opencastproject/capture/impl/jobs/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Job classes used in the impl classes.
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED)
-package org.opencastproject.capture.impl.jobs;
-
-import javax.xml.bind.annotation.XmlNsForm;
