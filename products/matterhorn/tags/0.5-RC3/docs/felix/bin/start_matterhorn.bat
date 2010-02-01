c78c51a36730d8d476a7986c0af6e3908f9fc66b
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-01 14:16:24 +0000

    MH-2427 Creating 0.5-RC3 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5529 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ products/matterhorn/tags/0.5-RC3/docs/felix/bin/start_matterhorn.bat
@@ -0,0 +1,33 @@
+SETLOCAL
+REM ##
+REM # Configure these variables to match your environment
+REM # If you have system-wide variables for FELIX_HOME and M2_REPO then you
+REM # should not have to make any changes to this file.
+REM ##
+
+REM # Make sure the following two path entries do *not* contain spaces
+REM # SET FELIX_HOME=C:\Libraries\felix-framework-2.0.0
+REM # SET M2_REPO=C:\Users\johndoe\.m2\repository
+REM # SET OPENCAST_LOGDIR=%FELIX_HOME%\logs
+SET DEBUG_PORT=8000
+SET DEBUG_SUSPEND=n
+
+REM ##
+REM # Only change the lines below if you know what you are doing
+REM ##
+
+SET FELIX_FILEINSTALL_OPTS=-Dfelix.fileinstall.dir=%FELIX_HOME%\load
+SET PAX_CONFMAN_OPTS=-Dbundles.configuration.location=%FELIX_HOME%\conf
+SET PAX_LOGGING_OPTS=-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dopencast.logdir=%OPENCAST_LOGDIR%
+SET CXF_OPTS=-Djava.util.logging.config.file=%FELIX_HOME%\conf\cxf.properties
+SET FELIX_CACHE=%FELIX_HOME%\felix-cache
+
+REM # Clear felix cache dir
+del /FQ %FELIX_CACHE%
+
+REM # Create the debug config
+SET DEBUG_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=%DEBUG_PORT%,server=y,suspend=%DEBUG_SUSPEND%
+
+REM # Finally start felix
+java %DEBUG_OPTS% -DM2_REPO=%M2_REPO% %FELIX_FILEINSTALL_OPTS% %PAX_CONFMAN_OPTS% %PAX_LOGGING_OPTS% %CXF_OPTS% -jar %FELIX_HOME%\bin\felix.jar %FELIX_CACHE%  
+ENDLOCAL
