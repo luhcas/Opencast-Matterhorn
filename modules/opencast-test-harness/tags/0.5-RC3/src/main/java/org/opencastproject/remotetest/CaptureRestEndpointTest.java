eda07d109217623504ae40227e5479b4dfa78483
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-01 14:45:09 +0000

    MH-2427 Creating 0.5-RC3 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5570 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-test-harness/tags/0.5-RC3/src/main/java/org/opencastproject/remotetest/CaptureRestEndpointTest.java
@@ -0,0 +1,117 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.remotetest;
+
+import static org.junit.Assert.assertEquals;
+
+import static org.opencastproject.remotetest.AllRemoteTests.BASE_URL;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** 
+ * Test the functionality of the capture endpoints. Does not assume capture
+ * devices are connected, so only mock captures are tested
+ */
+public class CaptureRestEndpointTest {
+  
+  private HttpClient httpClient;
+  
+  @Before
+  public void setup() throws Exception {
+    httpClient = new DefaultHttpClient();
+  }
+  
+  @After
+  public void tearDown() throws Exception {
+    httpClient.getConnectionManager().shutdown();
+  }
+  
+  @Test
+  public void testStopCaptureGet() throws Exception {
+
+    HttpGet get = new HttpGet(BASE_URL + "/capture/rest/stopCapture");
+    int getResponse = httpClient.execute(get).getStatusLine().getStatusCode();
+    
+    // when no capture is started, expect internal server error
+    assertEquals(getResponse, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+  }
+  
+  @Test
+  public void testStopCapturePost() throws Exception {
+    
+    HttpPost postStop = new HttpPost(BASE_URL + "/capture/rest/stopCapture");
+    List<NameValuePair> formParams = new ArrayList<NameValuePair>();
+    formParams.add(new BasicNameValuePair("recordingID", "none"));
+    postStop.setEntity(new UrlEncodedFormEntity(formParams));
+    
+    int postResponse = httpClient.execute(postStop).getStatusLine().getStatusCode();
+    
+    assertEquals(postResponse, HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    
+  }
+  
+  @Test
+  public void testStartCapturePost() throws Exception {
+    // Test Properties from resources
+    String props = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("capture.properties"));
+    
+    HttpPost postStart = new HttpPost(BASE_URL + "/capture/rest/startCapture");
+    List<NameValuePair> formParams = new ArrayList<NameValuePair>();
+    formParams.add(new BasicNameValuePair("config", props));
+    postStart.setEntity(new UrlEncodedFormEntity(formParams));
+    
+    int postResponse = httpClient.execute(postStart).getStatusLine().getStatusCode();
+    
+    // properties are setup to run a mock capture that should return OK
+    assertEquals(postResponse, HttpStatus.SC_OK);
+    
+  }
+  
+  @Test
+  public void testStartCaptureGet() throws Exception {
+    
+    HttpGet getStart = new HttpGet(BASE_URL + "/capture/rest/startCapture");
+    int getResponse = httpClient.execute(getStart).getStatusLine().getStatusCode();
+    
+    // Default capture simply runs a mock capture
+    assertEquals(getResponse, HttpStatus.SC_OK);
+  }
+  
+  @Test
+  public void printTestingInfo() {
+    System.out.println("Instructions to test capture agent:");
+    System.out.println("The capture agents can be tested by setting the appropriate\n" +
+        "properties for the JVM. They properties right now are: testHauppauge, testEpiphan, testBt878, testAlsa.\n" +
+        "Each property should be assigned to its location instead of setting a boolean. An example test could be:" +
+        "mvn test -DargLine=\"-DtestHauppauge=/dev/video0 -DtestAlsa=hw:0\"");
+  }
+  
+
+}
