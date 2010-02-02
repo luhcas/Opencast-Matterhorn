b5f33388bf913c28d8cf49e452240cc9d20a63e7
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-02 05:51:01 +0000

    MH-2427 Creating 0.5-RC3 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5667 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-distribution-service-local-impl/tags/0.5-RC3/src/main/java/org/opencastproject/distribution/local/endpoint/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Distribution REST and WSDL Service endpoints
+ */
+@XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED, attributeFormDefault=XmlNsForm.QUALIFIED)
+package org.opencastproject.distribution.local.endpoint;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
