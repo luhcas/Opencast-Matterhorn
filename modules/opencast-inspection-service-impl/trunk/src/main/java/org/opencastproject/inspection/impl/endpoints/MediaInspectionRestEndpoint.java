242f1073a772026dc1ef5a067ba4e066828e5e24
azeckoski <azeckoski@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-04 13:44:41 +0000

    http://issues.opencastproject.org/jira/browse/MH-2236 Minor cleanup of the REST endpoint to correctly handle the case where the service goes bye bye (osgi dynamic load fix)
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5735 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-inspection-service-impl/trunk/src/main/java/org/opencastproject/inspection/impl/endpoints/MediaInspectionRestEndpoint.java
+++ modules/opencast-inspection-service-impl/trunk/src/main/java/org/opencastproject/inspection/impl/endpoints/MediaInspectionRestEndpoint.java
@@ -45,9 +45,11 @@ public class MediaInspectionRestEndpoint {
   private static final Logger logger = LoggerFactory.getLogger(MediaInspectionRestEndpoint.class);
   
   protected MediaInspectionService service;
-
   public void setService(MediaInspectionService service) {
     this.service = service;
+  }
+  public void unsetService(MediaInspectionService service) {
+    this.service = null;
   }
 
   public MediaInspectionRestEndpoint() {
@@ -57,6 +59,7 @@ public class MediaInspectionRestEndpoint {
   @GET
   @Produces(MediaType.TEXT_XML)
   public Response getTrack(@QueryParam("url") URI url) {
+    checkNotNull(service);
     try {
       Track t = service.inspect(url);
       return Response.ok(t).build();
@@ -76,27 +79,39 @@ public class MediaInspectionRestEndpoint {
   protected final String docs;
   private String[] notes = {
           "All paths above are relative to the REST endpoint base (something like http://your.server/inspection/rest)",
-          "If the service is down or not working it will return a status 503, this means the the underlying service is not working and is either restarting or has failed",
+          "If the service is down or not working it will return a status 503, this means the underlying service is not working and is either restarting or has failed",
           "A status code 500 means a general failure has occurred which is not recoverable and was not anticipated. In other words, there is a bug! You should file an error report with your server logs from the time when the error occurred: <a href=\"https://issues.opencastproject.org\">Opencast Issue Tracker</a>",
           "Here is a sample video for testing: <a href=\"./?url=http://source.opencastproject.org/svn/modules/opencast-media/trunk/src/test/resources/aonly.mov\">analyze sample video</a>" };
 
   private String generateDocs() {
-
     DocRestData data = new DocRestData("inspection", "Media inspection", "/inspection/rest", notes);
-
     // abstract
     data.setAbstract("This service extracts technical metadata from media files.");
-
     // getTrack
     RestEndpoint endpoint = new RestEndpoint("getTrack", RestEndpoint.Method.GET, "/",
             "Analyze a given media file");
     endpoint.addOptionalParam(new Param("url", Param.Type.STRING, null, "Location of the media file"));
     endpoint.addFormat(Format.xml());
-    endpoint.addStatus(Status.OK("Result is returned"));
-    endpoint.addStatus(new Status(400, "Problem retrieving media file or invalid media file"));
+    endpoint.addStatus(Status.OK("XML encoded track is returned"));
+    endpoint.addStatus(new Status(400, "Problem retrieving media file or invalid media file or URL"));
     endpoint.setTestForm(RestTestForm.auto());
     data.addEndpoint(RestEndpoint.Type.READ, endpoint);
-
     return DocUtil.generate(data);
   }
+
+  /**
+   * Checks if the service or services are available,
+   * if not it handles it by returning a 503 with a message
+   * @param services an array of services to check
+   */
+  protected void checkNotNull(Object... services) {
+    if (services != null) {
+      for (Object object : services) {
+        if (object == null) {
+          throw new javax.ws.rs.WebApplicationException(javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE);
+        }
+      }
+    }
+  }
+
 }
