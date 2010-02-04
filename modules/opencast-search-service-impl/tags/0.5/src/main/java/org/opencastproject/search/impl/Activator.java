fb029525c2719f59fc74cb9342cb74ac8fb102d4
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-04 19:08:21 +0000

    MH-2514 Creating 0.5 Tag
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5774 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/opencast-search-service-impl/tags/0.5/src/main/java/org/opencastproject/search/impl/Activator.java
@@ -0,0 +1,51 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.search.impl;
+
+import org.opencastproject.search.api.SearchService;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+/**
+ * Activates the search service implementation.
+ */
+public class Activator implements BundleActivator {
+
+  /**
+   * {@inheritDoc}
+   * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+   */
+  @SuppressWarnings("unchecked")
+  public void start(BundleContext context) throws Exception {
+    SearchServiceImpl search = new SearchServiceImpl();
+    search.activate(null); // This is supposed to be a component, so we'd pass a component context here
+    Dictionary props = new Hashtable();
+    props.put("service.description", "Search Service");
+    context.registerService(SearchService.class.getName(), search, props);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception {
+  }
+
+}
