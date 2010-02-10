1cad0a548f1361a79b21cb0c9d1fd66614e7d910
rrolf <rrolf@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-10 18:03:04 +0000

    MH-2457 changed user privileges for update
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5883 e6edf6fb-f266-4316-afb4-e53d95876a76

--- products/matterhorn/scripts/trunk/vmbuilder/update-matterhorn.sh
+++ products/matterhorn/scripts/trunk/vmbuilder/update-matterhorn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #stop felix
-/home/opencast/shutdown.sh
+sudo /home/opencast/shutdown.sh
 cd /home/opencast/opencast
 # update from svn
 svn update
@@ -10,7 +10,7 @@ mvn clean install -DdeployTo=/usr/local/felix-framework-2.0.1/load
 # creating backup of configuration
 tar -czf /home/opencast/felix-config-backup.tar.gz /usr/local/felix-framework-2.0.1/conf/ 
 # update felix configuration
-cp -rf docs/felix/config/ /usr/local/felix-framework-2.0.1/conf/
+sudo p -rf docs/felix/config/ /usr/local/felix-framework-2.0.1/conf/
 cd /home/opnecast
 # restart felix
 /home/opencast/startup.sh
