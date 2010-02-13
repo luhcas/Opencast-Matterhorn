99f19abd15405f39398c98e13b232124ad6c8d2d
demo <demo@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-13 05:03:11 +0000

    restructuring svn
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5914 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-captions-service-impl/branches/0.4-RC/src/main/java/org/opencastproject/captions/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Captions service implementation classes
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED)
-package org.opencastproject.captions.impl;
-
-import javax.xml.bind.annotation.XmlNsForm;
