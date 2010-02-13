3f9e630fc5e9ba5ad2fa71a7034deaf4436edef9
demo <demo@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-13 05:05:33 +0000

    restructuring svn
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5991 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-media/tags/0.5-RC2/src/main/java/org/opencastproject/media/mediapackage/mpeg7/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlSchema(elementFormDefault = XmlNsForm.UNQUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED)
-package org.opencastproject.media.mediapackage.mpeg7;
-
-import javax.xml.bind.annotation.XmlSchema;
-import javax.xml.bind.annotation.XmlNsForm;
