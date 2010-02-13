e1c653fa107e8667a8f0790d55e84376b23158c8
demo <demo@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-13 05:04:10 +0000

    restructuring svn
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5946 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-scheduler-impl/branches/0.4-RC/src/main/java/org/opencastproject/scheduler/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Scheduler service implementation classes
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED)
-package org.opencastproject.scheduler.impl;
-
-import javax.xml.bind.annotation.XmlNsForm;
