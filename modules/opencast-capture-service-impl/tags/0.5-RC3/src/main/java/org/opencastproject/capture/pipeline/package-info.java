f138c5ff8896eae2215dab5b3fe216777cf7e121
demo <demo@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-13 05:04:50 +0000

    restructuring svn
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5968 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-service-impl/tags/0.5-RC3/src/main/java/org/opencastproject/capture/pipeline/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * GStreamer pipeline implementation classes.
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED)
-package org.opencastproject.capture.pipeline;
-
-import javax.xml.bind.annotation.XmlNsForm;
