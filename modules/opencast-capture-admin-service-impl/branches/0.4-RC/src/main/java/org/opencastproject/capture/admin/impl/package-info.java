f9eea3c72723fb8b8e4dcabb5a10f5a2d678e647
demo <demo@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-13 05:03:14 +0000

    restructuring svn
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@5916 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/opencast-capture-admin-service-impl/branches/0.4-RC/src/main/java/org/opencastproject/capture/admin/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Capture-admin service implementation classes
- */
-@javax.xml.bind.annotation.XmlSchema(elementFormDefault=XmlNsForm.QUALIFIED)
-package org.opencastproject.capture.admin.impl;
-
-import javax.xml.bind.annotation.XmlNsForm;
