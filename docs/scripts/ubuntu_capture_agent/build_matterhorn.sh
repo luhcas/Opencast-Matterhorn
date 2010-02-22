687bb9ea7f0243a1717e21d1673c324c767147b4
k.amundson <k.amundson@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-22 22:05:43 +0000

    MH-2659: Updated capture agent build script to use new repos
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6186 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
+++ docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
@@ -6,22 +6,25 @@ FELIX_HOME=$1
 export JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.15
 
 # get the necessary matterhorn source code
-svn co http://source.opencastproject.org/svn/products/matterhorn/trunk/ /home/$USERNAME/capture-agent --depth empty
+mkdir -p /home/$USERNAME/capture-agent
 cd /home/$USERNAME/capture-agent
-svn up pom.xml
-svn co http://source.opencastproject.org/svn/products/matterhorn/trunk/docs/ docs
-svn co http://source.opencastproject.org/svn/modules/opencast-runtime-tools/trunk opencast-runtime-tools
-svn co http://source.opencastproject.org/svn/modules/opencast-build-tools/trunk/ opencast-build-tools
-svn co http://source.opencastproject.org/svn/modules/opencast-util/trunk/ opencast-util
-svn co http://source.opencastproject.org/svn/modules/opencast-media/trunk/ opencast-media
-svn co http://source.opencastproject.org/svn/modules/opencast-dublincore/trunk opencast-dublincore
-svn co http://source.opencastproject.org/svn/modules/opencast-metadata-api/trunk opencast-metadata-api
-svn co http://source.opencastproject.org/svn/modules/opencast-capture-admin-service-api/trunk/ opencast-capture-admin-service-api
-svn co http://source.opencastproject.org/svn/modules/opencast-capture-service-api/trunk/ opencast-capture-service-api
-svn co http://source.opencastproject.org/svn/modules/opencast-capture-service-impl/trunk/ opencast-capture-service-impl
+svn cat http://opencast.jira.com/svn/MH/trunk/pom.xml > pom.xml
+svn co http://opencast.jira.com/svn/MH/trunk/docs docs
+mkdir -p modules
+cd modules
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-runtime-tools matterhorn-runtime-tools
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-build-tools matterhorn-build-tools
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-util matterhorn-util
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-media matterhorn-media
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-dublincore matterhorn-dublincore
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-metadata-api matterhorn-metadata-api
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-capture-admin-service-api matterhorn-capture-admin-service-api
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-capture-service-api matterhorn-capture-service-api
+svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-capture-service-impl matterhorn-capture-service-impl
 
 # setup felix configuration
 cp -r /home/$USERNAME/capture-agent/docs/felix/bin/* ${FELIX_HOME}/bin
 cp -r /home/$USERNAME/capture-agent/docs/felix/conf/* ${FELIX_HOME}/conf
 
+cd ..
 mvn clean install -Pcapture -DdeployTo=${FELIX_HOME}/load
