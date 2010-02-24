b431a11e6d1068a212e7e667e9b89fe8009915be
k.amundson <k.amundson@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-24 22:49:55 +0000

    MH-2552: Adding a script that will uninstall the Matterhorn Capture Agent from the system.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6211 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ docs/scripts/ubuntu_capture_agent/cleanup.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+#
+# Cleanup all traces of the capture agent
+
+# get the user associated with the capture agent build
+echo -n "Which user runs the capture agent? (WARNING: This user will be removed) "
+read USER
+
+id $USER &> /dev/null
+if [ "$?" -ne 0 ]; then
+  echo "User $USER does not exist."
+  exit
+fi
+
+# double check to make sure they aren't losing any vital information
+echo -n "Are you sure you want to remove the Matterhorn Capture Agent and user $USER from your system? (y/N) "
+read RESPONSE
+
+if [ "$RESPONSE" != "y" ]; then
+  exit
+fi
+
+# remove vga2usb driver
+lsmod | grep "^vga2usb"
+if [ "$?" -ne 0 ]; then
+  sudo rmmod vga2usb
+fi
+
+# restore appropriate sources.list
+sudo mv /etc/apt/sources.list.backup /etc/apt/sources.list
+
+# remove the configuration that starts matterhorn on boot
+sudo rm -f /etc/init/matterhorn.conf
+
+# remove the udev rules that manage the devices
+sudo rm -f /etc/udev/rules.d/95-perso.rules
+
+# remove the capture storage directory
+sudo rm -rf /opencast
+
+# remove the jv4linfo library
+sudo rm -f /usr/lib/libjv4linfo.so
+
+# remove the user, all with all the
+sudo userdel -rf $USERNAME
+
+echo "Done uninstalling Matterhorn Capture Agent." 
