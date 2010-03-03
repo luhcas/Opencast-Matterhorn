c761ed8be5a324d13f197b047a4e9e9e1bd0b86f
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-03 19:22:03 +0000

    MH-2821 -- added the DistributionContextStrategy
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6246 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/matterhorn-distribution-service-api/src/main/java/org/opencastproject/distribution/api/DistributionContextStragety.java
@@ -0,0 +1,45 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.distribution.api;
+
+import org.opencastproject.media.mediapackage.MediaPackage;
+
+/**
+ * DistributionContextStrategy implementations provide mappings between mediapackages and distribution channel specific
+ * keys (such as keys for obtaining iTunesU tabs or youTube playlists).
+ */
+public interface DistributionContextStragety {
+  /**
+   * Gets an identifier for a mediapackage. Implementations may return a series identifier, a creator name, or any other
+   * kind of metadata obtained from the mediapackage.
+   * 
+   * @param mp
+   *          The mediapackage
+   * @return The context ID
+   */
+  public String getContextId(MediaPackage mp);
+
+  /**
+   * Gets the name for a context within a distribution channel. This method is called once to create a new tab or
+   * playlist.
+   * 
+   * @param mp
+   *          The mediapackage
+   * @return The name of the context (playlist, tab, etc)
+   */
+  public String getContextName(MediaPackage mp);
+
+}
