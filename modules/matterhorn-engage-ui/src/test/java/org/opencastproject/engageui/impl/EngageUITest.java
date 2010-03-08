e54da6d969d8239e5e01a0bcd607b5d4991c0d6b
mketterl <mketterl@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-08 15:40:00 +0000

    MH-2892 adding test folder that will be used for xslt validation
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6275 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/matterhorn-engage-ui/src/test/java/org/opencastproject/engageui/impl/EngageUITest.java
@@ -0,0 +1,33 @@
+package org.opencastproject.engageui.impl;
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class EngageUITest{
+
+ 
+
+  public EngageUITest() {
+    super();
+  }
+
+  @Before
+  public void setUp() throws Exception {
+   
+  }
+  @After
+  public void tearDown() throws Exception{
+  }
+  
+  @Test
+  public void testAmount() throws Exception {
+	  Assert.assertTrue(1 == 1);
+  }
+
+  
+}
+
