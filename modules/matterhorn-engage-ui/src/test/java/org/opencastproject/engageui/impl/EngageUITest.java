6730f77250ec10132771a6a50b89e321995a3bf5
jemden <jemden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-10 23:48:53 +0000

    MH-2945
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6293 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-engage-ui/src/test/java/org/opencastproject/engageui/impl/EngageUITest.java
+++ modules/matterhorn-engage-ui/src/test/java/org/opencastproject/engageui/impl/EngageUITest.java
@@ -1,15 +1,28 @@
 package org.opencastproject.engageui.impl;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class EngageUITest {
 
-public class EngageUITest{
-
- 
+  private static final Logger logger = LoggerFactory.getLogger(EngageUITest.class);
 
   public EngageUITest() {
     super();
@@ -17,17 +30,39 @@ public class EngageUITest{
 
   @Before
   public void setUp() throws Exception {
-   
+
   }
+
   @After
-  public void tearDown() throws Exception{
+  public void tearDown() throws Exception {
   }
-  
+
   @Test
-  public void testAmount() throws Exception {
-	  Assert.assertTrue(1 == 1);
+  public void testEpisodeXSL() throws Exception {
+
+    File xsltFile = new File(this.getClass().getClassLoader().getResource(
+            "ui" + File.separator + "xsl" + File.separator + "player-hybrid-download.xsl").getFile());
+
+    File xmlFile = new File(this.getClass().getClassLoader().getResource("xml" + File.separator + "episode.xml")
+            .getFile());
+
+    InputStream expectedStream = this.getClass().getClassLoader().getResourceAsStream(
+            "xml" + File.separator + "episode-target.xml");
+    BufferedReader expectedReader = new BufferedReader(new InputStreamReader(expectedStream));
+    String expected = expectedReader.readLine();
+
+    Source xmlSource = new StreamSource(xmlFile);
+    Source xsltSource = new StreamSource(xsltFile);
+
+    OutputStream actualStream = new ByteArrayOutputStream();
+    Result result = new StreamResult(actualStream);
+
+    // create an instance of TransformerFactory
+    TransformerFactory transFact = TransformerFactory.newInstance();
+    Transformer trans = transFact.newTransformer(xsltSource);
+    trans.transform(xmlSource, result);
+    String actual = actualStream.toString();
+
+    Assert.assertTrue(expected.equals(actual));
   }
-
-  
 }
-
