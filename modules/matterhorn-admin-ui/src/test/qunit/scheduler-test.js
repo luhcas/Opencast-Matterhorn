bf4514f94caf987414ce0dd98ae5669e8fa2950c
msutton <msutton@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-11 18:51:21 +0000

    MH-1958, unit test updates
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6302 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-admin-ui/src/test/qunit/scheduler-test.js
+++ modules/matterhorn-admin-ui/src/test/qunit/scheduler-test.js
@@ -1,7 +1,7 @@
 module( "FormField", {
         setup: function(){ 
           var doc = $(document.body);
-          doc.append('<input type="text" id="textbox" />');
+          doc.append('<input type="text" id="textbox" value="true" />');
           doc.append('<input type="checkbox" id="checkbox" checked="checked" value="true" />');
         },
         teardown: function(){
@@ -12,6 +12,13 @@ module( "FormField", {
 test("FormField creation", function(){
      var field = new FormField();
      ok((field && typeof field === 'object' && field instanceof FormField), "Instantiate empty FormField");
-     field = new FormField('textbox', true);
-     ok((field && field.fields.textbox && field.fields.textbox[0].type === 'text'), "Add Field");
+     field.setFormFields('textbox');
+     ok((field.fields.textbox && field.fields.textbox[0].type === 'text'), "Add Field");
+     field.setFormFieldOpts({required: true, foo: true});
+     ok((field.required && field.foo), "FormField Opts set");
+});
+
+test("FormField get/set/disp", function(){
+     var field = new FormField('textbox', false, {getValue: function(){ return true; }});
+     ok(field.getValue(), "Run getValue");
 });
