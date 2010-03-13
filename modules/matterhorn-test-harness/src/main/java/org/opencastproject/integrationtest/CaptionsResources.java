49737c9b9fe6be4aa2edbfb04188d06ad6befe03
jamiehodge <jamiehodge@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-13 21:19:57 +0000

    MH-2616 Write Full Suite of Integration Tests
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6326 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/matterhorn-test-harness/src/main/java/org/opencastproject/integrationtest/CaptionsResources.java
@@ -0,0 +1,43 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.integrationtest;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+/**
+ * Captions REST resources
+ * @author jamiehodge
+ *
+ */
+
+public class CaptionsResources {
+	public static Client c = Client.create();
+	public static WebResource r = c.resource(IntegrationTests.BASE_URL + "/captions/rest/");
+	
+	public static ClientResponse search() throws Exception {
+		return r.path("search").get(ClientResponse.class);
+	}
+	
+	public static ClientResponse episode(String id) throws Exception {
+		return r.path(id).get(ClientResponse.class);
+	}
+	
+	public static ClientResponse add(String id, String doc, String format) throws Exception {
+		return r.path(id + '/' + format).post(ClientResponse.class, doc);
+	}
+}
---
+++ modules/matterhorn-test-harness/src/main/java/org/opencastproject/integrationtest/FilesResources.java
@@ -0,0 +1,52 @@
+/**
+ *  Copyright 2009 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+package org.opencastproject.integrationtest;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
+/**
+ * Files REST resources
+ * @author jamiehodge
+ *
+ */
+
+public class FilesResources {
+	public static Client c = Client.create();
+	public static WebResource r = c.resource(IntegrationTests.BASE_URL + "/files/rest/");
+	
+	public static ClientResponse getFile(String mediaPackageID, String mediaPackageElementID) throws Exception {
+		MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+		params.add(mediaPackageID, mediaPackageID);
+		params.add(mediaPackageElementID, mediaPackageElementID);
+		return r.post(ClientResponse.class, params);
+	}
+	
+	public static ClientResponse getFile(String mediaPackageID, 
+			String mediaPackageElementID, String fileName) throws Exception {
+		MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+		params.add("mediaPackageID", mediaPackageID);
+		params.add("mediaPackageElementID", mediaPackageElementID);
+		params.add("fileName", fileName);
+		return r.post(ClientResponse.class, params);
+	}
+	
+	// TODO POST and DELETE
+}
