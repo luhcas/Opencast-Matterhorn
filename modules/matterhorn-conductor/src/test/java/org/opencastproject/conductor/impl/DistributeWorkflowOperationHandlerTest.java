169a9123e99db6b12eceb88b2b23e266b98f7a49
bostjan <bostjan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-15 16:26:28 +0000

    MH-2973 DistributeWorkflowOperationHandlerTest bug: committed all the files, resolved the bug
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6340 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-conductor/src/test/java/org/opencastproject/conductor/impl/DistributeWorkflowOperationHandlerTest.java
+++ modules/matterhorn-conductor/src/test/java/org/opencastproject/conductor/impl/DistributeWorkflowOperationHandlerTest.java
@@ -16,33 +16,23 @@
 package org.opencastproject.conductor.impl;
 
 import org.opencastproject.distribution.api.DistributionService;
-import org.opencastproject.inspection.api.MediaInspectionService;
 import org.opencastproject.media.mediapackage.MediaPackage;
 import org.opencastproject.media.mediapackage.MediaPackageBuilder;
 import org.opencastproject.media.mediapackage.MediaPackageBuilderFactory;
-import org.opencastproject.media.mediapackage.MediaPackageElementBuilder;
-import org.opencastproject.media.mediapackage.MediaPackageElementBuilderFactory;
-import org.opencastproject.media.mediapackage.MediaPackageElements;
-import org.opencastproject.media.mediapackage.Track;
-import org.opencastproject.metadata.dublincore.DublinCoreCatalogService;
 import org.opencastproject.workflow.api.WorkflowInstanceImpl;
 import org.opencastproject.workflow.api.WorkflowOperationInstance;
 import org.opencastproject.workflow.api.WorkflowOperationInstanceImpl;
 import org.opencastproject.workflow.api.WorkflowOperationResult;
 import org.opencastproject.workflow.api.WorkflowInstance.WorkflowState;
-import org.opencastproject.workspace.api.Workspace;
 
 import org.easymock.EasyMock;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-@Ignore
 public class DistributeWorkflowOperationHandlerTest {
   private DistributeWorkflowOperationHandler operationHandler;
   private DistributionService service = null;
@@ -53,22 +43,23 @@ public class DistributeWorkflowOperationHandlerTest {
   @Before
   public void setup() throws Exception {
     MediaPackageBuilder builder = MediaPackageBuilderFactory.newInstance().newMediaPackageBuilder();
-    
+
     uriMP = InspectWorkflowOperationHandler.class.getResource("/distribute_mediapackage.xml").toURI();
     mp = builder.loadFromXml(uriMP.toURL().openStream());
-    
+
     // set up service
     operationHandler = new DistributeWorkflowOperationHandler();
-    
+
     // set up mock inspect
     service = EasyMock.createNiceMock(DistributionService.class);
-    EasyMock.expect(service.distribute((MediaPackage)EasyMock.anyObject(), (String[])EasyMock.anyObject())).andReturn(mp);
+    EasyMock.expect(service.distribute((MediaPackage) EasyMock.anyObject(), (String[]) EasyMock.anyObject()))
+            .andReturn(mp);
     EasyMock.replay(service);
     operationHandler.setDistributionService(service);
   }
 
   @Test
-  public void testPublishOperation() throws Exception {
+  public void testDistributeOperation() throws Exception {
     // Add the mediapackage to a workflow instance
     WorkflowInstanceImpl workflowInstance = new WorkflowInstanceImpl();
     workflowInstance.setId("workflow-distribute-test");
@@ -78,13 +69,14 @@ public class DistributeWorkflowOperationHandlerTest {
     List<WorkflowOperationInstance> operationsList = new ArrayList<WorkflowOperationInstance>();
     operationsList.add(operationInstance);
     workflowInstance.setOperations(operationsList);
+    // workflowInstance.getCurrentOperation().setConfiguration("source-tags", value);
+    // workflowInstance.getCurrentOperation().setConfiguration("target-tags", value);
     workflowInstance.next(); // Simulate starting the workflow
 
     // Run the media package through the operation handler, ensuring that metadata gets added
     WorkflowOperationResult result = operationHandler.start(workflowInstance);
     MediaPackage mpNew = result.getMediaPackage();
     // TODO: some test to perform
-    
-    
+
   }
 }
