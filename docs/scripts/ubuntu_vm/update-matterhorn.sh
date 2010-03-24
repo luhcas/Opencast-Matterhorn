7872ffe734af2e28e8fa33c59d41450998d5f78b
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-24 22:17:25 +0000

    NOJIRA: Fixed update-matterhorn.sh - copy conf
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6419 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/ubuntu_vm/update-matterhorn.sh
+++ docs/scripts/ubuntu_vm/update-matterhorn.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+#stop felix
+sudo /home/opencast/shutdown.sh
+
 cd /opt/matterhorn/matterhorn_trunk
+
 # update from svn
 svn update
+
+# Clean old jars
+rm -rf /opt/matterhorn/felix/load
+
 # build matterhorn
 mvn clean install -DdeployTo=/opt/matterhorn/felix/load
 
-#stop felix
-sudo /home/opencast/shutdown.sh
 # creating backup of configuration
 tar -czf /home/opencast/felix-config-backup.tar.gz /opt/matterhorn/felix/conf/ 
+
 # update felix configuration
-sudo cp -rf docs/felix/conf/ /opt/matterhorn/felix/conf/
+sudo cp -rf docs/felix/conf /opt/matterhorn/felix/.
+
 cd /home/opencast
+
 # update felix config (url)
 MY_IP=`ifconfig | grep "inet addr:" | grep -v 127.0.0.1 | awk '{print $2}' | cut -d':' -f2`
 sed -i "s/http:\/\/localhost:8080/http:\/\/$MY_IP:8080/" /opt/matterhorn/felix/conf/config.properties
