34ef5bafcfd823f2cee68b72901dbb582af9c00d
k.amundson <k.amundson@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-04-06 16:42:30 +0000

    MH-3172: used for nightly testing of a capture box.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6507 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/ubuntu_capture_agent/nightly.sh
+++ docs/scripts/ubuntu_capture_agent/nightly.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 
-# Must be ran as "matterhorn" user
-
-export M2_REPO=/home/$USER/.m2/repository
-export FELIX_HOME=/home/$USER/felix-framework-2.0.4
+USERNAME=$1
+export M2_REPO=/home/$USERNAME/.m2/repository
+export FELIX_HOME=/home/$USERNAME/felix-framework-2.0.4
 export JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.15
 
-su $USER -c "date >> /home/$USER/nightly.log"
-su $USER -c "svn cat http://opencast.jira.com/svn/MH/trunk/pom.xml > pom.xml"
-su $USER -c "svn up /home/$USER/capture-agent/modules/*"
-su $USER -c "svn up /home/$USER/capture-agent/docs"
+su $USERNAME -c "svn cat http://opencast.jira.com/svn/MH/trunk/pom.xml > /home/$USERNAME/capture-agent/pom.xml"
+su $USERNAME -c "svn up /home/$USERNAME/capture-agent/modules/*"
+su $USERNAME -c "svn up /home/$USERNAME/capture-agent/docs"
 
-cd /home/$USER/capture-agent
-su $USER -c "mvn clean install -Pcapture -DdeployTo=${FELIX_HOME}/load"
+REV=`su $USERNAME -c "svn info /home/$USERNAME/capture-agent/modules/matterhorn-capture-agent-impl | grep Revision"`
+
+cd /home/$USERNAME/capture-agent
+su $USERNAME -c "mvn clean install -Pcapture -DdeployTo=${FELIX_HOME}/load | mail -s \"testopencast $REV `date`\" lecture@cs.usask.ca"
 
 /sbin/reboot now
-
