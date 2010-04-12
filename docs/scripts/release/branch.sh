84ebde6eb655664dc2d2e3173847158557bbecc1
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-04-12 16:35:34 +0000

    NOJIRA Added release directory
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6552 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ docs/scripts/release/branch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# Branch a Module
+#
+BRANCH_OLD=0.6-SNAPSHOT
+BRANCH_VER=0.7-SNAPSHOT
+JIRA_TKT=MH-3333
+
+WORK_DIR=/Users/mtrehan/Matterhorn/svn
+
+SVN_DIR=$WORK_DIR/$JIRA_TKT
+SVN_URL=https://opencast.jira.com/svn/MH
+TRUNK_URL=$SVN_URL/trunk
+BRANCH_URL=$SVN_URL/branches/$BRANCH_VER
+
+#svn copy $TRUNK_URL $BRANCH_URL -m "$JIRA_TKT Creating $BRANCH_VER Branch"
+
+cd $WORK_DIR
+
+svn co $BRANCH_URL $JIRA_TKT
+
+cd $SVN_DIR
+
+echo "Main:"
+
+sed "1,\$s/\>$BRANCH_OLD\</\>$BRANCH_VER\</" $SVN_DIR/pom.xml >/tmp/mh-branch-pom.xml
+cp /tmp/mh-branch-pom.xml $SVN_DIR/pom.xml
+
+for i in modules/matterhorn-*
+do
+    echo " Module: $i"
+
+    if [ -f $SVN_DIR/$i/pom.xml ]; then
+        echo " $BRANCH_VER: $i"
+        sed "1,\$s/\>$BRANCH_OLD\</\>$BRANCH_VER\</" $SVN_DIR/$i/pom.xml >/tmp/mh-branch-pom.xml
+        cp /tmp/mh-branch-pom.xml $SVN_DIR/$i/pom.xml
+        sleep 1
+    fi
+done
+
+svn commit -m "$JIRA_TKT Updated pom.xml files to reflect correct version"
