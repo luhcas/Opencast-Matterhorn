5e8133a32993f8ea6a0991d7f2d306cc9981e377
ruben.perez <ruben.perez@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-04-20 08:50:00 +0000

    MH-3211 Modified the script so that it checks out all the matterhorn trunk, instead of just some bundles, to avoid not having all the necessary stuff if the dependencies change
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6611 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
+++ docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
@@ -8,20 +8,7 @@ export JAVA_HOME=$2
 # get the necessary matterhorn source code
 mkdir -p /home/$USERNAME/capture-agent
 cd /home/$USERNAME/capture-agent
-svn cat http://opencast.jira.com/svn/MH/trunk/pom.xml > pom.xml
-svn co http://opencast.jira.com/svn/MH/trunk/docs docs
-mkdir -p modules
-cd modules
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-runtime-tools matterhorn-runtime-tools
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-build-tools matterhorn-build-tools
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-util matterhorn-util
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-media matterhorn-media
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-dublincore matterhorn-dublincore
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-metadata-api matterhorn-metadata-api
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-capture-admin-service-api matterhorn-capture-admin-service-api
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-capture-agent-api matterhorn-capture-agent-api
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-capture-agent-impl matterhorn-capture-agent-impl
-svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-http matterhorn-http
+svn co http://opencast.jira.com/svn/MH/trunk
 svn co http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-rest matterhorn-rest
 
 # setup felix configuration
