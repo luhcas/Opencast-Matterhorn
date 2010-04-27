79a31f614c04279b98c847447ddec589fc5157a9
msutton <msutton@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-04-27 22:28:48 +0000

    MISSING JIRA: Confidence monitoring module.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6705 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/matterhorn-confidence-monitoring-ui/src/main/resources/ui/js/confidence.js
@@ -0,0 +1,31 @@
+//load the devices
+$.get(CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/devices", function(data){
+      //do stuff to make a device array.
+      var devices = $('name', data);
+      for(d in devices){
+        devices[d].text();
+        $('#device_tab').append('<li>' + devices[d] + '</li>');
+      }
+      });
+}
+
+Monitor.selectDevice = function(device){
+  if(Monitor.selectedDevice != device){
+    if(Monitor.intervalId){ clearInterval(Monitor.intervalImgId); clearInterval(Monitor.intervalAudioId); }
+    Monitor.intervalImgId = setInterval(Monitor.updateImg, 5000);
+    Monitor.intervalAudioId = setInterval(Monitor.updateAudio, 1000);
+  }
+}
+
+Monitor.updateImg = function(){
+  var imgGrab = CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/" + Monitor.selectedDevice;
+  $("#image_preview").replaceWith('<img src="' + imgGrab + '"></img>');
+}
+
+Monitor.updateAudio = function(){
+  
+}
+
+AudioBar.setValue = function(){
+  
+}
