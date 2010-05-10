510c3bc8736bb336d5594388fd7ab510f8745d1b
msutton <msutton@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-10 21:31:57 +0000

    MH-3441 - confidence monitoring changes
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6837 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-confidence-monitoring-ui/src/main/resources/ui/js/confidence.js
+++ modules/matterhorn-confidence-monitoring-ui/src/main/resources/ui/js/confidence.js
@@ -1,25 +1,29 @@
-var CAPTURE_AGENT_CONFIDENCE_MONITORING_URL = "/confidence/rest";
-var Monitor = {} || Monitor;
+var CAPTURE_AGENT_CONFIDENCE_MONITORING_URL = "http://orochi.unl.edu:8080/confidence/rest";
 var AudioBar = {} || AudioBar;
-var Monitor.intervalImgId = null;
-var Monitor.intervalAudioId = null;
-var Monitor.selectedDevice = null;
+var Monitor = {} || Monitor;
+Monitor.intervalImgId = null;
+Monitor.intervalAudioId = null;
+Monitor.selectedDevice = null;
 
 Monitor.loadDevices = function(){
   //load the devices
   $.get(CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/devices", function(data){
-      //do stuff to make a device array.
-      var devices = $('name', data);
-      for(d in devices){
-        devices[d].text();
-        $('#device_tab').append('<li>' + devices[d] + '</li>');
-      }
+        //do stuff to make a device array.
+        var devices = $('name', data);
+        log(devices);
+        for(d in devices){
+          devices[d].text();
+          $('#device_tab').append('<li>' + devices[d] + '</li>');
+        }
       });
 }
 
 Monitor.selectDevice = function(device){
   if(Monitor.selectedDevice != device){
-    if(Monitor.intervalId){ clearInterval(Monitor.intervalImgId); clearInterval(Monitor.intervalAudioId); }
+    if(Monitor.intervalId){ 
+      clearInterval(Monitor.intervalImgId); 
+      clearInterval(Monitor.intervalAudioId);
+    }
     Monitor.intervalImgId = setInterval(Monitor.updateImg, 5000);
     Monitor.intervalAudioId = setInterval(Monitor.updateAudio, 1000);
   }
@@ -36,4 +40,10 @@ Monitor.updateAudio = function(){
 
 AudioBar.setValue = function(){
   
+}
+
+log = function(){
+  if(typeof console != 'undefined'){
+    console.log.apply(this, arguments);
+  }
 }
