30172695eb93339b86136f1108d6eca08bf311f5
bostjan <bostjan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-12 15:21:59 +0000

    MH-2363: Added a test for DistributeWorkflowOperationHandler, checking if correct elements as defined with source-tags get distributed
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@6884 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-conductor/src/test/java/org/opencastproject/workflow/handler/DistributeWorkflowOperationHandlerTest.java
+++ modules/matterhorn-conductor/src/test/java/org/opencastproject/workflow/handler/DistributeWorkflowOperationHandlerTest.java
@@ -20,6 +20,7 @@ import org.opencastproject.media.mediapackage.MediaPackage;
 import org.opencastproject.media.mediapackage.MediaPackageBuilder;
 import org.opencastproject.media.mediapackage.MediaPackageBuilderFactory;
 import org.opencastproject.media.mediapackage.Track;
+import org.opencastproject.workflow.api.WorkflowInstance;
 import org.opencastproject.workflow.api.WorkflowInstanceImpl;
 import org.opencastproject.workflow.api.WorkflowOperationInstance;
 import org.opencastproject.workflow.api.WorkflowOperationInstanceImpl;
@@ -60,33 +61,22 @@ public class DistributeWorkflowOperationHandlerTest {
     // set up service
     operationHandler = new DistributeWorkflowOperationHandler();
 
-    // set up mock inspect
-    service = EasyMock.createNiceMock(DistributionService.class);
+  }
+
+  @Test
+  public void testDistributeOperation() throws Exception {
+    // set up mock distribution
+    service = EasyMock.createStrictMock(DistributionService.class);
     EasyMock.expect(
             service.distribute((MediaPackage) EasyMock.anyObject(), (String) EasyMock.anyObject(), (String) EasyMock
                     .anyObject())).andReturn(mpAfter);
     EasyMock.replay(service);
     operationHandler.setDistributionService(service);
-  }
 
-  @Test
-  public void testDistributeOperation() throws Exception {
-    // Add the mediapackage to a workflow instance
-    WorkflowInstanceImpl workflowInstance = new WorkflowInstanceImpl();
-    workflowInstance.setId("workflow-distribute-test");
-    workflowInstance.setState(WorkflowState.RUNNING);
-    workflowInstance.setMediaPackage(mp);
-    WorkflowOperationInstanceImpl operationInstance = new WorkflowOperationInstanceImpl();
+    String sourceTags = "nosuchtag";
+    String targetTags = "engage,publish";
 
-    String sourceTags = "engage,atom,rss";
-    operationInstance.setConfiguration("source-tags", sourceTags);
-    String targetTags = "publish";
-    operationInstance.setConfiguration("target-tags", targetTags);
-
-    List<WorkflowOperationInstance> operationsList = new ArrayList<WorkflowOperationInstance>();
-    operationsList.add(operationInstance);
-    workflowInstance.setOperations(operationsList);
-    workflowInstance.next(); // Simulate starting the workflow
+    WorkflowInstance workflowInstance = getWorkflowInstance(sourceTags, targetTags);
 
     // Run the media package through the operation handler, ensuring that metadata gets added
     WorkflowOperationResult result = operationHandler.start(workflowInstance);
@@ -96,6 +86,57 @@ public class DistributeWorkflowOperationHandlerTest {
     String actualTags = StringUtils.join(mpNew.getTrack(t.getIdentifier()).getTags(), ",");
     Assert.assertEquals(targetTags, actualTags);
 
+  }
+
+  @Test
+  public void testSourceTags() throws Exception {
+    service = EasyMock.createStrictMock(DistributionService.class);
+    EasyMock.expect(
+            service.distribute((MediaPackage) EasyMock.anyObject(), EasyMock.matches("catalog-2"), EasyMock
+                    .matches("notes"), EasyMock.matches("track-2"), EasyMock.matches("catalog-1"))).andReturn(mpAfter);
+    EasyMock.expect(
+            service.distribute((MediaPackage) EasyMock.anyObject(), EasyMock.matches("catalog-2"), EasyMock
+                    .matches("notes"), EasyMock.matches("catalog-1"))).andReturn(mpAfter);
+    EasyMock.expect(
+            service.distribute((MediaPackage) EasyMock.anyObject(), EasyMock.matches("catalog-2"), EasyMock
+                    .matches("catalog-1"))).andReturn(mpAfter);
+    EasyMock.replay(service);
+    operationHandler.setDistributionService(service);
+
+    
+    // Source tags get tested by StrictMock
+    String sourceTags = "engage,atom,rss";
+    String targetTags = "engage,publish";
+    WorkflowInstance workflowInstance = getWorkflowInstance(sourceTags, targetTags);
+    operationHandler.start(workflowInstance);
+
+    sourceTags = "rss";
+    workflowInstance = getWorkflowInstance(sourceTags, targetTags);
+    operationHandler.start(workflowInstance);
+
+    sourceTags = "nosuchtag";
+    workflowInstance = getWorkflowInstance(sourceTags, targetTags);
+    operationHandler.start(workflowInstance);
+
+  }
+
+  private WorkflowInstance getWorkflowInstance(String sourceTags, String targetTags) {
+    // Add the mediapackage to a workflow instance
+    WorkflowInstanceImpl workflowInstance = new WorkflowInstanceImpl();
+    workflowInstance.setId("workflow-distribute-test");
+    workflowInstance.setState(WorkflowState.RUNNING);
+    workflowInstance.setMediaPackage(mp);
+    WorkflowOperationInstanceImpl operationInstance = new WorkflowOperationInstanceImpl();
+
+    operationInstance.setConfiguration("source-tags", sourceTags);
+    operationInstance.setConfiguration("target-tags", targetTags);
+
+    List<WorkflowOperationInstance> operationsList = new ArrayList<WorkflowOperationInstance>();
+    operationsList.add(operationInstance);
+    workflowInstance.setOperations(operationsList);
+    workflowInstance.next();
+
+    return workflowInstance;
   }
 
 }
