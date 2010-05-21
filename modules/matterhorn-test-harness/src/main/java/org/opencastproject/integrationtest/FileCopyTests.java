46433cf5b3ba69be83eab89b56354a1fa4a8e218
johnk <johnk@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-21 00:15:45 +0000

    MH-3051 - Completed FileCopyTests java.io file copying.  roughed in java.nio copying, did not complete copying via Matterhorn API's due to project reassignment.  See JIRA for results.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@7058 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-test-harness/src/main/java/org/opencastproject/integrationtest/FileCopyTests.java
+++ modules/matterhorn-test-harness/src/main/java/org/opencastproject/integrationtest/FileCopyTests.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,6 +22,11 @@ import org.apache.commons.cli.PosixParser;
 
 public class FileCopyTests {
 
+    private static enum COPY_TYPE {
+        IO, NIO, API
+    };
+
+    private static COPY_TYPE copyType = COPY_TYPE.IO;
     private static File sourceDir;
     private static File destDir;
     private static int numCopies;
@@ -39,7 +45,8 @@ public class FileCopyTests {
         options.addOption(new Option("d", "dest", true, "destination directory"));
         options.addOption(new Option("n", "number", true, "number of concurrent copies to make"));
         options.addOption(new Option("m", "message", true, "optional message describing test"));
-        
+        options.addOption(new Option("t", "type", true, "type of file copy, one of IO, NIO, API, defaults to IO"));
+
         CommandLineParser parser = new PosixParser();
         CommandLine line = null;
         try {
@@ -82,72 +89,150 @@ public class FileCopyTests {
             System.out.println("missing required parameter -n or --number");
             System.exit(1);
         }
-        
+
         if (line.hasOption("message")) {
             String message = line.getOptionValue("message");
             System.out.println(message);
         }
 
+        if (line.hasOption("type")) {
+            String type = line.getOptionValue("type");
+            if (type.equalsIgnoreCase(COPY_TYPE.IO.toString())) {
+                copyType = COPY_TYPE.IO;
+            }
+            else if (type.equalsIgnoreCase(COPY_TYPE.NIO.toString())) {
+                copyType = COPY_TYPE.NIO;
+            }
+            else if (type.equalsIgnoreCase(COPY_TYPE.API.toString())) {
+                copyType = COPY_TYPE.API;
+            }
+            else {
+                System.out.println("wrong type parameter, must be one of IO, NIO or API");
+                System.exit(1);
+            }
+        }
+
         File[] sourceFiles = sourceDir.listFiles();
         numCopies = sourceFiles.length < numCopies ? sourceFiles.length : numCopies;
         executor = Executors.newFixedThreadPool(numCopies);
-        Runnable copyTask;
+        Runnable copyTask = null;
         final CountDownLatch startGate = new CountDownLatch(1);
         final CountDownLatch endGate = new CountDownLatch(numCopies);
         for (int i = 0; i < numCopies; i++) {
             final File inputFile = sourceFiles[i];
             final File outputFile = new File(destDir, inputFile.getName());
-            copyTask = new Runnable() {
-                final int BUFF_SIZE = 1000000;
-                final byte[] buffer = new byte[BUFF_SIZE];
+            if (copyType.compareTo(COPY_TYPE.IO) == 0) {
+                copyTask = new Runnable() {
+                    final int BUFF_SIZE = 1000000;
+                    final byte[] buffer = new byte[BUFF_SIZE];
 
-                public void run() {
-                    try {
-                        startGate.await();
-                        InputStream in = null;
-                        OutputStream out = null;
-                        int counter = 0;
+                    public void run() {
                         try {
-                            String threadName = Thread.currentThread().getName().trim();
-                            char lastThreadNameChar = threadName.charAt(threadName.length() - 1);
-                            System.out.println("copying " + inputFile.getCanonicalPath() + " of size " + inputFile.length()/1000000 + " MB to " + outputFile.getCanonicalPath() + " in thread "
-                                    + threadName);
-                            in = new FileInputStream(inputFile);
-                            out = new FileOutputStream(outputFile);
-                            while (true) {
-                                int amountRead = in.read(buffer);
-                                if (amountRead == -1) {
-                                    break;
+                            startGate.await();
+                            InputStream in = null;
+                            OutputStream out = null;
+                            int counter = 0;
+                            try {
+                                String threadName = Thread.currentThread().getName().trim();
+                                char lastThreadNameChar = threadName.charAt(threadName.length() - 1);
+                                System.out.println("copying " + inputFile.getCanonicalPath() + " of size " + inputFile.length() / 1000000 + " MB to "
+                                        + outputFile.getCanonicalPath() + " in thread " + threadName);
+                                in = new FileInputStream(inputFile);
+                                out = new FileOutputStream(outputFile);
+                                while (true) {
+                                    int amountRead = in.read(buffer);
+                                    if (amountRead == -1) {
+                                        break;
+                                    }
+                                    out.write(buffer, 0, amountRead);
+                                    counter++;
+                                    if ((counter % 10) == 0)
+                                        System.out.print(lastThreadNameChar);
                                 }
-                                out.write(buffer, 0, amountRead);
-                                counter++;
-                                if ((counter % 10) == 0) System.out.print(lastThreadNameChar);
                             }
+                            finally {
+                                if (in != null) {
+                                    in.close();
+                                }
+                                if (out != null) {
+                                    out.close();
+                                }
+                            }
+                        }
+                        catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
                         finally {
-                            if (in != null) {
-                                in.close();
-                            }
-                            if (out != null) {
-                                out.close();
-                            }
+                            endGate.countDown();
+                            System.out.println("\nFile copy completed in thread " + Thread.currentThread().getName());
                         }
                     }
-                    catch (FileNotFoundException e) {
-                        e.printStackTrace();
+                };
+            }
+            else if (copyType.compareTo(COPY_TYPE.NIO) == 0) {
+                copyTask = new Runnable() {
+                    public void run() {
+                        String threadName = Thread.currentThread().getName().trim();
+                        char lastThreadNameChar = threadName.charAt(threadName.length() - 1);
+
+                        FileChannel source = null;
+                        FileChannel destination = null;
+                        try {
+                            if (!outputFile.exists()) {
+                                outputFile.createNewFile();
+                            }
+                            System.out.println("copying " + inputFile.getCanonicalPath() + " of size " + inputFile.length() / 1000000 + " MB to "
+                                    + outputFile.getCanonicalPath() + " in thread " + threadName);
+                            source = new FileInputStream(inputFile).getChannel();
+                            destination = new FileOutputStream(outputFile).getChannel();
+
+                            long count = 0, counter = 0;
+                            long size = inputFile.length();
+                            while ((count += source.transferTo(count, 10000, destination)) < size) {
+                                if ((counter % 100) == 0) System.out.print(lastThreadNameChar);
+                                counter++;
+                            }
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        finally {
+                            if (source != null) {
+                                try {
+                                    source.close();
+                                }
+                                catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (destination != null) {
+                                try {
+                                    destination.close();
+                                }
+                                catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            endGate.countDown();
+                            System.out.println("\nFile copy completed in thread " + Thread.currentThread().getName());
+                        }
                     }
-                    catch (IOException e) {
-                        e.printStackTrace();
+                };
+            }
+            else if (copyType.compareTo(COPY_TYPE.API) == 0) {
+                copyTask = new Runnable() {
+                    public void run() {
+                        System.out.println("Copies via application API not implemented yet");
+                        System.exit(1);
                     }
-                    catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    finally {
-                        endGate.countDown();
-                        System.out.println("\nFile copy completed in thread " + Thread.currentThread().getName());
-                    }
-                }
-            };
+                };
+            }
             executor.execute(copyTask);
         }
         long start = System.nanoTime();
@@ -161,6 +246,33 @@ public class FileCopyTests {
         long end = System.nanoTime();
         System.out.println("Copied " + numCopies + " files in " + (float) (end - start) / 1000000000 + " seconds");
         System.exit(0);
+    }
+
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+
+            long count = 0;
+            long size = sourceFile.length();
+            while ((count += destination.transferFrom(source, count, size - count)) < size) {
+
+            }
+        }
+        finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
     }
 
 }
