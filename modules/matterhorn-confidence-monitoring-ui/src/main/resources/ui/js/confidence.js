925572bf3c187907ad832cceb6992f6bd44fbc44
msutton <msutton@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-25 21:54:34 +0000

    MH-3750, Updates to confidence monitoring.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@7152 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-confidence-monitoring-ui/src/main/resources/ui/js/confidence.js
+++ modules/matterhorn-confidence-monitoring-ui/src/main/resources/ui/js/confidence.js
@@ -1,45 +1,86 @@
-var CAPTURE_AGENT_CONFIDENCE_MONITORING_URL = "http://orochi.unl.edu:8080/confidence/rest";
+var CAPTURE_AGENT_CONFIDENCE_MONITORING_URL = "/confidence/rest";
 var AudioBar = {} || AudioBar;
 var Monitor = {} || Monitor;
 Monitor.intervalImgId = null;
 Monitor.intervalAudioId = null;
-Monitor.selectedDevice = null;
+Monitor.selectedVideoDevice = null;
+Monitor.selectedAudioDevice = null;
+Monitor.devices = [];
 
 Monitor.loadDevices = function(){
   //load the devices
   $.get(CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/devices", function(data){
-        //do stuff to make a device array.
-        var devices = $('name', data);
-        log(devices);
-        for(d in devices){
-          devices[d].text();
-          $('#device_tab').append('<li>' + devices[d] + '</li>');
-        }
-      });
+    //do stuff to make a device array.
+    var devices = $('ns1\\:agent-device', data).toArray();
+    log(devices);
+    Monitor.devices = [];
+    for(d in devices){
+   	  var devName = $('name', devices[d]).text();
+   	  var devType = $('type', devices[d]).text();
+   	  var i = Monitor.devices.push({name: devName, type: devType});
+      $('#device_tab').append('<li onclick="Monitor.selectDevice(' + (i - 1) + ')">' + devName + '</li>');
+    }
+	});
 }
 
-Monitor.selectDevice = function(device){
-  if(Monitor.selectedDevice != device){
-    if(Monitor.intervalId){ 
-      clearInterval(Monitor.intervalImgId); 
-      clearInterval(Monitor.intervalAudioId);
-    }
-    Monitor.intervalImgId = setInterval(Monitor.updateImg, 5000);
-    Monitor.intervalAudioId = setInterval(Monitor.updateAudio, 1000);
-  }
+Monitor.selectDevice = function(index){
+	var device = Monitor.devices[index];
+	if(device && device.type && device.name){
+		switch(device.type){
+			case 'video':
+				Monitor.videoDevice(index);
+				break;
+			case 'audio':
+				Monitor.audioDevice(index);
+			  break;
+			default:
+				log('Bad device selected.');
+				break;
+		}
+	}
+}
+
+Monitor.videoDevice = function(index){
+	if(Monitor.selectedVideoDevice != index){
+		Monitor.selectedVideoDevice = index;
+		clearInterval(Monitor.intervalImgId);
+	}
+	Monitor.intervalImgId = setInterval(Monitor.updateImg, 30000); //30 Second refresh on image.
+}
+
+Monitor.audioDevice = function(index){
+	if(Monitor.selectedAudioDevice != index){
+		Monitor.selectedAudioDevice = index;
+		clearInterval(Monitor.intervalAudioId);
+	}
+	Monitor.intervalAudioId = setInterval(Monitor.updateAudio, 5000); //5 Second refresh on audio
 }
 
 Monitor.updateImg = function(){
-  var imgGrab = CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/" + Monitor.selectedDevice;
+	log('update image');
+  var imgGrab = CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/" + Monitor.devices[Monitor.selectedVideoDevice].name;
   $("#image_preview").replaceWith('<img src="' + imgGrab + '"></img>');
 }
 
 Monitor.updateAudio = function(){
-  
+	log('update audio');
+	$.get(CAPTURE_AGENT_CONFIDENCE_MONITORING_URL + "/audio/" + Monitor.devices[Monitor.selectedAudioDevice].name + "/0",
+	function(data){ 
+		var a = data.split('\n');
+		var dbLevel = parseFloat(a[1]);
+		if(dbLevel && dbLevel != 'NaN'){
+			AudioBar.setValue(dbLevel);
+		}else{
+			log('Bad audio levels', dbLevel);
+		}
+	});
 }
 
-AudioBar.setValue = function(){
-  
+AudioBar.setValue = function(dbLevel){
+	var level_pct = Math.round((1 - Math.pow(10, dbLevel/20)) * 100);
+	log(level_pct, dbLevel)
+  $('#left_mask').css('height', level_pct + "%");
+  //$('#right_mask').css('height', right_pct + "%");
 }
 
 window.log = function(){
