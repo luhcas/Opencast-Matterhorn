3f1717daeb9d10a7b71a04ab0885785fbd2debcb
rrolf <rrolf@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-28 07:11:47 +0000

    MH-3933 The user will no longer be asked to install OpenCaps, but the scripts will still be copied to the VM
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@7211 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/ubuntu_vm/matterhorn_setup.sh
+++ docs/scripts/ubuntu_vm/matterhorn_setup.sh
@@ -253,12 +253,12 @@ else
   echo "**** Do you want to install ffmpeg? [y/N]"
   read ffresp
 
-  echo "**** Do you want to install OpenCaps? [y/N]"
-  read opencaps
-  if [ $opencaps = "y" ] || [ $opencaps = "Y" ]; then
-    echo "**** Install OpenCaps as Matterhorn plugin only? [Y/n]"
-    read opencapsminimal
-  fi
+#  echo "**** Do you want to install OpenCaps? [y/N]"
+#  read opencaps
+#  if [ $opencaps = "y" ] || [ $opencaps = "Y" ]; then
+#    echo "**** Install OpenCaps as Matterhorn plugin only? [Y/n]"
+#    read opencapsminimal
+#  fi
 
 
   # update felix config (url)
