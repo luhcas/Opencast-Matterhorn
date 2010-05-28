92e68437ab7d6178488266a6a697e7db057bffd0
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-28 19:14:24 +0000

    MH-3942 added symlinks to startup/shutdown/update scripts
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@7234 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/felix/bin/start_matterhorn.sh
+++ docs/felix/bin/start_matterhorn.sh
@@ -1,48 +1,80 @@
-##
-# Configure these variables to match your environment
-##
+#!/bin/bash
 
-if [ ! -z $FELIX_HOME ]; then
+if [ -n "${FELIX_HOME:-x}" ]; then
   FELIX=$FELIX_HOME
 else
-  FELIX="/Applications/Matterhorn"
+  FELIX="/opt/matterhorn/felix"
 fi
 
-if [ ! -z $M2_REPO ]; then
+
+if [ -n "${M2_REPO:-x}" ]; then
   M2_REPO=$M2_REPO
 else
-  M2_REPO="/Users/johndoe/.m2/repository"
+  M2_REPO="/home/opencast/.m2/repository"
 fi
 
-if [ ! -z $OPENCAST_LOGDIR ]; then
-  LOGDIR=$OPENCAST_LOGDIR
-else
-  LOGDIR=$FELIX/logs
-fi
 
-##
-# To enable the debugger on the vm, enable all of the following options
-##
+MATTERHORN_HOME=/opt/matterhorn
+MATTERHORN_CONF=/opt/matterhorn/felix/conf
+MATTERHORN_LIB=/opt/matterhorn/felix
+MATTERHORN_LOG=/opt/matterhorn/log
+FELIX_HOME=/opt/matterhorn/felix
 
-DEBUG_PORT="8000"
-DEBUG_SUSPEND="n"
-#DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
-
-##
-# Only change the line below if you want to customize the server
-##
-
+#
+# Define Constants
+#
 MAVEN_ARG="-DM2_REPO=$M2_REPO"
-FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$FELIX/load"
-PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$FELIX/conf"
-PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dopencast.logdir=$LOGDIR"
-UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX/conf/services/java.util.logging.properties"
+MATTERHORN_BUNDLE_DIR=$MATTERHORN_LIB/load
+MATTERHORN_WORK_DIR=$MATTERHORN_LIB/work
+MATTERHORN_STATIC_DIR=$MATTERHORN_WORK_DIR/opencast/static
+MATTERHORN_CACHE_DIR=$MATTERHORN_LIB/felix-cache
+MATTERHORN_LOG_FILE=/opt/matterhorn/log/felix.log
+
+#
+# Check if nightly is already running
+#
+FELIX_PID=`ps ux | awk '/felix.jar/ && !/awk/ {print $2}'`
+if [ ! -z $FELIX_PID ]; then
+  echo "Felix is already running with pid $FELIX_PID"
+  exit 1
+fi
+
+#
+# Clear the matterhorn work directory
+#
+# rm -rf $MATTERHORN_WORK_DIR
+
+#
+# Create the directories under /var/lib/matterhorn
+#
+
+mkdir -p $MATTERHORN_WORK_DIR
+mkdir -p $MATTERHORN_WORK_DIR/opencast
+mkdir -p $MATTERHORN_STATIC_DIR
+mkdir -p $MATTERHORN_BUNDLE_DIR
+
+# Save current version
+svnversion > $MATTERHORN_STATIC_DIR/version.txt
+
+#
+# Cd into the work dir, since some java code creates work files in the
+# directory used when starting the vm.
+#
+cd $MATTERHORN_WORK_DIR
+
+#
+# Start felix
+#
+FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$MATTERHORN_BUNDLE_DIR"
+PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$MATTERHORN_CONF"
+CXF_OPTS="-Djava.util.logging.config.file=$MATTERHORN_CONF/cxf.properties"
+JAVA_OPTS="-Xms256m -Xmx512m -XX:PermSize=64m -XX:MaxPermSize=128m -Djava.io.tmpdir=$MATTERHORN_WORK_DIR"
+DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dopencast.logdir=$MATTERHORN_LOG"
+UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX_HOME/conf/services/java.util.logging.properties"
 GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
 
-# Clear the felix cache directory
-FELIX_CACHE="$FELIX/felix-cache"
-rm -rf $FELIX_CACHE
 
-# Finally start felix
-cd $FELIX
-java $DEBUG_OPTS $GRAPHICS_OPTS $MAVEN_ARG $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS $UTIL_LOGGING_OPTS $CXF_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHE
+# Start felix
+nohup java $JAVA_OPTS $MAVEN_ARG $DEBUG_OPTS $GRAPHICS_OPTS $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $CXF_OPTS $PAX_LOGGING_OPTS $UTIL_LOGGING_OPTS -jar $FELIX_HOME/bin/felix.jar $MATTERHORN_CACHE_DIR >> $MATTERHORN_LOG_FILE &
+
---
+++ docs/felix/bin/update-matterhorn.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd /opt/matterhorn/matterhorn_trunk
+
+# update from svn
+if [ -z $1 ]
+then
+  svn update
+else
+  svn update -r $1
+fi
+
+#stop felix
+sudo /home/opencast/shutdown.sh
+
+# Clean old jars
+rm -rf /opt/matterhorn/felix/load
+
+export MAVEN_OPTS='-Xms256m -Xmx960m -XX:PermSize=64m -XX:MaxPermSize=150m'
+
+# build matterhorn
+mvn clean install -DskipTests -DdeployTo=/opt/matterhorn/felix/load
+
+# creating backup of configuration
+tar -czf /home/opencast/felix-config-backup.tar.gz /opt/matterhorn/felix/conf/ 
+
+# update felix configuration
+cp -rf docs/felix/conf/* /opt/matterhorn/felix/conf/
+cd /home/opencast
+
+# update felix config (url)
+MY_IP=`ifconfig | grep "inet addr:" | grep -v 127.0.0.1 | awk '{print $2}' | cut -d':' -f2`
+sed -i "s/http:\/\/localhost:8080/http:\/\/$MY_IP:8080/" /opt/matterhorn/felix/conf/config.properties
+sed -i "s/rtmp:\/\/localhost\/matterhorn-engage/rtmp:\/\/$MY_IP\/matterhorn-engage/" /opt/matterhorn/felix/conf/config.properties
+sed -i 's/\${org.opencastproject.storage.dir}\/streams/\/opt\/matterhorn\/red5\/webapps\/matterhorn\/streams/' /opt/matterhorn/felix/conf/config.properties
+sed -i "s/conf\/security.xml/\/opt\/matterhorn\/felix\/conf\/security.xml/" /opt/matterhorn/felix/conf/config.properties
+# update capture properties
+sed -i "s/http:\/\/localhost:8080/http:\/\/$MY_IP:8080/" /opencast/config/capture.properties
+
+# restart felix
+sudo /home/opencast/startup.sh
