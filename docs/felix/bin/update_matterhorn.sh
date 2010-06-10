5d8de42f9f4517cd5b87c9e2c6f8f962c2d37bff
mtrehan <mtrehan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-06-10 04:29:25 +0000

    MH-4020 Changed felix/load to felix/matterhorn and added code to change felix/load/.cfg files to absolute path from relative path
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@7398 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/felix/bin/update_matterhorn.sh
+++ docs/felix/bin/update_matterhorn.sh
@@ -15,16 +15,18 @@ sudo /home/opencast/shutdown.sh
 
 # Clean old jars
 rm -rf /opt/matterhorn/felix/load
+rm -rf /opt/matterhorn/felix/matterhorn
 
 export MAVEN_OPTS='-Xms256m -Xmx960m -XX:PermSize=64m -XX:MaxPermSize=150m'
 
 # build matterhorn
-mvn clean install -DskipTests -DdeployTo=/opt/matterhorn/felix/load
+mvn clean install -DskipTests -DdeployTo=/opt/matterhorn/felix/matterhorn
 
 # creating backup of configuration
 tar -czf /home/opencast/felix-config-backup.tar.gz /opt/matterhorn/felix/conf/ 
 
 # update felix configuration
+rm -rf /opt/matterhorn/felix/conf
 cp -rf docs/felix/* /opt/matterhorn/felix/
 cd /home/opencast
 
@@ -36,6 +38,8 @@ sed -i 's/\${org.opencastproject.storage.dir}\/streams/\/opt\/matterhorn\/red5\/
 sed -i "s/conf\/security.xml/\/opt\/matterhorn\/felix\/conf\/security.xml/" /opt/matterhorn/felix/conf/config.properties
 # update capture properties
 sed -i "s/http:\/\/localhost:8080/http:\/\/$MY_IP:8080/" /opencast/config/capture.properties
+
+for i in /opt/matterhorn/felix/load/*.cfg; do sed -i 's/\.\//\/opt\/matterhorn\/felix\//' $i; done
 
 # restart felix
 sudo /home/opencast/startup.sh
