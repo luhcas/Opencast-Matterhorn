ae23c18bc6532ba324c1c95215fefea6a6f44a8c
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-06-20 15:14:59 +0000

    MH-4258 -- added mysql5 and postgres84 DDL scripts.  Also moved the dictionary scanner config file into it's proper location.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@7542 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ docs/scripts/ddl/mysql5.sql
@@ -0,0 +1,104 @@
+CREATE TABLE `ANNOTATION` (
+  `ID` int(11) NOT NULL,
+  `OUTPOINT` int(11) default NULL,
+  `INPOINT` int(11) default NULL,
+  `MEDIA_PACKAGE_ID` varchar(255) collate utf8_unicode_ci default NULL,
+  `SESSION_ID` varchar(255) collate utf8_unicode_ci default NULL,
+  `CREATED` datetime default NULL,
+  `LENGTH` int(11) default NULL,
+  `ANNOTATION_VAL` varchar(255) collate utf8_unicode_ci default NULL,
+  `ANNOTATION_KEY` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `CAPTURE_AGENT` (
+  `NAME` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `state` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `capabilities` blob,
+  `lastHeardFrom` bigint(20) NOT NULL,
+  `url` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `JOB` (
+  `ID` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `HOST` varchar(255) collate utf8_unicode_ci default NULL,
+  `STATUS` int(11) default NULL,
+  `ELEMENT_XML` mediumtext collate utf8_unicode_ci,
+  `TYPE` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_EVENT` (
+  `ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_EVENT_METADATA` (
+  `EVENT_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `METADATA_ID` bigint(20) NOT NULL,
+  PRIMARY KEY  (`EVENT_ID`,`METADATA_ID`),
+  KEY `FK_SCHED_EVENT_METADATA_METADATA_ID` (`METADATA_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_METADATA` (
+  `ID` bigint(20) NOT NULL,
+  `MD_VAL` varchar(255) collate utf8_unicode_ci default NULL,
+  `MD_KEY` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_R_EVENT` (
+  `ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `RECURRENCE` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_R_EVENT_ITEM` (
+  `REC_EVENT_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `EVENT_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`REC_EVENT_ID`,`EVENT_ID`),
+  KEY `FK_SCHED_R_EVENT_ITEM_EVENT_ID` (`EVENT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_R_EVENT_METADATA` (
+  `REC_EVENT_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `MD_ID` bigint(20) NOT NULL,
+  PRIMARY KEY  (`REC_EVENT_ID`,`MD_ID`),
+  KEY `FK_SCHED_R_EVENT_METADATA_MD_ID` (`MD_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SEQUENCE` (
+  `SEQ_NAME` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `SEQ_COUNT` decimal(38,0) default NULL,
+  PRIMARY KEY  (`SEQ_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SERIES` (
+  `ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SERIES_METADATA` (
+  `METADATA_KEY` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `METADATA_VAL` varchar(256) collate utf8_unicode_ci default NULL,
+  `SERIES_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`METADATA_KEY`,`SERIES_ID`),
+  KEY `FK_SERIES_METADATA_SERIES_ID` (`SERIES_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SERVICE_REGISTRATION` (
+  `ID` bigint(20) NOT NULL,
+  `HOST` varchar(255) collate utf8_unicode_ci default NULL,
+  `JOB_TYPE` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `UPLOAD` (
+  `ID` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `total` bigint(20) NOT NULL,
+  `received` bigint(20) NOT NULL,
+  `filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
---
+++ docs/scripts/ddl/postgres84.sql
@@ -0,0 +1,148 @@
+CREATE TABLE annotation (
+    id integer NOT NULL,
+    outpoint integer,
+    inpoint integer,
+    media_package_id character varying(255),
+    session_id character varying(255),
+    created timestamp without time zone,
+    length integer,
+    annotation_val character varying(255),
+    annotation_key character varying(255)
+);
+
+CREATE TABLE capture_agent (
+    name character varying(255) NOT NULL,
+    state character varying(255) NOT NULL,
+    capabilities bytea,
+    lastheardfrom bigint NOT NULL,
+    url character varying(255)
+);
+
+CREATE TABLE job (
+    id character varying(255) NOT NULL,
+    host character varying(255),
+    status integer,
+    element_xml text,
+    type character varying(255)
+);
+
+CREATE TABLE sched_event (
+    id character varying(128) NOT NULL
+);
+
+CREATE TABLE sched_event_metadata (
+    event_id character varying(128) NOT NULL,
+    metadata_id bigint NOT NULL
+);
+
+CREATE TABLE sched_metadata (
+    id bigint NOT NULL,
+    md_val character varying(255),
+    md_key character varying(255)
+);
+
+CREATE TABLE sched_r_event (
+    id character varying(128) NOT NULL,
+    recurrence character varying(255)
+);
+
+CREATE TABLE sched_r_event_item (
+    rec_event_id character varying(128) NOT NULL,
+    event_id character varying(128) NOT NULL
+);
+
+CREATE TABLE sched_r_event_metadata (
+    rec_event_id character varying(128) NOT NULL,
+    md_id bigint NOT NULL
+);
+
+CREATE TABLE sequence (
+    seq_name character varying(50) NOT NULL,
+    seq_count numeric(38,0)
+);
+
+CREATE TABLE series (
+    id character varying(128) NOT NULL
+);
+
+CREATE TABLE series_metadata (
+    metadata_key character varying(128) NOT NULL,
+    metadata_val character varying(256),
+    series_id character varying(128) NOT NULL
+);
+
+CREATE TABLE service_registration (
+    id bigint NOT NULL,
+    host character varying(255),
+    job_type character varying(255)
+);
+
+CREATE TABLE upload (
+    id character varying(255) NOT NULL,
+    total bigint NOT NULL,
+    received bigint NOT NULL,
+    filename character varying(255) NOT NULL
+);
+
+ALTER TABLE ONLY annotation
+    ADD CONSTRAINT annotation_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY capture_agent
+    ADD CONSTRAINT capture_agent_pkey PRIMARY KEY (name);
+
+ALTER TABLE ONLY job
+    ADD CONSTRAINT job_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY sched_event_metadata
+    ADD CONSTRAINT sched_event_metadata_pkey PRIMARY KEY (event_id, metadata_id);
+
+ALTER TABLE ONLY sched_event
+    ADD CONSTRAINT sched_event_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY sched_metadata
+    ADD CONSTRAINT sched_metadata_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY sched_r_event_item
+    ADD CONSTRAINT sched_r_event_item_pkey PRIMARY KEY (rec_event_id, event_id);
+
+ALTER TABLE ONLY sched_r_event_metadata
+    ADD CONSTRAINT sched_r_event_metadata_pkey PRIMARY KEY (rec_event_id, md_id);
+
+ALTER TABLE ONLY sched_r_event
+    ADD CONSTRAINT sched_r_event_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY sequence
+    ADD CONSTRAINT sequence_pkey PRIMARY KEY (seq_name);
+
+ALTER TABLE ONLY series_metadata
+    ADD CONSTRAINT series_metadata_pkey PRIMARY KEY (metadata_key, series_id);
+
+ALTER TABLE ONLY series
+    ADD CONSTRAINT series_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY service_registration
+    ADD CONSTRAINT service_registration_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY upload
+    ADD CONSTRAINT upload_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY sched_event_metadata
+    ADD CONSTRAINT fk_sched_event_metadata_event_id FOREIGN KEY (event_id) REFERENCES sched_event(id);
+
+ALTER TABLE ONLY sched_event_metadata
+    ADD CONSTRAINT fk_sched_event_metadata_metadata_id FOREIGN KEY (metadata_id) REFERENCES sched_metadata(id);
+
+ALTER TABLE ONLY sched_r_event_item
+    ADD CONSTRAINT fk_sched_r_event_item_event_id FOREIGN KEY (event_id) REFERENCES sched_event(id);
+
+ALTER TABLE ONLY sched_r_event_item
+    ADD CONSTRAINT fk_sched_r_event_item_rec_event_id FOREIGN KEY (rec_event_id) REFERENCES sched_r_event(id);
+
+ALTER TABLE ONLY sched_r_event_metadata
+    ADD CONSTRAINT fk_sched_r_event_metadata_md_id FOREIGN KEY (md_id) REFERENCES sched_metadata(id);
+
+ALTER TABLE ONLY sched_r_event_metadata
+    ADD CONSTRAINT fk_sched_r_event_metadata_rec_event_id FOREIGN KEY (rec_event_id) REFERENCES sched_r_event(id);
+
+ALTER TABLE ONLY series_metadata
+    ADD CONSTRAINT fk_series_metadata_series_id FOREIGN KEY (series_id) REFERENCES series(id);
