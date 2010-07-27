f386d547e4299857e62aca11602063899663d494
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-07-27 19:36:23 +0000

    Fix for MH-5008
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@8408 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/ubuntu_capture_agent/setup_environment.sh
+++ docs/scripts/ubuntu_capture_agent/setup_environment.sh
@@ -59,9 +59,9 @@ if [[ "$config" ]]; then
     # TODO: this property also admits a URL, rather than an IP. Remove the filter and check this still works for an arbitrary address.
     # > And how do we check if that URL is correct or not?
     ask -a -f '^[012]*[0-9]*[0-9]\.[012]*[0-9]*[0-9]\.[012]*[0-9]*[0-9]\.[012]*[0-9]*[0-9](:[0-9]*[0-9]*[0-9]*[0-9]*[0-9])?$'\
-        "Detected IP is $default_ip. Press [enter] to accept, or enter new address" ip
-    : ${ip:=$default_ip}
-    sed -i "s#^${SERVER_URL_KEY//./\\.}=.*\$#${SERVER_URL_KEY}=http://$ip#" "$GEN_PROPS"
+        "Detected IP is $default_ip. Press [enter] to accept, or enter a new address including protocol (eg: http://$default_ip)" ip
+    : ${ip:="http://"$default_ip}
+    sed -i "s#^${SERVER_URL_KEY//./\\.}=.*\$#${SERVER_URL_KEY}=$ip#" "$GEN_PROPS"
 fi
 
 # Prompt for the URL where the core lives.
