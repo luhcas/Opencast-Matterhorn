6b2f8b228a3361b24ee29f233d3b6641dc83ae56
jemden <jemden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-08-19 14:31:10 +0000

    MH-5038
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@8762 e6edf6fb-f266-4316-afb4-e53d95876a76

--- shared-resources/js/segments.js
+++ shared-resources/js/segments.js
@@ -64,12 +64,14 @@ Opencast.segments = ( function() {
        } else {
          // Disable and grey out "Slides" Tab
          $("#oc_btn-slides").removeClass("oc_btn-tabs");
+         $("#oc_btn-slides").removeClass("handcursor");
          $('#oc_btn-slides').attr("disabled", "true");
          $("#oc_btn-slides").css("color", "#dddddd");
          $("#oc_btn-slides").css("background-color", "#aaaaaa");
 
          // Disable and grey out "Slides Text" Tab
          $("#oc_btn-slidetext").removeClass("oc_btn-tabs");
+         $("#oc_btn-slidetext").removeClass("handcursor");
          $('#oc_btn-slidetext').attr("disabled", "true");
          $("#oc_btn-slidetext").css("color", "#dddddd");
          $("#oc_btn-slidetext").css("background-color", "#aaaaaa");
