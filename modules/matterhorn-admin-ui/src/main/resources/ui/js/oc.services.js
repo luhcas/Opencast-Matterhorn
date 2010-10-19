2d9e11d659671a9b8ff0de965516bd8a582504b3
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-10-19 12:57:50 +0000

    MH-5559 -- updated the js and html for the servers and services views
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@8993 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.services.js
+++ modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.services.js
@@ -9,22 +9,29 @@ Opencast.Services.servicesView = {};
  */
 Opencast.Services.buildServersView = function(data) {
   var servers = Opencast.Services.serversView;
-  jQuery.each(data, function(index, serviceInstance) {
-    var server = servers[serviceInstance.host];
+  jQuery.each(data.statistics.service, function(index, serviceInstance) {
+    var reg = serviceInstance.serviceRegistration;
+    var server = servers[reg.host];
     if(server == null) {
-      server = {'host' : serviceInstance.host, 'online' : serviceInstance.online, 'maintenance' : serviceInstance.maintenance};
-      servers[serviceInstance.host] = server;
+      server = {'host' : reg.host};
+      servers[reg.host] = server;
       server.services = [];
     }
+    // if the service is not a job producer, we don't show it here
+    if(!reg.jobproducer) return true;
+
     // Add the service type to this server
     var singleService = {};
     server.services.push(singleService);
-    var serviceTypeIdentifier = serviceInstance.type.replace(/\./g, "_");
+    var serviceTypeIdentifier = reg.type.replace(/\./g, "_");
     singleService.id = serviceTypeIdentifier;
-    singleService.running = serviceInstance.running;
-    singleService.meanRunTime = serviceInstance.meanRunTime;
-    singleService.queued = serviceInstance.queued;
-    singleService.meanQueueTime = serviceInstance.meanQueueTime;
+    singleService.path = reg.path;
+    singleService.online = reg.online;
+    singleService.maintenance = reg.maintenance;
+    singleService.running = serviceInstance.@running;
+    singleService.meanRunTime = serviceInstance.@meanruntime;
+    singleService.queued = serviceInstance.@queued;
+    singleService.meanQueueTime = serviceInstance.@meanqueuetime;
   });
 }
 
@@ -33,22 +40,28 @@ Opencast.Services.buildServersView = function(data) {
  */
 Opencast.Services.buildServicesView = function(data) {
   var services = Opencast.Services.servicesView;
-  jQuery.each(data, function(index, serviceInstance) {
-    var serviceTypeIdentifier = serviceInstance.type.replace(/\./g, "_");
+  jQuery.each(data.statistics.service, function(index, serviceInstance) {
+    var reg = serviceInstance.serviceRegistration;
+
+    // if the service is not a job producer, we don't show it here
+    if(!reg.jobproducer) return true;
+
+    var serviceTypeIdentifier = reg.type.replace(/\./g, "_");
     var service = services[serviceTypeIdentifier];
     if(service == null) {
-      service = {'id' : serviceTypeIdentifier, 'online' : serviceInstance.online, 'maintenance' : serviceInstance.maintenance};
+      service = {'id' : serviceTypeIdentifier, 'online' : reg.online, 'maintenance' : reg.maintenance};
       service.servers = [];
       services[serviceTypeIdentifier] = service;
     }
+
     // Add the server to this service
     var singleService = {};
     service.servers.push(singleService);
-    singleService.host = serviceInstance.host;
-    singleService.running = serviceInstance.running;
-    singleService.meanRunTime = serviceInstance.meanRunTime;
-    singleService.queued = serviceInstance.queued;
-    singleService.meanQueueTime = serviceInstance.meanQueueTime;
+    singleService.host = reg.host;
+    singleService.running = serviceInstance.@running;
+    singleService.meanRunTime = serviceInstance.@meanruntime;
+    singleService.queued = serviceInstance.@queued;
+    singleService.meanQueueTime = serviceInstance.@meanqueuetime;
   });
 }
 
@@ -56,14 +69,13 @@ Opencast.Services.buildServicesView = function(data) {
  * The labels for the UI.  TODO: i18n
  */
 Opencast.Services.labels = {
+    "org_opencastproject_caption"                : "Captioning",
     "org_opencastproject_analysis_text"          : "Text analysis",
-    "org_opencastproject_analysis_vsegmenter"    : "Video segmentation",
+    "org_opencastproject_analysis_segmenter"     : "Video segmentation",
     "org_opencastproject_composer"               : "Encoding, image extraction, and trimming",
     "org_opencastproject_distribution_download"  : "Media distribution (local downloads)",
     "org_opencastproject_distribution_streaming" : "Media distribution (local streaming)",
     "org_opencastproject_distribution_itunesu"   : "Media distribution (iTunes)",
     "org_opencastproject_distribution_youtube"   : "Media distribution (YouTube)",
-    "org_opencastproject_inspection"             : "Media inspection",
-    "org_opencastproject_search"                 : "Search",
-    "workingfiles"                               : "Working files repository"
+    "org_opencastproject_inspection"             : "Media inspection"
 }
