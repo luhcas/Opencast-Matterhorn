45220bddcbb93e176a2e0daa6bdd44d2df8068b7
rrolf <rrolf@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-12-03 13:58:13 +0000

    MH-5673 Updated init script based on Rubencinos code review and Tobias comment
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@9278 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/felix/bin/matterhorn_init_d.sh
+++ docs/felix/bin/matterhorn_init_d.sh
@@ -16,23 +16,53 @@ set -e
 # These variables are set in the configuration scripts.
 ##
 #eg:  /opt/matterhorn
-MATTERHORN=/opt/matterhorn
+MATTERHORN=${MATTERHORN_HOME:-/opt/matterhorn}
 #eg:  /opt/matterhorn/felix, or $MATTERHORN/felix
-export FELIX_HOME=$MATTERHORN/felix
+FELIX=${FELIX_HOME:-$MATTERHORN/felix}
 #eg:  /opt/matterhorn/capture-agent, or $MATTERHORN/capture-agent
 CA=$CA_DIR
 #eg:  Commonly opencast or matterhorn.  Can also be your normal user if you are testing.
-MATTERHORN_USER=$USERNAME
-export M2_REPO=/home/$USERNAME
+MATTERHORN_USER=$USER
+M2_REPOSITORY=${M2_REPO:-/home/$MATTERHORN_USER}
 #Enable this if this machine is a CA.  This will enable capture device autoconfiguration.
 IS_CA=false
 
-NAME=matterhorn
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:$FELIX_HOME
+LOGDIR=$FELIX/logs
 
+NAME=matterhorn
+PATH=$PATH:$FELIX
+
+##
+# To enable the debugger on the vm, enable all of the following options
+##
+
+DEBUG_PORT="8000"
+DEBUG_SUSPEND="n"
+#DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
+
+##
+# Only change the line below if you want to customize the server
+##
+
+MAVEN_ARG="-DM2_REPO=$M2_REPOSITORY"
+FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$FELIX/load"
+PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$FELIX/conf"
+PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dopencast.logdir=$LOGDIR"
+UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX/conf/services/java.util.logging.properties"
+GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
+JAVA_OPTS="-Xms256m -Xmx256m -XX:MaxPermSize=128m"
+
+FELIX_CACHE="$FELIX/felix-cache"
+
+
+###############################
+### NO CHANGES NEEDED BELOW ###
+###############################
+
+ 
 case "$1" in
   start)
-    echo -n "Starting Matterhorn: " 
+    echo -n "Starting Matterhorn as user $MATTERHORN_USER: " 
     if $IS_CA ; then
         $CA/device_config.sh
         if [ -d $CA/epiphan_driver -a -z "$(lsmod | grep vga2usb)" ]; then
@@ -40,13 +70,19 @@ case "$1" in
         fi
     fi
 
+# check if felix is already running
+    MATTERHORN_PID=`ps aux | awk '/felix.jar/ && !/awk/ {print $2}'`
+    if [ ! -z "$MATTERHORN_PID" ]; then
+      echo "OpenCast Matterhorn is already running"
+      exit 1
+    fi
+
+
 # copy away the gogo shell files otherwise matterhorn can not run in background
-    if [ ! -d $FELIX/backup ]; then 
-      mkdir $FELIX/backup
-    fi
-    if [ -f $FELIX/bundle/org.apache.felix.gogo.* ]; then
-      mv -f $FELIX/bundle/org.apache.felix.gogo.* $FELIX/backup/
-    fi
+    mkdir -p $FELIX/backup
+    for item in $(ls $FELIX/bundle/org.apache.felix.gogo.* 2> /dev/null); do
+       mv -f $item $FELIX/backup/
+    done
 
 # Make sure matterhorn bundles are reloaded
     if [ -d "$FELIX_CACHE" ]; then
@@ -57,20 +93,23 @@ case "$1" in
     fi
     
     cd $FELIX
-    su -c  "nohup $FELIX_HOME/bin/start_matterhorn.sh & > /dev/null 2>&1" $MATTERHORN_USER
+
+# starting felix
+
+    su -c "cd $FELIX && java -Dgosh.args='--noshutdown -c noop=true' $DEBUG_OPTS $GRAPHICS_OPTS $MAVEN_ARG $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS $UTIL_LOGGING_OPTS $CXF_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHE &" $MATTERHORN_USER > /dev/null 2> /dev/null
     echo "done." 
-    ;;
+    ;;    
   stop)
     echo -n "Stopping Matterhorn: " 
     MATTERHORN_PID=`ps aux | awk '/felix.jar/ && !/awk/ {print $2}'`
-    if [ -z $MATTERHORN_PID ]; then
+    if [ -z "$MATTERHORN_PID" ]; then
       echo "Matterhorn already stopped"
       exit 1
     fi
 
     kill $MATTERHORN_PID
 
-    sleep 10
+    sleep 7
 
     MATTERHORN_PID=`ps aux | awk '/felix.jar/ && !/awk/ {print $2}'`
     if [ ! -z $MATTERHORN_PID ]; then
@@ -94,11 +133,11 @@ case "$1" in
     ;;
   status)
     MATTERHORN_PID=`ps aux | awk '/felix.jar/ && !/awk/ {print $2}'`
-    if [ -z $MATTERHORN_PID ]; then
+    if [ -z "$MATTERHORN_PID" ]; then
       echo "Matterhorn is not running"
       exit 0
     else
-      echo "OpenCast Matterhorn is running"
+      echo "OpenCast Matterhorn is running with PID $MATTERHORN_PID"
       exit 0
     fi   
     ;;
