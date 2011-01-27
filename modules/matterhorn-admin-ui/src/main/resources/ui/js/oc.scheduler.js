5cd60a530cda8cd43f9407caff5a6d917791b863
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-01-27 10:32:20 +0000

    MH-5762: fixed
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@9668 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.scheduler.js
+++ modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.scheduler.js
@@ -781,7 +781,7 @@ ocScheduler.RegisterComponents = function(){
           }
         },
         validate: function(){
-          if(this.fields.recurEnd.datepicker && this.fields.recurStart.datepicker && ocScheduler.components.recurrenceDuration.validate() &&
+          if(this.fields.recurEnd.datepicker && this.fields.recurStart.datepicker &&    // ocScheduler.components.recurrenceDuration.validate() &&
              this.fields.recurStartTimeHour && this.fields.recurStartTimeMin &&
              this.fields.recurEnd.datepicker('getDate') > this.fields.recurStart.datepicker('getDate')){
             return true;
