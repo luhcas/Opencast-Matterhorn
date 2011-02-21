d7a13edf6d8929ffeb87ace437a0a22c5dfde656
cab938 <cab938@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-02-21 01:55:00 +0000

    MH-6884 missed adding these files to previous patch
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@9944 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ modules/matterhorn-kernel/src/main/java/org/opencastproject/kernel/rest/docs/RestParameter.java
@@ -0,0 +1,27 @@
+package org.opencastproject.kernel.rest.docs;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * This annotation type is used for annotating parameters for RESTful query.
+ * This annotation type needs to be kept until runtime.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RestParameter {
+
+  /**
+   * @return a name of the parameter.
+   */
+  String name();
+  
+  /**
+   * @return a description of the parameter.
+   */
+  String description();
+  
+  /**
+   * @return a boolean indicating whether this parameter is required.
+   */
+  boolean isRequired();
+}
---
+++ modules/matterhorn-kernel/src/main/java/org/opencastproject/kernel/rest/docs/RestResponse.java
@@ -0,0 +1,23 @@
+package org.opencastproject.kernel.rest.docs;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * This annotation type is used for annotating responses for RESTful query.
+ * This annotation type needs to be kept until runtime.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RestResponse {
+
+  /**
+   * @return a HTTP response code, such as 200, 400 etc.
+   */
+  int responseCode();
+  
+  /**
+   * @return a description of the response.
+   */
+  String description();
+  
+}
