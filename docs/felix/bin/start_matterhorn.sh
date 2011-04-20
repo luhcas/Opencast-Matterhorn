b18779887fde246b02dc421d4cf5f00c6eb584c9
twunden <twunden@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-04-20 13:51:05 +0000

    MH-7624 -- made sure Matterhorn can be installed under paths that contain spaces
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@10316 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/felix/bin/start_matterhorn.sh
+++ docs/felix/bin/start_matterhorn.sh
@@ -4,22 +4,22 @@
 # Configure these variables to match your environment
 ##
 
-if [ ! -z $FELIX_HOME ]; then
-  FELIX=$FELIX_HOME
+if [ ! -z "$FELIX_HOME" ]; then
+  FELIX_HOME="$FELIX_HOME"
 else
-  FELIX="/Applications/Matterhorn"
+  FELIX_HOME="/Applications/Matterhorn"
 fi
 
-if [ ! -z $M2_REPO ]; then
-  M2_REPO=$M2_REPO
+if [ ! -z "$M2_REPO" ]; then
+  M2_REPO="$M2_REPO"
 else
-  M2_REPO="/Users/johndoe"
+  M2_REPO="/Users/johndoe/.m2/repository"
 fi
 
-if [ ! -z $OPENCAST_LOGDIR ]; then
-  LOGDIR=$OPENCAST_LOGDIR
+if [ ! -z "$OPENCAST_LOGDIR" ]; then
+  LOGDIR="$OPENCAST_LOGDIR"
 else
-  LOGDIR=$FELIX/logs
+  LOGDIR="$FELIX_HOME/logs"
 fi
 
 ##
@@ -35,10 +35,11 @@ DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,
 ##
 
 MAVEN_ARG="-DM2_REPO=$M2_REPO"
-FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$FELIX/load"
-PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$FELIX/conf"
-PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dopencast.logdir=$LOGDIR"
-UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX/conf/services/java.util.logging.properties"
+FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$FELIX_HOME/load"
+PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$FELIX_HOME/conf"
+MATTERHORN_LOGGING_OPTS="-Dopencast.logdir=$LOGDIR"
+PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN"
+UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX_HOME/conf/services/java.util.logging.properties"
 GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
 JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=256m"
 
@@ -54,16 +55,17 @@ then
 	fi
 fi
 
-FELIX_CACHE="$FELIX/felix-cache"
+FELIX_CACHE="$FELIX_HOME/felix-cache"
 
 # Make sure matterhorn bundles are reloaded
 if [ -d "$FELIX_CACHE" ]; then
+  cd "$FELIX_CACHE"
   echo "Removing cached matterhorn bundles from $FELIX_CACHE"
-  for bundle in `find "$FELIX_CACHE" -type f -name bundle.location | xargs grep --files-with-match -e "file:" | sed -e s/bundle.location// `; do
-    rm -r $bundle
+  for bundle in `find . -type f -name "bundle.location" | xargs grep --files-with-match -e "file:" | sed -e 's/.\/\(.*\)\/bundle.location/\1/'`; do
+    rm -r "$FELIX_CACHE/$bundle"
   done
 fi
 
 # Finally start felix
-cd $FELIX
-java $DEBUG_OPTS $GRAPHICS_OPTS $MAVEN_ARG $JAVA_OPTS $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS $UTIL_LOGGING_OPTS $CXF_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHE
+cd "$FELIX_HOME"
+java $DEBUG_OPTS $GRAPHICS_OPTS $MAVEN_ARG $JAVA_OPTS "$FELIX_FILEINSTALL_OPTS" "$PAX_CONFMAN_OPTS" $PAX_LOGGING_OPTS "$MATTERHORN_LOGGING_OPTS" "$UTIL_LOGGING_OPTS" -jar "$FELIX_HOME/bin/felix.jar" "$FELIX_CACHE"
