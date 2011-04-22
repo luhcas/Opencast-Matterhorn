67a654b33ba4d43f55b85f5c4418554a4fdc1d0f
jholtzman <jholtzman@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-04-22 01:48:25 +0000

    MH-7614 -- updated the streaming distribution service so it uses the new rest doc annotations
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@10325 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-distribution-service-streaming/src/main/java/org/opencastproject/distribution/streaming/endpoint/StreamingDistributionRestService.java
+++ modules/matterhorn-distribution-service-streaming/src/main/java/org/opencastproject/distribution/streaming/endpoint/StreamingDistributionRestService.java
@@ -15,6 +15,8 @@
  */
 package org.opencastproject.distribution.streaming.endpoint;
 
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
 import org.opencastproject.distribution.api.DistributionService;
 import org.opencastproject.job.api.JaxbJob;
 import org.opencastproject.job.api.Job;
@@ -22,21 +24,18 @@ import org.opencastproject.job.api.JobProducer;
 import org.opencastproject.mediapackage.MediaPackageElement;
 import org.opencastproject.mediapackage.MediaPackageElementParser;
 import org.opencastproject.rest.AbstractJobProducerEndpoint;
-import org.opencastproject.rest.RestConstants;
 import org.opencastproject.serviceregistry.api.ServiceRegistry;
-import org.opencastproject.util.DocUtil;
-import org.opencastproject.util.doc.DocRestData;
-import org.opencastproject.util.doc.Format;
-import org.opencastproject.util.doc.Param;
-import org.opencastproject.util.doc.RestEndpoint;
-import org.opencastproject.util.doc.RestTestForm;
+import org.opencastproject.util.doc.rest.RestParameter;
+import org.opencastproject.util.doc.rest.RestParameter.Type;
+import org.opencastproject.util.doc.rest.RestQuery;
+import org.opencastproject.util.doc.rest.RestResponse;
+import org.opencastproject.util.doc.rest.RestService;
 
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -48,6 +47,12 @@ import javax.ws.rs.core.Response.Status;
  * Rest endpoint for distributing media to the local streaming distribution channel.
  */
 @Path("/")
+@RestService(name = "localdistributionservice", title = "Local Distribution Service", abstractText = "This service "
+        + "distributes media packages to the Matterhorn feed and engage services.", notes = { "All paths above are "
+        + "relative to the REST endpoint base (something like http://your.server/files).  If the service is down "
+        + "or not working it will return a status 503, this means the the underlying service is not working and is "
+        + "either restarting or has failed. A status code 500 means a general failure has occurred which is not "
+        + "recoverable and was not anticipated. In other words, there is a bug!" })
 public class StreamingDistributionRestService extends AbstractJobProducerEndpoint {
 
   /** The logger */
@@ -66,8 +71,6 @@ public class StreamingDistributionRestService extends AbstractJobProducerEndpoin
    *          this component's context
    */
   public void activate(ComponentContext cc) {
-    String serviceUrl = (String) cc.getProperties().get(RestConstants.SERVICE_PATH_PROPERTY);
-    docs = generateDocs(serviceUrl);
   }
 
   /**
@@ -91,6 +94,9 @@ public class StreamingDistributionRestService extends AbstractJobProducerEndpoin
   @POST
   @Path("/")
   @Produces(MediaType.TEXT_XML)
+  @RestQuery(name = "distribute", description = "Distribute a media package to this distribution channel", returnDescription = "The job that can be used to track the distribution", restParameters = {
+          @RestParameter(name = "mediapackageId", isRequired = true, description = "The mediapackage identifier", type = Type.STRING),
+          @RestParameter(name = "element", isRequired = true, description = "The element to distribute", type = Type.TEXT) }, reponses = { @RestResponse(responseCode = SC_OK, description = "An XML representation of the distribution job") })
   public Response distribute(@FormParam("mediapackageId") String mediaPackageId, @FormParam("element") String elementXml)
           throws Exception {
     Job job = null;
@@ -108,6 +114,7 @@ public class StreamingDistributionRestService extends AbstractJobProducerEndpoin
   @POST
   @Path("/retract")
   @Produces(MediaType.TEXT_XML)
+  @RestQuery(name = "retract", description = "Retract a media package from this distribution channel", returnDescription = "The job that can be used to track the retraction", restParameters = { @RestParameter(name = "mediapackageId", isRequired = true, description = "The mediapackage identifier", type = Type.STRING) }, reponses = { @RestResponse(responseCode = SC_OK, description = "An XML representation of the retraction job") })
   public Response retract(@FormParam("mediapackageId") String mediaPackageId) throws Exception {
     Job job = null;
     try {
@@ -117,50 +124,6 @@ public class StreamingDistributionRestService extends AbstractJobProducerEndpoin
       return Response.serverError().status(Status.INTERNAL_SERVER_ERROR).build();
     }
     return Response.ok(new JaxbJob(job)).build();
-  }
-
-  @GET
-  @Produces(MediaType.TEXT_HTML)
-  @Path("docs")
-  public String getDocumentation() {
-    return docs;
-  }
-
-  protected String docs;
-  private String[] notes = {
-          "All paths above are relative to the REST endpoint base (something like http://your.server/files)",
-          "If the service is down or not working it will return a status 503, this means the the underlying service "
-                  + "is not working and is either restarting or has failed. A status code 500 means a general failure has "
-                  + "occurred which is not recoverable and was not anticipated. In other words, there is a bug!" };
-
-  private String generateDocs(String serviceUrl) {
-    DocRestData data = new DocRestData("streamingdistributionservice", "Streaming Distribution Service", serviceUrl,
-            notes);
-
-    // abstract
-    data.setAbstract("This service distributes media packages to the streaming server.");
-
-    // distribute
-    RestEndpoint endpoint = new RestEndpoint("distribute", RestEndpoint.Method.POST, "/",
-            "Distribute a media package to the streaming server");
-    endpoint.addFormat(new Format("XML", null, null));
-    endpoint.addRequiredParam(new Param("mediapackageId", Param.Type.STRING, null, "The media package identifier"));
-    endpoint.addRequiredParam(new Param("element", Param.Type.TEXT, null, "A media package element"));
-    endpoint.addStatus(org.opencastproject.util.doc.Status.ok(null));
-    endpoint.addStatus(org.opencastproject.util.doc.Status.error(null));
-    endpoint.setTestForm(RestTestForm.auto());
-    data.addEndpoint(RestEndpoint.Type.WRITE, endpoint);
-
-    // retract
-    RestEndpoint retractEndpoint = new RestEndpoint("retract", RestEndpoint.Method.POST, "/retract",
-            "Retract a media package from the streaming server");
-    retractEndpoint.addRequiredParam(new Param("mediapackageId", Param.Type.STRING, null, "The media package ID"));
-    retractEndpoint.addStatus(org.opencastproject.util.doc.Status.ok(null));
-    retractEndpoint.addStatus(org.opencastproject.util.doc.Status.error(null));
-    retractEndpoint.setTestForm(RestTestForm.auto());
-    data.addEndpoint(RestEndpoint.Type.WRITE, retractEndpoint);
-
-    return DocUtil.generate(data);
   }
 
   /**
