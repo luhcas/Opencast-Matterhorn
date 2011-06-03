41eedf7ed6c0ec20e0108dafc34e787af3d884b6
matjaz <matjaz@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-06-03 17:38:45 +0000

    MH-7835 Added master config.txt with the list of all URLs and packages needed for check-prereq script. Also added script check-urls, which reports which URLs from master config.txt are dead.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@10549 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/scripts/3rd_party/wget.java
+++ docs/scripts/3rd_party/wget.java
@@ -1,47 +1,191 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class wget
 {
-  public static void main(String[] args)
+  private static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+  public static class _rcm
   {
+    private int rc;
+    private String message;
+
+    public _rcm(int _rc, String _message) {
+      this.rc = _rc;
+      this.message = _message;
+    }
+  }
+
+  public static void getHttpResponseCode(HttpURLConnection huc, _rcm rcm) throws Exception
+  {
+    try {
+      /* set timeout and user agent string */
+      huc.setConnectTimeout(30000);
+      huc.setReadTimeout(30000);
+      huc.setRequestProperty("User-Agent",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
+//    huc.setRequestProperty("User-Agent",
+//      "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.9) Gecko/20100824 Firefox/3.6.9");
+      huc.setRequestProperty("Connection", "close");
+
+      rcm.rc = huc.getResponseCode();
+      rcm.message = huc.getURL().toString();  // needed in case of 404
+    }
+    catch (MalformedURLException mue) {
+      rcm.rc = 420;  // Malformed URL
+      rcm.message = mue.getMessage();
+    }
+    catch (UnknownHostException uhe) {
+      rcm.rc = 421;  // Unknown host
+      rcm.message = uhe.getMessage();
+    }
+    catch (FileNotFoundException fnfe) {  // never thrown?!
+      rcm.rc = 404;  // File Not Found
+      rcm.message = fnfe.getMessage();
+    }
+    catch (ConnectException ce) {
+      rcm.rc = 408;  // Request timeout
+      rcm.message = ce.getMessage();
+    }
+    catch (SocketTimeoutException ste) {
+      rcm.rc = 408;  // Request timeout
+      rcm.message = ste.getMessage();
+    }
+    catch (IOException ioe) {
+      rcm.rc = 422;  // Other I/O Exception
+      rcm.message = ioe.getMessage();
+    }
+  } // getHttpResponseCode
+
+  public static void main(String[] args) throws Exception
+  {
+    boolean head = false;
+
+    for (String arg: args) {
+      if (arg.equals("-h")) {
+        head = true;
+
+        /* remove switch from argument list */
+        List<String> list = new ArrayList<String>(Arrays.asList(args));
+        list.removeAll(Arrays.asList(arg));
+        args = list.toArray(EMPTY_STRING_ARRAY);
+      }
+    }
+
     if (args.length < 1) {
-      System.err.println("Usage: java wget <URL>");
+      System.err.format("Usage: java %s [-h] <URL>\n", wget.class.getName());
+      System.err.format("  -h  fetch headers only\n");
       System.exit(1);
     }
 
-    String url = args[0];
+    String url = args[0];  // take only the first argument
 
     URL u;
+    HttpURLConnection huc;
     InputStream is = null;
     DataInputStream dis = null;
-    int c;
+    _rcm rcm = new _rcm(-1, "");
+    int rcc, ii, b;
 
     try {
       u = new URL(url);
-      is = u.openStream();
-      dis = new DataInputStream(new BufferedInputStream(is));
-      while ((c = dis.read()) != -1) {
-        System.out.write(c);
+      huc = (HttpURLConnection)u.openConnection();
+
+      /* Check for errors and repeat after sleep */
+      getHttpResponseCode(huc, rcm); ii = 0;
+      while (ii < 5) {
+        rcc = rcm.rc / 100;
+        if (rcm.rc == 408 || rcm.rc == 503) {
+          System.err.println("WARNING: Service unavailable or timeout occurred - repeating...");
+          Thread.currentThread().sleep(15000);
+
+          huc = (HttpURLConnection)u.openConnection();
+
+          getHttpResponseCode(huc, rcm); ii = ii + 1;
+        }
+        else if (rcm.rc < 0) {  // Response is not valid HTTP
+          throw new IOException("Response is not valid HTTP");
+        }
+        else if (rcc == 1) break;  // Informational
+        else if (rcc == 2) break;  // Success
+        else if (rcc == 3) break;  // Redirection
+        else if (rcc == 4) {  // Client Error
+          if (rcm.rc == 420)
+            throw new MalformedURLException(rcm.message);
+          else if (rcm.rc == 421)
+            throw new UnknownHostException(rcm.message);
+          else if (rcm.rc == 404)
+            throw new FileNotFoundException(rcm.message);
+          else if (rcm.rc == 422)
+            throw new IOException(rcm.message);
+          else
+            throw new IOException("Client Error (" + rcm.rc + ")");
+        }
+        else if (rcc == 5) {  // Server Error
+          throw new IOException("Server Error (" + rcm.rc + ")");
+        }
+        else {  // Unknown Error
+          throw new IOException("Unknown Error (" + rcm.rc + ")");
+        }
+      } // while
+
+      is = huc.getInputStream();
+      if (!head) {
+        /* read stream and output it to stdout */
+        dis = new DataInputStream(new BufferedInputStream(is));
+        while ((b = dis.read()) != -1) {
+          System.out.write(b);
+        }
       }
+    }
+    catch (ClassCastException cce) {
+      System.err.println("ERROR: Wrong protocol");
+      System.err.println(cce.toString());
+      System.exit(2);
     }
     catch (MalformedURLException mue) {
       System.err.println("ERROR: Malformed URL specified");
-      mue.printStackTrace();
-      System.exit(2);
+      System.err.println(mue.toString());
+      System.exit(3);
+    }
+    catch (UnknownHostException uhe) {
+      System.err.println("ERROR: Unknown host");
+      System.err.println(uhe.toString());
+      System.exit(4);
+    }
+    catch (FileNotFoundException fnfe) {
+      System.err.println("ERROR: 404 File not found");
+      System.err.println(fnfe.toString());
+      System.exit(5);
+    }
+    catch (ConnectException ce) {
+      System.err.println("ERROR: Connection timed out");
+      System.err.println(ce.toString());
+      System.exit(6);
+    }
+    catch (SocketTimeoutException ste) {
+      System.err.println("ERROR: Socket timed out");
+      System.err.println(ste.toString());
+      System.exit(7);
     }
     catch (IOException ioe) {
       System.err.println("ERROR: I/O Exception occured");
-      ioe.printStackTrace();
-      System.exit(3);
+      System.err.println(ioe.toString());
+      System.exit(8);
+    }
+    catch (InterruptedException ie) {
+      System.err.println("ERROR: Interrupted Exception occured");
+      System.err.println(ie.toString());
+      System.exit(9);
     }
     finally {
       try {
         is.close();
         System.out.close();
       }
-      catch (IOException ioe) {
+      catch (Exception e) {
       }
     }
-  }
-}
+  } // main
+} // class wget
